7e2da9eb72476d71f6a05b60445a26198608967f
junes7 <2junes@naver.com>
2021-01-26 23:56:52 +0900

    review how to get sum of large number by using for loop and if statement with python

--- coding_test_book/3-2.py
+++ coding_test_book/3-2.py
@@ -6,18 +6,28 @@ data = list(map(int, input().split()))
 
 data.sort()
 result = 0
-# first = data[n-1]
-# second = data[n-2]
-
-# while True:
-
-# 좀 더 간단한 방법으로 하기
-count = 0
 first = data[n-1]
 second = data[n-2]
-count += int(m/(k+1)) * k
-count += m % (k+1)
-result += count * first
-result += (m-count) * second
+
+while True:
+    for i in range(k):
+        if m == 0:
+            break
+        result += first
+        m -= 1
+    if m == 0:
+        break
+    result += second
+    m -= 1
 print(result)
+
+# 좀 더 간단한 방법으로 하기
+# count = 0
+# first = data[n-1]
+# second = data[n-2]
+# count += int(m/(k+1)) * k
+# count += m % (k+1)
+# result += count * first
+# result += (m-count) * second
+# print(result)
 
