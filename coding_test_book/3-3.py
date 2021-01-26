d6905ae4a2580aa3e67a269343aa06650e825670
junes7 <2junes@naver.com>
2021-01-27 00:44:06 +0900

    review and solve number card game by using for loop and list and map method with python

--- coding_test_book/3-3.py
+++ coding_test_book/3-3.py
@@ -1,1 +1,34 @@
-n, m = map(int, input().split)
+n, m = map(int, input().split())
+
+# 책의 방법
+result = 0
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_val = min(data)
+    result = max(result, min_val)
+print('결과:', result)
+
+
+
+
+# matrix = []
+# for i in range(n):
+#     matrix.append(list(map(int, input().split())))
+# # 입력 결과 확인
+# print('입력 결과 확인:')
+# for i in matrix:
+#     for j in i:
+#         print(j, end=' ')
+#     print()
+# # 검사
+# min = 0
+# min_list = []
+# for i in range(n):
+#     min = matrix[i][0]
+#     print('min', min)
+#     for j in range(m):
+#         if matrix[i][j] < min:
+#             min = matrix[i][j]
+#     min_list.append(min)
+
+# print(max(min_list))
