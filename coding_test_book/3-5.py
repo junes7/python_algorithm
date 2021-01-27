b72e578b487a5e5114fda5625c6466e7a0730568
junes7 <2junes@naver.com>
2021-01-27 23:27:33 +0900

    solve number to one with some rule by using map method and while loop and if elif statement

--- coding_test_book/3-5.py
+++ coding_test_book/3-5.py
@@ -1,12 +1,12 @@
-# n, k = map(int, input().split())
-# count = 0
-# while True:
-#     if n == 1:
-#         break
-#     if n % k != 0:
-#         n -= 1
-#         count += 1
-#     elif n % k == 0:
-#         n//=k
-#         count += 1
-# print(count)
+n, k = map(int, input().split())
+count = 0
+while True:
+    if n == 1:
+        break
+    if n % k != 0:
+        n -= 1
+        count += 1
+    elif n % k == 0:
+        n//=k
+        count += 1
+print(count)
