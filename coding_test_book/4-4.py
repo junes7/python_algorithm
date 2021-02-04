ea8351a6e5481080cdaae1cbb0a8aaf80122aab7
junes7 <2junes@naver.com>
2021-02-04 23:35:31 +0900

    solve game development with while loop and if else statement

--- coding_test_book/4-4.py
+++ coding_test_book/4-4.py
@@ -13,4 +13,30 @@ def turn_left():
     direction = -1
     if direction == -1:
         direction = 3
-        
+
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn_time = 0
+print(count)
