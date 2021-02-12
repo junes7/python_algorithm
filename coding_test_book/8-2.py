4482f0e9f48c16b869b611307cca40deeb472018
junes7 <2junes@naver.com>
2021-02-12 22:27:13 +0900

    review implement fibonacci function with recursive function

--- coding_test_book/8-2.py
+++ coding_test_book/8-2.py
@@ -1,4 +1,10 @@
+# 피보나치 수열 소스코드
+# 한 번 계산된 결과를 메모이제이션(Memoization)하기
+# 위한 리스트 초기화
 d = [0] * 100
+
+# 피보나치 함수(Fibonacci Function)를 재귀 함수
+# (recursive function)로 구현(탑다운 다이나믹 프로그래밍)
 
 def fibo(x):
     if x == 1 or x == 2:
