c752289c49298e4909fa3abc70f51dcf56b4a64d
junes7 <2junes@naver.com>
2021-02-16 23:59:08 +0900

    study how to implement improved dijkstra algorithm

---
+++ 9-2.py
@@ -0,0 +1,29 @@
+# 개선된 다익스트라 알고리즘
+import heapq
+import sys
+input = sys.stdin.readline
+# 무한을 의미하는 값으로 10억을 설정
+INF = int(1e9)
+
+# 노드의 개수, 간선의 개수를 입력받기
+n, m = map(int, input().split())
+# 시작 노드 번호를 입력받기
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n + 1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    graph[a].append((b, c))
+
+
+
+
+
+
+
+
