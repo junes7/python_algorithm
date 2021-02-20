2e34059f939d05ba1ad705bdbd0c90dbeb615c4d
junes7 <2junes@naver.com>
2021-02-20 16:30:27 +0900

    solve how to check prime number problem with time complexity O(x)

---
+++ coding_test_book/prime_number.py
@@ -0,0 +1,9 @@
+# 시간 복잡도 O(x)
+def is_prime_number(x):
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
+print(is_prime_number(4))
+print(is_prime_number(7))
