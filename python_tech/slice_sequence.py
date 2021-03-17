7a13c7e33f30d7dda08068f41a9090afae85106a
junes7 <2junes@naver.com>
2021-03-17 23:34:38 +0900

    study how to use stride with list

--- python_tech/slice_sequence.py
+++ python_tech/slice_sequence.py
@@ -11,4 +11,12 @@ print(a[2:5])
 print(a[2:-1])
 print(a[-3:-1])
 
+a = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+odds = a[::2]
+evens = a[1::2]
+print(odds)
+print(evens)
 
+x = b'mongose'
+y = x[::-1]
+print(y)
