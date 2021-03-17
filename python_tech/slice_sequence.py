356cc390db07abbe1cceff06dd16e618e398d863
junes7 <2junes@naver.com>
2021-03-17 22:50:58 +0900

    study how to slice a sequence

---
+++ python_tech/slice_sequence.py
@@ -0,0 +1,14 @@
+a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+print('First four:', a[:4])
+print('Last four:', a[-4:])
+print('Middle two:', a[3:-3])
+print(a[:])
+print(a[:5])
+print(a[:-1])
+print(a[4:])
+print(a[-3:])
+print(a[2:5])
+print(a[2:-1])
+print(a[-3:-1])
+
+
