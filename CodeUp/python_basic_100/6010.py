9c6755819bca5e20a41c91c1789bf45b54a9ddea
junes7 <2junes@naver.com>
2021-05-31 17:56:30 +0900

    how to print input values of various types

---
+++ CodeUp/python_basic_100/6010.py
@@ -0,0 +1,3 @@
+# 정수 1개 입력받아 int로 변환하여 출력하기
+n = int(input())
+print(n)
---
+++ CodeUp/python_basic_100/6012.py
@@ -0,0 +1,10 @@
+# 정수 2개 입력받아 그대로 출력하기 1
+# method 1
+a = input()
+b = input()
+print(a, b, sep="\n")
+# method 2
+# a = input()
+# b = input()
+# print(a)
+# print(b)
