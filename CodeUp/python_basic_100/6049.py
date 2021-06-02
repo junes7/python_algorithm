5956e4f721ea5c6d702b2b938d8e5227d4b8fc64
junes7 <2junes@naver.com>
2021-06-02 15:03:51 +0900

    review how to compare two input integer values by using if else condition

---
+++ CodeUp/python_basic_100/6049.py
@@ -0,0 +1,10 @@
+# 정수 2개 입력받아 비교하기 2
+# method 1
+a, b = input().split()
+print(True if int(a) == int(b) else False)
+# method 2
+# a, b = input().split()
+# if int(a) == int(b):
+#     print(True)
+# else:
+#     print(False)
---
+++ CodeUp/python_basic_100/6052.py
@@ -0,0 +1,12 @@
+# 정수 입력받아 참 거짓 평가하기
+# 정수 1개를 입력받아 값이 0이면 False,
+# 0이 아니면 True를 출력한다.
+# method 1
+a = int(input())
+if a != 0:
+    print(True)
+else:
+    print(False)   
+# method 2
+# a = int(input())
+# print(True if a != 0 else False)
