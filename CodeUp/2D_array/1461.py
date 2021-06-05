350b0e3056fa56831349a0fd3720cac4bea88ecd
junes7 <2junes@naver.com>
2021-06-05 23:19:13 +0900

    solve fill the 2D array in reverse order

--- CodeUp/2D_array/1461.py
+++ CodeUp/2D_array/1461.py
@@ -1,16 +1,17 @@
-# 2차원 배열 순서대로 채우기 1-1
+# 2차원 배열 순서대로 채우기 1-2
 # 입력이 n인 경우 n*n 크기의 2차원 배열 출력
 # 배열의 크기 입력
 n = int(input())
-b=[]
+b = []
 t = 0
-# n * n 공간 생성 및 채우기
+# n * n 공간 생성 및 역순으로 채우기
 for i in range(n):
     b.append([])
     for j in range(n):
         t += 1
         b[i].append(t)
-# 채운 배열 출력
+    b[i].sort(reverse=True)
+# 입력 확인
 for i in range(n):
     for j in range(n):
         print(b[i][j], end=' ')
