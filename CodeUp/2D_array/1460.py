df630cc5dff67b363442331a25aee0afa56fce32
junes7 <2junes@naver.com>
2021-06-05 22:51:11 +0900

    solve that fill 2D array in order

---
+++ CodeUp/2D_array/1460.py
@@ -0,0 +1,18 @@
+# 2차원 배열 순서대로 채우기 1-1
+# 입력이 n인 경우 n*n 크기의 2차원 배열 출력
+# 배열의 크기 입력
+n = int(input())
+b=[]
+t = 0
+# n * n 공간 생성 및 채우기
+for i in range(n):
+    b.append([])
+    for j in range(n):
+        t += 1
+        b[i].append(t)
+
+# 채운 배열 출력
+for i in range(n):
+    for j in range(n):
+        print(b[i][j], end=' ')
+    print()
