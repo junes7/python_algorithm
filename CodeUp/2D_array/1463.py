b199058bc947a138031a8062902c7686ec476a0a
junes7 <2junes@naver.com>
2021-06-06 10:16:06 +0900

    solve that fill 2D array in order with regular intervals and reverse row order

--- CodeUp/2D_array/1463.py
+++ CodeUp/2D_array/1463.py
@@ -1,18 +1,18 @@
-# 2차원 배열 순서대로 채우기 1-2
+# 2차원 배열 순서대로 채우기
 # 입력이 n인 경우 n*n 크기의 2차원 배열 출력
-# 배열의 크기 입력
+# 배열의 크기 입력 및 아래서부터 일정 간격 숫자 채우기
 # method 1
 n = int(input())
 b = []
-t = 0
-# n * n 공간 생성 및 역순으로 채우기
 for i in range(n):
+    t = n - i
     b.append([])
     for j in range(n):
-        t += 1
         b[i].append(t)
-    b[i].sort(reverse=True)
-# 입력 확인
+        if j == n-1:
+            break
+        t += n
+# 입력 값 확인
 for i in range(n):
     for j in range(n):
         print(b[i][j], end=' ')
