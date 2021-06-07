f8afe7715126979e4f6d3ce5d624155d52875d43
junes7 <2junes@naver.com>
2021-06-07 10:38:10 +0900

    study how to fill 2D array with 2D array with up down zigzag order

---
+++ CodeUp/2D_array/1470.py
@@ -0,0 +1,22 @@
+# 2차원 배열 지그재그 채우기 2-3
+# 입력이 n인 경우 n*n 크기 배열 지그재그로 채우기
+n = int(input())
+b = []
+for i in range(n):
+    b.append([])
+    for j in range(n):
+        b[i].append(0)
+t = 1
+# 값 채우기
+for i in range(n):
+    for j in range(n):
+        if i % 2 == 0:
+            b[j][i] = t
+        else:
+            b[n-1-j][i] = t
+        t += 1
+# 입력 배열 확인
+for i in range(n):
+    for j in range(n):
+        print(b[i][j], end=' ')
+    print()
