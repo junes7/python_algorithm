1c7354dbc0b103975a12a48856ea44500ef18968
junes7 <2junes@naver.com>
2021-06-07 10:18:24 +0900

    study how to fill 2D array with zigzag orderand reverse order about even row

---
+++ CodeUp/2D_array/1469.py
@@ -0,0 +1,17 @@
+# 2차원 배열 지그재그 채우기 2-2
+# 입력이 n인 경우 n*n 크기 배열 지그재그로 채우기
+n = int(input())
+b = []
+t = 1
+for i in range(n):
+    b.append([])
+    for j in range(n):
+        b[i].append(t)
+        t += 1
+    if i % 2 == 0:
+        b[i].sort(reverse=True)
+# 입력 배열 확인
+for i in range(n):
+    for j in range(n):
+        print(b[i][j], end=' ')
+    print()
