677396b37e85a936615cbbc7b176bb01ffcd80a4
junes7 <2junes@naver.com>
2021-06-08 20:06:16 +0900

    study how to fill 2D array with diagonal order

--- CodeUp/2D_array/1477.py
+++ CodeUp/2D_array/1477.py
@@ -3,14 +3,14 @@
 # 2차원 배열 빗금 채우기로 출력
 # method 1
 n, m = map(int, input().split())
-b = [[0] * m for i in range(n)]
-t = 0
+b = [[0]*m for i in range(n)]
+cnt = 0
 for i in range(n+m-1):
-    for j in range(m):
-        for k in range(n):
+    for j in range(n):
+        for k in range(m):
             if j+k == i:
-                t += 1
-                b[k][j] = t
+                cnt += 1
+                b[j][k] = cnt
 for i in range(n):
     for j in range(m):
         print(b[i][j], end=' ')
