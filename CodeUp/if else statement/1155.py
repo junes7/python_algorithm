d985ef1272bc20647686901255b02f4bce11da3f
junes7 <2junes@naver.com>
2021-06-12 09:57:02 +0900

    solve a multiple of seven and odd even distinguish

---
+++ CodeUp/if else statement/1155.py
@@ -0,0 +1,3 @@
+# 7의 배수(a multiple of seven)
+n = int(input())
+print('multiple' if n % 7 == 0 else 'not multiple')
---
+++ CodeUp/if else statement/1156.py
@@ -0,0 +1,6 @@
+# 홀수 짝수 구별 Odd Even Distinguish
+# 어떤 자연수가 입력되면 홀수이면 'odd' 출력,
+# 짝수이면 'even' 출력
+# method 1
+n  = int(input())
+print('odd' if n % 2 == 1 else 'even')
