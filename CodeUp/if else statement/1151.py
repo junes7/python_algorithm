2c7b4517337543540b6053597372babcd5338c0e
junes7 <2junes@naver.com>
2021-06-12 09:11:23 +0900

    study how to express less than 10 with if else statement

---
+++ CodeUp/if else statement/1151.py
@@ -0,0 +1,6 @@
+# 10보다 작은 수
+# 10보다 작은 수가 입력되면 small을 출력,
+# 10이상이면 아무것도 출력하지 마시오.
+# method 1
+n = int(input())
+print('small' if n < 10 else '')
---
+++ CodeUp/if else statement/1152.py
@@ -0,0 +1,6 @@
+# 10보다 작은 수 (else 버전)
+# 10보다 작은 정수가 입력되면 small을 출력,
+# 그 이상의 수가 입력되면 big을 출력하시오
+# method 1
+n = int(input())
+print('small' if n < 10 else 'big')
