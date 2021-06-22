63cdb76a24af4ed06836bd1171d0229c27d9f2af
junes7 <2junes@naver.com>
2021-06-22 18:48:45 +0900

    solve that filling 2D array with snail pattern problems by using for, while loop and if, else statement

--- CodeUp/2D_array/1491.py
+++ CodeUp/2D_array/1491.py
@@ -1,4 +1,4 @@
-# 2차원 배열 달팽이 채우기 4-6
+# 2차원 배열 달팽이 채우기 4-8
 # filling 2D array with snail pattern
 # 다음과 같은 n*m 배열 구조를 출력해보자.
 # 입력이 n m인 경우 2차원 배열을 출력해보자.
@@ -6,12 +6,20 @@ n, m = map(int, input().split())
 a = [[0] * m for i in range(n)]
 k, c = n*m+1, -1
 f = n
-i = n-1
-j = m
-d, e = m, n
+i = n
+j = 0
+d, e = n, m
 while True:
-    # 행 고정, 열 감소 또는 증가
+    # 열 고정, 행 감소 또는 증가
     for b in range(1, d+1):
+        if k <= 1:
+             break
+        k -= 1
+        i += c
+        a[i][j] = k
+    # 행 고정, 열 증가 또는 감소
+    c *= -1
+    for b in range(1, e):
         if k <= 1:
             break
         k -= 1
@@ -20,14 +28,6 @@ while True:
     f -= 1
     if f < 1:
         break
-    # 열 고정, 행 감소 또는 증가
-    for b in range(1, e):
-        if k <= 1:
-            break
-        k -= 1
-        i += c
-        a[i][j] = k
-    c *= -1
     d -= 1
     e -= 1
 for x in range(n):
