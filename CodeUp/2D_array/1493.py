bafd11fab1c6b65e64dcd216b9db9d04c1c0ea45
junes7 <2junes@naver.com>
2021-06-23 11:34:29 +0900

    solve that create 1D cumulative sum array and 2D cumulative sum array problems by using for loop and if, else statement

---
+++ CodeUp/2D_array/1493.py
@@ -0,0 +1,26 @@
+# 2차원 누적 합 배열 만들기 5-2
+# create 2D cumulative sum array
+# n*m개의 데이터를 배열에 입력 받은 후, (1, 1)
+# 데이터 ~ (n, m)영역의 누적 합 배열을 만들고
+# 출력해보자.
+n, m = map(int, input().split())
+d = [[0] * m for i in range(n)]
+for i in range(n):
+    t = list(map(int, input().split()))
+    d[i] = t
+s = [[0] * m for i in range(n)]
+tm = 0
+a = [0] * m
+for i in range(n):
+    for j in range(m):
+        if j == 0:
+            a[j] += d[i][j]
+            s[i][j] = a[j]
+        else:
+            a[j] += d[i][j]
+            tm = s[i][j-1] + a[j]
+            s[i][j] = tm
+for x in range(n):
+    for y in range(m):
+        print(s[x][y], end=' ')
+    print()
