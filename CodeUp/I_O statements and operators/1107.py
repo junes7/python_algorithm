77ce59d4445fe53d9c73e4742424fdbacc2bf37b
junes7 <2junes@naver.com>
2021-06-27 14:15:22 +0900

    solve print repeatedly problems with print method

---
+++ CodeUp/I_O statements and operators/1107.py
@@ -0,0 +1,5 @@
+# 반복 출력하기 1(print repeatedly)
+# print 단어를 연속으로 50번 출력하시오.(띄어쓰기 없음)
+# printprint ... printprint (50번 출력)
+# method 1
+print('print' * 50, sep='')
---
+++ CodeUp/I_O statements and operators/1108.py
@@ -0,0 +1,6 @@
+# 반복 출력하기 2(print repeatedly)
+# hello를 20번 연속출력한 다음 한 칸을 띄우고(공백 한칸(줄바꿈 아님))world를
+# 연속 30번 출력하시오.
+# hellohello ... hello worldworld ... world (hello 20번 world 30번)
+# method 1
+print('hello' * 20, 'world' * 30)
