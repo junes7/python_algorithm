faaabc5e0886de98c37de88c6f3f6c734e0a6439
junes7 <2junes@naver.com>
2021-06-27 13:43:36 +0900

    review how to print greeting sentence with print method

---
+++ CodeUp/I_O statements and operators/1101.py
@@ -0,0 +1,7 @@
+# Hello, World!
+# 프로그래밍 세계에 오신걸 환영합니다. 첫 번째 문제는 인사말을 출력하는
+# 문제입니다. 기본 출력 명령은 print 입니다. Hello, World!를 출력하시오.
+# method 1
+# print('Hello,', 'World!')
+# method 2
+print('Hello, World!')
---
+++ CodeUp/I_O statements and operators/1102.py
@@ -0,0 +1,7 @@
+# Hello, World! (줄 바꿈 버전)
+# Hello, World!를 다음과 같이 두 줄로 출력하시오.
+# Hello,
+# World!
+# \n은 줄을 바꾸는 제어 문자이다. 이를 활용해보자.
+# method 1
+print('Hello,', 'World!', sep='\n')
