f1d450acfa5d2815be9fd077e319950e54222d88
junes7 <2junes@naver.com>
2021-07-03 14:01:32 +0900

    solve print special formatting characters and division and square problems with special format characters

---
+++ CodeUp/I_O statements and operators/1731.py
@@ -0,0 +1,10 @@
+# 특수 서식 문자 출력하기(print special formatting characters)
+# 다음 문장을 출력하시오.
+# special characters
+# [\n,\",\\] is very important.
+# 위 해당 문장들을 출력한다. 역 슬래시(\)는 원 기호와 같습니다. 한글 글꼴을
+# 사용하는 우리 나라에서는 역슬리시(\) 키를 누르면 원(\) 기호가 출력됩니다.
+# 즉, 출력되는 문장의 역슬래시 자리에 원 기호를 사용하시면 됩니다.
+# method 1
+print('special characters')
+print('[\\n,\\\",\\\\] is very important.')
