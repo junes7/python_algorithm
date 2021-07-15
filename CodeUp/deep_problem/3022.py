a677cea0ccb9cc791bec6983f2e61eba2105026a
junes7 <2junes@naver.com>
2021-07-16 08:30:13 +0900

    solve add and subtract large numbers problems with print method and operator

---
+++ CodeUp/deep_problem/3022.py
@@ -0,0 +1,7 @@
+# 큰 수 뺄셈(Subtract large numbers)
+# 큰 수 덧셈에 성공했다. 이번에는 큰 수 뺄셈에 도전하자.
+# 큰 수 a, b가 두 줄에 걸쳐 입력된다. (a, b는 100자리 이하)
+# a-b 의 결과를 출력한다.
+# method 1
+a, b = int(input()), int(input())
+print(a-b)
