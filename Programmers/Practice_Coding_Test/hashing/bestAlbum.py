4b7dcc6a7c091a2d886b9b52b6848bd77dee165b
junes7 <2junes@naver.com>
2021-07-25 09:45:06 +0900

    solve bestAlbum problems by using for loop, dictionary array, and if, else statement

---
+++ Programmers/Practice_Coding_Test/hashing/bestAlbum.py
@@ -0,0 +1,53 @@
+# 베스트앨범(bestAlbum)
+# 스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트
+# 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은
+# 다음과 같습니다.
+# 1. 속한 노래가 많이 재생된 장르를 먼저 수록합니다.
+# 2. 장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+# 3. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저
+# 수록합니다.
+# 노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는
+# 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를
+# 순서대로 return 하도록 solution 함수를 완성하세요.
+# genres[i]는 고유번호가 i인 노래의 장르입니다.
+# plays[i]는 고유번호가 i인 노래가 재생된 횟수입니다.
+# genres와 plays의 길이는 같으며, 이는 1 이상 10,000 이하입니다.
+# 장르 종류는 100개 미만입니다.
+# 장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
+# 모든 장르는 재생된 횟수가 다릅니다.
+# zip()은 동일한 개수로 이루어진 자료형을 묶어 주는 역할을 하는 함수이다.
+# method 1
+# from collections import defaultdict
+# def solution(genres, plays):
+#     answer = []
+#     music = defaultdict(list)
+#     musicSum = defaultdict(int)
+#     for i, [g, p] in enumerate(zip(genres, plays)):
+#         music[g].append((-p, i))
+#         musicSum[g] += p
+#     musicSum = [[musicSum[k], k] for k in musicSum]
+#     musicSum.sort(reverse=True)
+#     for val, key in musicSum:
+#         musicList = sorted(music[key])
+#         for p, i in musicList[:2]:
+#             answer.append(i)
+#     return answer
+# method 2
+from collections import defaultdict
+def solution(genres, plays):
+    answer = []
+    music = defaultdict(list)
+    musicSum = defaultdict(int)
+    for i, [g, p] in enumerate(zip(genres, plays)):
+        music[g].append((-p, i))
+        musicSum[g] += p
+    musicSum = [[musicSum[k], k] for k in musicSum]
+    musicSum.sort(reverse=True)
+    for val, key in musicSum:
+        musicList = sorted(music[key])
+        for p, i in musicList[:2]:
+            answer.append(i)
+    return answer
+genres = ["classic", "pop", "classic", "classic", "pop"]
+plays = [500, 600, 500, 800, 2500]
+print(solution(genres, plays))
