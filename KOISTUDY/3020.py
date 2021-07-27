67d0b37c817ab478208154e93b9b2ae9fe4121dd
junes7 <2junes@naver.com>
2021-07-27 10:35:00 +0900

    review how to print string and single, double quotes with print method

---
+++ KOISTUDY/3020.py
@@ -0,0 +1,5 @@
+# 출력하기 07
+# 다음과 같은 파일 경로를 출력하는 연습을 해보자.
+# "C:\Download\hello.py"
+# (큰따옴표(") 문자도 함께 출력한다.)
+print('"C:\Download\hello.py"')
---
+++ KOISTUDY/3021.py
@@ -0,0 +1,4 @@
+# 출력하기 08
+# 이번에는 다음과 같은 python 소스 코드 내용을 똑같이 출력해 보자.
+# print("Hello World")
+print('print("Hello World")')
