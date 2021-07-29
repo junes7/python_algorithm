4bb16b5b08da27803638b5fce4ecfc5d65acf721
junes7 <2junes@naver.com>
2021-07-29 10:37:18 +0900

    solve print a sentence multiple times and calculate square root of two integers by using map and input method

---
+++ KOISTUDY/3047.py
@@ -0,0 +1,9 @@
+# 문장 여러 번 출력하기
+# print a sentence multiple times
+# 반복 횟수와 문장을 입력받아 그 문장을 그 횟수 만큼 출력해 보자.
+# method 1
+# n = input()
+# s = input()
+# print(s * int(n))
+# method 2
+print(int(input())*input())
---
+++ KOISTUDY/3048.py
@@ -0,0 +1,7 @@
+# 정수 2개 입력받아 거듭제곱 계산하기
+# calculate square root of two integers
+# 정수 2개(a, b)를 입력받아 a를 b번 곱해 거듭제곱한 값을 출력해 보자.
+# (*2를 10번 거듭제곱한 값은 2^10 즉, 2를 10번 곱한 2*2*2*2*2*2*2*2*2*2 값을 의미한다.)
+# method 1
+a, b = map(int,input().split())
+print(a**b)
