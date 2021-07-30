1533747676788fefcee60fd9d882854a2fb90cdf
junes7 <2junes@naver.com>
2021-07-30 09:09:48 +0900

    print multiply with power of two and comparison of two integers problems by using map and input method

---
+++ KOISTUDY/3057.py
@@ -0,0 +1,5 @@
+# 2의 거듭제곱 배로 곱해 출력하기
+# print multiply with power of two
+# 정수 2개(a, b)를 입력받아 a에 2b를 곱한 값을 출력해 보자.
+a,b=map(int,input().split())
+print(a<<b)
---
+++ KOISTUDY/3058.py
@@ -0,0 +1,6 @@
+# 정수 2개 입력받아 비교하기 1
+# comparison of two integers
+# 두 정수(a, b)를 입력받아 a의 값이 b의 값보다 작으면 True, 이외의
+# 경우에는 False 를 출력해 보자.
+a,b=map(int,input().split())
+print(True if a<b else False)
