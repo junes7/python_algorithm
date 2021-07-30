7940461e0fbf763ce5bbc0c51354968106ddd98e
junes7 <2junes@naver.com>
2021-07-30 09:31:58 +0900

    solve comparison of two integers and evaluate true or false about one integer problems by using bool, map, input, and print method

---
+++ KOISTUDY/3061.py
@@ -0,0 +1,6 @@
+# 정수 2개 입력받아 비교하기 4
+# comparison of two integers
+# 두 정수(a, b)를 입력받아 a의 값과 b의 값이 서로 다르면 True를,
+# 그렇지 않으면 False를 출력해 보자.
+a,b=map(int,input().split())
+print(a!=b)
---
+++ KOISTUDY/3062.py
@@ -0,0 +1,3 @@
+# 정수 입력받아 참 거짓 평가하기
+# Evaluate true or false about one integer
+print(bool(int(input())))
