1485101ae77aa7606452b88ba66cf23660fe9ab0
junes7 <2junes@naver.com>
2021-08-01 10:51:50 +0900

    solve that print number until one integer problems by using for loop and input, print method

---
+++ KOISTUDY/3085.py
@@ -0,0 +1,9 @@
+# 정수 1개 입력받아 그 수까지 출력하기
+# print number until one integer
+# method 1
+# for i in range(int(input())+1):
+#     print(i)
+# method 2
+n=int(input())
+for i in range(n+1):
+    print(i)
---
+++ KOISTUDY/3086.py
@@ -0,0 +1,4 @@
+# 정수 1개 입력받아 그 수까지 출력하기 2
+# print number until one integer
+for i in range(int(input())+1):
+    print(i)
