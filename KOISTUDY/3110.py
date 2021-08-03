8fddf50a434050ef0b4d154b23060c3b071c1fd7
junes7 <2junes@naver.com>
2021-08-03 14:00:53 +0900

    review how to print two strings with reverse order and one string three times with input, and print method

---
+++ KOISTUDY/3110.py
@@ -0,0 +1,6 @@
+# 문자 2개 입력받아 순서 바꿔 출력하기 2
+# print two strings with reverse order
+# 스페이스로 분리되어 입력된 문자(character) 2개를 입력받아 순서를 바꿔
+# 출력해 보자.
+a,b=input().split()
+print(b,a)
---
+++ KOISTUDY/3111.py
@@ -0,0 +1,6 @@
+# 문장 1개 입력받아 3번 출력하기
+# print one string three times
+# 정수(integer), 실수, 문자(character), 문자열(string) 등 1개만 입력받아
+# 한 줄로 3번 출력해보자.
+s=input()
+print(s,s,s)
