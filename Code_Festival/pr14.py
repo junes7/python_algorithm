729b04abd516eb6938cdb527cce2b0233715ac41
junes7 <2junes@naver.com>
2021-10-20 08:30:42 +0900

    solve that distinguish multiple of three problem by using if, else statement or ternary conditional operator

---
+++ Code_Festival/pr14.py
@@ -0,0 +1,9 @@
+﻿# method 1 wiht ternary conditional operator
+n=int(input())
+print("짝" if n%3==0 else n)
+# method 2 with if else statement
+m=int(input())
+if m%3==0:
+    print("짝")
+else:
+    print(m);
