930f73ffdad3ffe21d6252d50f41ca4d9ef3f8b8
junes7 <2junes@naver.com>
2021-10-21 08:11:40 +0900

    solve quotient and variable problem by using list or variable method

---
+++ Code_Festival/pr20.py
@@ -0,0 +1,9 @@
+﻿# quotient and remainder
+# use list
+n=list(map(int,input().split()))
+result=n[0]//n[1]
+left=n[0]%n[1]
+print(result, left)
+# use variable
+a,b=map(int,input().split())
+print(a//b, a%b)
