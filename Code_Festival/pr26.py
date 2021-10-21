207d8dc6ba01e65f7ac9e0b83ed992fad4b79293
junes7 <2junes@naver.com>
2021-10-22 07:52:20 +0900

    solve planet problem with dictionay list

---
+++ Code_Festival/pr26.py
@@ -0,0 +1,6 @@
+﻿# planet problem
+planet={'수성': 'Mercury', '금성': 'Venus', '지구': 'Earth',
+'화성': 'Mars', '목성': 'Jupiter', '토성': 'Saturn',
+'천왕성': 'Uranus', '해왕성': 'Neptune'}
+n=input()
+print(planet[n])
