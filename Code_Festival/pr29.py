ada003e59a9aade76d9e76b590eef5c0bf25579f
junes7 <2junes@naver.com>
2021-10-22 09:13:52 +0900

    solve pass uppercase only problem by using ternary conditional operator or if else statement

---
+++ Code_Festival/pr29.py
@@ -0,0 +1,9 @@
+﻿# pass uppercase only
+# use ternary conditional operator
+n=input()
+print("YES" if n.isupper() else "NO")
+# use if else statement
+if n.isupper():
+    print("YES")
+else:
+    print("NO")
