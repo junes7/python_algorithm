61a5d4624e0468d34b5ff4b220df52711d1d9dc2
junes7 <2junes@naver.com>
2021-10-23 10:12:04 +0900

    solve that print elements with reverse order problem by using for loop, and strip, list method

---
+++ Code_Festival/pr33.py
@@ -0,0 +1,11 @@
+﻿# print elements with reverse order
+# without int casting
+# method 1
+# n=list(map(int, input().split()))
+# l=len(n)
+# for i in range(l):
+#     print(n[l-1-i], end=' ')
+# method 2
+n=list(input().strip().split())
+for i in range(len(n)-1,-1,-1):
+    print(n[i], end=' ')
