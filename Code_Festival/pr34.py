47c6b7ba6a9886090a621fcdb9e5d6481bef28b3
junes7 <2junes@naver.com>
2021-10-23 11:36:53 +0900

    solve distinction that list is sorted or not problem by using for loop, if else statement, and list method

---
+++ Code_Festival/pr34.py
@@ -0,0 +1,14 @@
+﻿# sort 확인하기
+# 키가 주어지면 순서대로 제대로 섰는지 확인하는 프로그램
+# method 1
+# n=list(map(int, input().split()))
+# b=0
+# for i in range(len(n)-1):
+#     if n[i] > n[i+1]:
+#         b=1
+#         break
+# print("NO" if b!=0 else "YES")
+# method 2
+l=list(input().strip().split())
+l=[int(i) for i in l]
+print("NO" if l!=sorted(l) else "YES")
