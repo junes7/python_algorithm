624b2977992c790ff25505e182b7a3a90fb5e267
junes7 <2junes@naver.com>
2021-10-23 12:05:57 +0900

    solve use factory function problem by using function definition and square operator

---
+++ Code_Festival/pr35.py
@@ -0,0 +1,13 @@
+﻿# use factory function
+# get number to the power of two, three, and four
+def one(n):
+    def two(value):
+        sq=value**n;
+        return sq;
+    return two
+a=one(2)
+b=one(3)
+c=one(4)
+print(a(10))
+print(b(10))
+print(c(10))
