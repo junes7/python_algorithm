99d251855ebef529875d0411e0e59371a04208ae
junes7 <2junes@naver.com>
2021-10-24 11:08:51 +0900

    distinction prime number problem by using for loop, if else statement, and modulus operator

---
+++ Code_Festival/pr41.py
@@ -0,0 +1,13 @@
+﻿# 소수판별(distinction prime number)
+# 숫자가 주어지면 소수인지 아닌지
+# 판별하는 프로그램 작성
+n=int(input())
+if n<=1:
+    print('소수가 아닙니다.')
+else:
+    for i in range(2,n):
+        if n%i==0:
+            print('소수가 아닙니다.')
+            break
+    else:
+        print('소수입니다.')
