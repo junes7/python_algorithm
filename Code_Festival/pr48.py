e2b79d25b0eecf1b0b7a32e06479bc0d854973d0
junes7 <2junes@naver.com>
2021-10-26 06:44:21 +0900

    solve that print with changing uppercase and lowercase each other

---
+++ Code_Festival/pr48.py
@@ -0,0 +1,11 @@
+﻿# 대소문자 바꿔서 출력
+# print with changing uppercase and lowercase
+n=input()
+b=[]
+for i in n:
+    if i.islower():
+        b.append(i.upper())
+    else:
+        b.append(i.lower())
+for i in b:
+    print(i,end='')
