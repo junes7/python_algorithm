3b6d0aa2f2eb286aaf426d8e0f96cf159b5d24cc
junes7 <2junes@naver.com>
2021-10-26 07:05:37 +0900

    solve bubble sort problem by using nested for loop, if else statement, and swap value

---
+++ Code_Festival/pr50.py
@@ -0,0 +1,17 @@
+﻿# defining bubble sort
+def bubble(n, data):
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if data[j] > data[j+1]:
+                # method 1
+                # t=data[j]
+                # data[j]=data[j+1]
+                # data[j+1]=t
+                # method 2
+                data[j], data[j+1]=data[j+1], data[j]
+    for i in range(n):
+        print(data[i], end=' ')
+
+n=int(input())
+data=list(map(int, input().split()))
+bubble(n, data)
