3140aca421af4d1d41477a5620dbf0ba09c82ba5
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-06 19:47:24 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,18 +1,22 @@
 def solution(a, b, c):
-    count,answer=0,1
-    if a==b:
-        if b==c:
-            count=3 
-        else:
-            count=2
-    elif a!=b:
-        if b==c:
-            count=2
-        else:
-            if a==c:
-                count=2
-            else:
-                count=1
-    for i in range(1,count+1):
-        answer*=(a**i+b**i+c**i)
+    # count,answer=0,1
+    # if a==b:
+    #     if b==c:
+    #         count=3 
+    #     else:
+    #         count=2
+    # elif a!=b:
+    #     if b==c:
+    #         count=2
+    #     else:
+    #         if a==c:
+    #             count=2
+    #         else:
+    #             count=1
+    # for i in range(1,count+1):
+    #     answer*=(a**i+b**i+c**i)
+    answer=1
+    cnt=abs(len(set([a,b,c]))-3)+1
+    for i in range(1,cnt+1):
+        answer*=(a**i+b**i+c**i)    
     return answer
