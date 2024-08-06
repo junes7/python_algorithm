46503ce3885b7e5acf950375437b12bf3d2c3d7d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-07 07:57:22 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,7 +1,10 @@
 def solution(arr, queries):
-    for q in queries:
-        t=arr[q[0]]
-        arr[q[0]]=arr[q[1]]
-        arr[q[1]]=t
+    # for q in queries:
+    #     # t=arr[q[0]]
+    #     # arr[q[0]]=arr[q[1]]
+    #     # arr[q[1]]=t
+    #     arr[q[0]],arr[q[1]]=arr[q[1]],arr[q[0]]
+    for i,j in queries:
+        arr[i],arr[j]=arr[j],arr[i]
     answer=arr
     return answer
