d5240ba1fad21e63c30adab59bfb93a7ec222f2d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-07 17:52:00 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,16 @@
+def solution(arr1, arr2):
+    answer=0
+    if len(arr1) != len(arr2):
+        if len(arr1) < len(arr2):
+            answer=-1
+        else:
+            answer=1
+    else:
+        if sum(arr1) != sum(arr2):
+            if sum(arr1) > sum(arr2):
+                answer=1
+            else:
+                answer=-1
+        else:
+            answer=0
+    return answer
