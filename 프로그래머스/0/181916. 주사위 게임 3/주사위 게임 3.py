22a25068c67519d31f6778ce544b32a77499dcf4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-08 17:13:36 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,20 +1,38 @@
 def solution(a, b, c, d):
     answer=0
-    d_list=sorted([a,b,c,d])
-    if d_list[0]==d_list[3]:
-        answer=1111*d_list[0]
-    elif d_list[0]==d_list[2] or d_list[1]==d_list[3]:
-        p=d_list[1]
-        q=d_list[3]+d_list[0]-d_list[1]
-        answer=(10*p+q)**2
-    elif d_list[0]==d_list[1] and d_list[2]==d_list[3]:
-        answer=(d_list[0]+d_list[2])*abs(d_list[0]-d_list[2])
-    elif d_list[0]==d_list[1]:
-        answer=d_list[2]*d_list[3]
-    elif d_list[1]==d_list[2]:
-        answer=d_list[0]*d_list[3]
-    elif d_list[2]==d_list[3]:
-        answer=d_list[0]*d_list[1]
+    # method1
+    # d_list=sorted([a,b,c,d])
+    # print(d_list)
+    # if d_list[0]==d_list[3]:
+    #     answer=1111*d_list[0]
+    # elif d_list[0]==d_list[2] or d_list[1]==d_list[3]:
+    #     p=d_list[1]
+    #     q=d_list[3]+d_list[0]-d_list[1]
+    #     answer=(10*p+q)**2
+    # elif d_list[0]==d_list[1] and d_list[2]==d_list[3]:
+    #     answer=(d_list[0]+d_list[2])*abs(d_list[0]-d_list[2])
+    # elif d_list[0]==d_list[1]:
+    #     answer=d_list[2]*d_list[3]
+    # elif d_list[1]==d_list[2]:
+    #     answer=d_list[0]*d_list[3]
+    # elif d_list[2]==d_list[3]:
+    #     answer=d_list[0]*d_list[1]
+    # else:
+    #     answer=d_list[0]
+    # method2
+    l=sorted([a,b,c,d])
+    c=[l.count(t) for t in l]
+    if max(c)==4:
+        answer=1111*l[0]
+    elif max(c)==3:
+        print(c.index(1),c.index(3))
+        answer=(10*l[c.index(3)]+l[c.index(1)])**2
+    elif max(c)==2:
+        if min(c)==1:
+            answer=eval('*'.join(str(l[i]) for i,x in enumerate(c) if x==1))
+        else:
+            answer=(max(l)+min(l))*abs(max(l)-min(l))
     else:
-        answer=d_list[0]
+        answer=l[0]
+    
     return answer
