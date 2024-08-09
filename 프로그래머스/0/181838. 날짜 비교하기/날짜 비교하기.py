daf053b24dbbe50f8cab77870c316e322ff7d678
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-09 21:06:08 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,4 +1,7 @@
 def solution(date1, date2):
-    from datetime import datetime    
-    answer = 1 if datetime(date1[0],date1[1],date1[2])<datetime(date2[0],date2[1],date2[2]) else 0
+    # method1
+    # from datetime import datetime    
+    # answer = 1 if datetime(date1[0],date1[1],date1[2])<datetime(date2[0],date2[1],date2[2]) else 0
+    # method2
+    answer=int(date1<date2)
     return answer
