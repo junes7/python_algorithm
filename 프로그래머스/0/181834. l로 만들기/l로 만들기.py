099946d689453bcc19841546dadd8813f2db8a76
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-09 19:04:08 +0900

    [level 0] Title: l로 만들기, Time: 9.78 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,3 +1,6 @@
 def solution(myString):
+    # method1
+    # answer=''.join('l' if ord(c)<ord('l') else c for c in myString)
+    # method2
     answer=''.join('l' if c<'l' else c for c in myString)
     return answer
