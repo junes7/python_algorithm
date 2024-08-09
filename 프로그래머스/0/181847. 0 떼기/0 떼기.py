0d11e7623fed93adf0a92c507c77e5ebba6ee3bd
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-09 18:24:29 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -1,3 +1,6 @@
 def solution(n_str):
-    answer=n_str[[i for i in range(len(n_str)) if n_str[i]!='0'][0]:]
+    # method1
+    # answer=n_str[[i for i in range(len(n_str)) if n_str[i]!='0'][0]:]
+    # method2
+    answer=n_str.lstrip('0')
     return answer
