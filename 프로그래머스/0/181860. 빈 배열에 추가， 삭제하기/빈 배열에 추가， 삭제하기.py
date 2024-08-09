14e98da3cc6f10b4a9075c7ec683e1532cd150d4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-09 21:39:05 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.07 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -1,10 +1,18 @@
 def solution(arr, flag):
-    answer = []
-    for i,b in enumerate(flag):
+    # method1
+    # answer = []
+    # for i,b in enumerate(flag):
+    #     if b:
+    #         for j in range(arr[i]*2):
+    #             answer.append(arr[i])
+    #     else:
+    #         for j in range(arr[i]):
+    #             answer.pop()
+    # method2
+    answer=[]
+    for i,b in zip(arr,flag):
         if b:
-            for j in range(arr[i]*2):
-                answer.append(arr[i])
+            answer+=[i]*i*2
         else:
-            for j in range(arr[i]):
-                answer.pop()
+            answer=answer[:-i]
     return answer
