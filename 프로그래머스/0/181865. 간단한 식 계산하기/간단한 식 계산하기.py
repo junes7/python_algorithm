1ddcd2a3dc201be6d5d73404fdbc365ec0ccdf80
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-09 19:30:27 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,3 +1,7 @@
 def solution(binomial):
-    answer=eval(binomial)
+    # method1
+    # answer=eval(binomial)
+    # method2
+    e=[int(n) if i%2==0 else n for i,n in enumerate(binomial.split())]
+    answer=e[0]+e[2] if e[1]=='+' else e[0]-e[2] if e[1]=='-' else e[0]*e[2]
     return answer
