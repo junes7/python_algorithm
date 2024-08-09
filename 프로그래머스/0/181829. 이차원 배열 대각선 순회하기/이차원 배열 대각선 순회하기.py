36f3747ac5fe91b16fcd69f4dea9a8d783543b82
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-09 20:43:38 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
@@ -6,5 +6,5 @@ def solution(board, k):
     #         if i+j<=k:
     #             answer+=board[i][j]
     # method2
-    answer=sum([board[i][j] if i+j<=k else 0 for i in range(len(board)) for j in range(len(board[i]))])
+    answer=sum([board[i][j] for i in range(len(board)) for j in range(len(board[i])) if i+j<=k])
     return answer
