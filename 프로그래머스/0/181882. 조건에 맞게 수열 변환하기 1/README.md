9c72df02a69d7dafda45199eb6686267839667f0
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-09 17:27:19 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 84.41 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/README.md
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 조건에 맞게 수열 변환하기 1 - 181882 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181882) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181882#) 
 
 ### 성능 요약
 
-메모리: 39.3 MB, 시간: 114.60 ms
+메모리: 39.4 MB, 시간: 84.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 17:23:54
+2024년 08월 09일 17:27:17
 
 ### 문제 설명
 
--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -1,10 +1,13 @@
 def solution(arr):
-    answer = []
-    for n in arr:
-        if n>=50 and n%2==0:
-            answer.append(n//2)
-        elif n<50 and n%2!=0:
-            answer.append(n*2)
-        else:
-            answer.append(n)
+    # method1
+    # answer = []
+    # for n in arr:
+    #     if n>=50 and n%2==0:
+    #         answer.append(n//2)
+    #     elif n<50 and n%2!=0:
+    #         answer.append(n*2)
+    #     else:
+    #         answer.append(n)
+    # method2
+    answer=[n//2 if n>=50 and not n%2 else n*2 if n<50 and n%2 else n for n in arr]
     return answer
