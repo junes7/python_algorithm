e4a5867f5e8632a07b595a0fb82a38860e31e1cb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-09 21:44:37 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/README.md
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 빈 배열에 추가, 삭제하기 - 181860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181860?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.05 ms
+메모리: 10.2 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 21:41:55
+2024년 08월 09일 21:44:35
 
 ### 문제 설명
 
--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -1,17 +1,16 @@
 def solution(arr, flag):
+    answer=[]
     # method1
-    answer = []
-    for i,b in enumerate(flag):
-        if b:
-            answer+=[arr[i]]*arr[i]*2
-        else:
-            for j in range(arr[i]):
-                answer.pop()
-    # method2
-    # answer=[]
-    # for i,b in zip(arr,flag):
+    # for i,b in enumerate(flag):
     #     if b:
-    #         answer+=[i]*i*2
+    #         answer+=[arr[i]]*arr[i]*2
     #     else:
-    #         answer=answer[:-i]
+    #         for j in range(arr[i]):
+    #             answer.pop()
+    # method2
+    for i,b in zip(arr,flag):
+        if b:
+            answer+=[i]*i*2
+        else:
+            answer=answer[:-i]
     return answer
