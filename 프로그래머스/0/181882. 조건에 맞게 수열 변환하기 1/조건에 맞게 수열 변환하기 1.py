ecbed8dbe597b8061fa31deb9448bb1f77d64ce7
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-11 01:06:21 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 82.82 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -1,13 +1,3 @@
 def solution(arr):
-    # method1
-    # answer = []
-    # for n in arr:
-    #     if n>=50 and n%2==0:
-    #         answer.append(n//2)
-    #     elif n<50 and n%2!=0:
-    #         answer.append(n*2)
-    #     else:
-    #         answer.append(n)
-    # method2
     answer=[n//2 if n>=50 and not n%2 else n*2 if n<50 and n%2 else n for n in arr]
     return answer
