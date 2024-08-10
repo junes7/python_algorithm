2bad4ba3c087daebf13c0635670fd7f46b47380d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-10 23:34:25 +0900

    [level 0] Title: 커피 심부름, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,12 +1,15 @@
 def solution(order):
-    s=0
-    for o in order:
-        if o.startswith('ice') or o.endswith('ice'):
-            s+=4500 if 'ame' in o else 5000
-        elif o.startswith('hot') or o.endswith('hot'):
-            s+=4500 if 'ame' in o else 5000
-        elif o.startswith('ame') or o.startswith('any'):
-            s+=4500
-        else:
-            s+=5000
+    # method1
+    # s=0
+    # for o in order:
+    #     if o.startswith('ice') or o.endswith('ice'):
+    #         s+=4500 if 'ame' in o else 5000
+    #     elif o.startswith('hot') or o.endswith('hot'):
+    #         s+=4500 if 'ame' in o else 5000
+    #     elif o.startswith('ame') or o.startswith('any'):
+    #         s+=4500
+    #     else:
+    #         s+=5000
+    # method2
+    s=sum([5000 if 'latte' in o else 4500 for o in order])
     return s
