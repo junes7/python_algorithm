d97bb58a844ae9f1b73dbc6f0860123253d43526
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-10 17:34:09 +0900

    [level 0] Title: 2의 영역, Time: 1.65 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,12 +1,15 @@
 def solution(arr):
-    s,e=-1,-1
-    for i in range(len(arr)):
-        if arr[i]==2:
-            s=i
-            break
-    for i in range(len(arr)-1,-1,-1):
-        if arr[i]==2:
-            e=i
-            break
-    answer=[-1] if s==-1 and e==-1 else arr[s:e+1]
+    # method1
+    # s,e=-1
+    # for i in range(len(arr)):
+    #     if arr[i]==2:
+    #         s=i
+    #         break
+    # for i in range(len(arr)-1,-1,-1):
+    #     if arr[i]==2:
+    #         e=i
+    #         break
+    # answer=[-1] if s==-1 and e==-1 else arr[s:e+1]
+    # method2
+    answer=[-1] if 2 not in arr else arr[arr.index(2):len(arr)-arr[::-1].index(2)]
     return answer
