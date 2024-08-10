466630faae19fac8c55569d5d8cebf3f141f4f08
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-10 21:55:29 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 4.33 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -6,4 +6,6 @@ def solution(arr):
     #         answer*=1 if arr[i][j]==arr[j][i] else 0
     # method2
     answer=int(all(arr[i][j]==arr[j][i] for i in range(len(arr)) for j in range(len(arr[i]))))
+    print(*arr)
+    print(list(map(list,zip(*arr))))
     return answer
