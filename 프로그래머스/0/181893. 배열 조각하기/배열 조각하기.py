5658a6db7b041758f03bcb8520937924003e23f6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-10 19:36:56 +0900

    [level 0] Title: 배열 조각하기, Time: 1.20 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -1,6 +1,4 @@
 def solution(arr, query):
-    answer = []
     for i,n in enumerate(query):
-        answer=arr[:n+1] if not i%2 else arr[n:]
-        arr=answer
-    return answer
+        arr=arr[:n+1] if not i%2 else arr[n:]
+    return arr
