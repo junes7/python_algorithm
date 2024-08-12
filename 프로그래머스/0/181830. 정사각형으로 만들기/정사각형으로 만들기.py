49a29609b3d00a7bbf3039eb56e970da7357b810
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-12 10:40:10 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.10 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -1,10 +1,9 @@
 def solution(arr):
-    m=arr
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            if len(arr)>len(arr[i]):
-                m[i]+=[0]*(len(arr)-len(arr[i]))
-    if len(arr)<len(arr[0]):
-        for i in range(len(arr[0])-len(arr)):
-            m.append([0]*len(arr[0]))
-    return m
+    n,m=len(arr),len(arr[0])
+    if n<m:
+        for i in range(m-n):
+            arr.append([0]*m)
+    else:
+        for i in range(n):
+            arr[i]+=[0]*(n-m)
+    return arr
