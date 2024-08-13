9b70bfe09111c42baf057e7816a9bea17dcd35dc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-13 12:01:01 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,3 +1,4 @@
-def solution(numbers):
-    answer = [n*2 for n in numbers]
-    return answer
+# def solution(numbers):
+#     answer = [n*2 for n in numbers]
+#     return answer
+solution=lambda numbers:[n*2 for n in numbers]
