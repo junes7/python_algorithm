c6828b06fb5163818bf4a6b4ae7ec2ff3318720e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 00:36:47 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,3 +1,6 @@
 def solution(my_string):
-    answer=my_string.swapcase()
+    # method1
+    # answer=my_string.swapcase()
+    # method2
+    answer=''.join(c.upper() if c.islower() else c.lower() for c in my_string)
     return answer
