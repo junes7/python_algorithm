c50fd47ae8560511946048befbb3e9369d277506
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-13 23:09:45 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,3 +1,10 @@
 def solution(price):
-    answer=int(price*0.8 if price>=500000 else price*0.9 if price>=300000 else price*0.95 if price>=100000 else price)
+    # answer=int(price*0.8 if price>=500000 else price*0.9 if price>=300000 else price*0.95 if price>=100000 else price)
+    drs={500000:0.8,300000:0.9,100000:0.95,0:1}
+    answer=0
+    for dr,r in drs.items():
+        if price>=dr:
+            answer=int(r*price)
+            break
+    
     return answer
