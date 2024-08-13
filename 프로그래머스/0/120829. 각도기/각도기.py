85f52551e505372c524ae82655e542fffa95537d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-13 17:06:58 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,3 +1,6 @@
 def solution(angle):
-    answer=1 if 0<angle and angle<90 else 2 if angle==90 else 3 if 90<angle and angle<180 else 4
+    # method1
+    # answer=1 if 0<angle and angle<90 else 2 if angle==90 else 3 if 90<angle and angle<180 else 4
+    # method2
+    answer=(angle//90)*2+int(angle%90>0)
     return answer
