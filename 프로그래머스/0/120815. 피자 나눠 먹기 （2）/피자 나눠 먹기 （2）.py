a288157bcdd4551e77d340b52c3523455e571324
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 00:24:14 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,8 +1,11 @@
 def solution(n):
-    i=1
-    while True:
-        if (6*i)%n==0:
-            r=i
-            break
-        i+=1
-    return r
+    # method1
+    # i=1
+    # while True:
+    #     if (6*i)%n==0:
+    #         r=i
+    #         break
+    #     i+=1
+    # method2
+    import math
+    return n//math.gcd(6,n)
