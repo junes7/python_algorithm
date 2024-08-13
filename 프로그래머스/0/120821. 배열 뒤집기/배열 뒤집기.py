0f066cef2e69030dc15ef24e2a45399dfbecb28f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-13 21:31:02 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,3 +1,6 @@
 def solution(num_list):
-    answer=num_list[::-1]
-    return answer
+    # answer=num_list[::-1]
+    ans=[]
+    for i in range(len(num_list)-1,-1,-1):
+        ans+=[num_list[i]]
+    return ans
