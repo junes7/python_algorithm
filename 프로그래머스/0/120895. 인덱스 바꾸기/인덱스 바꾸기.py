ba16f3ef32c032c81097b6dbad2b42037702ea49
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 19:38:40 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,7 +1,9 @@
 def solution(my_string, num1, num2):
-    # method1 swap
     l=list(my_string)
-    t=l[num1]
-    l[num1]=l[num2]
-    l[num2]=t
+    # method1 swap
+    # t=l[num1]
+    # l[num1]=l[num2]
+    # l[num2]=t
+    # method2
+    l[num1],l[num2]=l[num2],l[num1]
     return ''.join(l)
