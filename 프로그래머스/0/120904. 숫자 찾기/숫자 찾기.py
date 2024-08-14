9f4a3579023f6598a400a5c3cf673b80bf21761d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 18:24:18 +0900

    [level 0] Title: 숫자 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,3 +1,3 @@
 def solution(num, k):
-    answer=str(num).find(str(k))+1 if str(num).find(str(k))>-1 else -1
-    return answer
+    r=-1 if str(k) not in str(num) else str(num).find(str(k))+1
+    return r
