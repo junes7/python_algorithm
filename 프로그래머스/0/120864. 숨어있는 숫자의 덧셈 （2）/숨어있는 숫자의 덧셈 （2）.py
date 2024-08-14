113e88a7938f47485279793b80aa61380a62cd46
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 23:41:59 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,5 +1,5 @@
 def solution(my_string):
-    # method1
+    # method1 while 반복문과 isdigit
     # i,s,n=0,0,''
     # while i < len(my_string):
     #     if my_string[i].isdigit():
@@ -12,7 +12,7 @@ def solution(my_string):
     #             s+=int(n)
     #         n=''
     #     i+=1
-    # method2
+    # method2 isdigit과 split 함수
     s=''.join(i if i.isdigit() else ' ' for i in my_string)
     s=sum(map(int,s.split()))
     return s
