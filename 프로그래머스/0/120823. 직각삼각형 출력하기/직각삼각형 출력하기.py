518bd6835dc5d24b5c2a19a9af5819d8452417ca
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 08:42:36 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 15.86 ms, Memory: 7.57 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.py
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.py
@@ -1,11 +1,11 @@
 n=int(input(),10)
 # method1 double for statement
-# for i in range(1,n+1):
-#     for j in range(i):
-#         print('*',end='')
-#     print()
-# method2 single for statement
 for i in range(1,n+1):
-    print('*'*i)
+    for j in range(i):
+        print('*',end='')
+    print()
+# method2 single for statement
+# for i in range(1,n+1):
+#     print('*'*i)
 # method3 join function
 # print('\n'.join('*'*i for i in range(1,n+1)))
