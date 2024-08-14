7547770db931b3d465be5733cec9191ab1775f55
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 14:22:14 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,3 +1,8 @@
 def solution(my_string):
-    answer=my_string[::-1]
+    # method1
+    # answer=my_string[::-1]
+    # method2
+    answer=''
+    for i in range(len(my_string)-1,-1,-1):
+        answer+=my_string[i]
     return answer
