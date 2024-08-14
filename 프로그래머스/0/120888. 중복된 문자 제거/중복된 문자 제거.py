7ec8ea91ae5887f609474de3f3d92a917dcc74d9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 21:46:55 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,6 +1,9 @@
 def solution(my_string):
-    r=[]
-    for c in my_string:
-        if c not in r:
-            r+=[c]
-    return ''.join(r)
+    # method1 for 반복문으로 중복 제거
+    # r=''
+    # for c in my_string:
+    #     if c not in r:
+    #         r+=c
+    # method2 dictionary로 중복 제거
+    r=''.join(dict.fromkeys(my_string))
+    return r
