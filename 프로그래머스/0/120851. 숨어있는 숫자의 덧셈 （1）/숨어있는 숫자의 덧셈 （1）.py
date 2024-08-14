80ebb97a128f11ce2b54155d762334b0ff407c32
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 14:36:02 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,6 +1,11 @@
 def solution(my_string):
     r=0
+    # method1
+    # for c in my_string:
+    #     if 48<=ord(c) and ord(c)<=57:
+    #         r+=ord(c)%48
+    # method2
     for c in my_string:
-        if 48<=ord(c) and ord(c)<=57:
-            r+=ord(c)%48
+        if c.isnumeric():
+            r+=int(c)
     return r
