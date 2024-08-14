5f2aef285e3bc481179ca39788678f872dec405a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 19:05:42 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.89 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,8 +1,8 @@
 def solution(my_string):
-    # method1 isdigit함수
-    r=sorted([int(c) for c in my_string if c.isdigit()])
+    # method1 isdigit 함수
+    # r=sorted([int(c) for c in my_string if c.isdigit()])
     # method2 isnumeric 함수
-    
+    r=sorted([int(c) for c in my_string if c.isnumeric()])
     # method3 아스키 코드 값
     # r=sorted([int(c) for c in my_string if 48<=ord(c) and ord(c)<=57])
     return r
