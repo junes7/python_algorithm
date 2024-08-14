28b9ab12d43f97a2d31fa212d4dd6ef19b78f338
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 16:40:45 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -2,8 +2,11 @@ def solution(n):
     # method1
     # s=eval('+'.join(i for i in str(n)))
     # method2
-    s=0
-    while n>0:
-        s+=n%10
-        n//=10
+    # s=0
+    # while n>0:
+    #     s+=n%10
+    #     n//=10
+    # method3
+    s=sum(map(int,str(n)))
+    print(s)
     return s
