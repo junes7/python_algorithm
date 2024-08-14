34a13c506af18962ba68a0ebeb03774e4c5ae98f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-14 21:29:38 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,14 +1,24 @@
 def solution(numbers, direction):
-    r=[0]*len(numbers)
-    for i,n in enumerate(numbers):
-        if direction=='right':
-            if i==len(numbers)-1:
-                r[0]=numbers[i]
-            else:
-                r[i+1]=numbers[i] 
-        else:
-            if i==0:
-                r[len(numbers)-1]=numbers[i]
-            else:
-                r[i-1]=numbers[i]   
-    return r
+    # method1 인덱스 위치에 맞게 값 변경
+    # n=[0]*len(numbers)
+    # for i in range(len(numbers)):
+    #     if direction=='right':
+    #         if i==len(numbers)-1:
+    #             n[0]=numbers[i]
+    #         else:
+    #             n[i+1]=numbers[i] 
+    #     else:
+    #         if i==0:
+    #             n[len(numbers)-1]=numbers[i]
+    #         else:
+    #             n[i-1]=numbers[i]
+    # method2 slice 함수
+    # n=[numbers[-1]]+numbers[:-1] if direction=='right' else numbers[1:]+[numbers[0]]
+    # method3 rotate 함수
+    # from collections import deque
+    # n=deque(numbers)
+    # n.rotate(1) if direction=='right' else n.rotate(-1)
+    # n=list(n)
+    # method4 pop 함수
+    n=[numbers.pop()]+numbers if direction=='right' else numbers+[numbers.pop(0)]
+    return n
