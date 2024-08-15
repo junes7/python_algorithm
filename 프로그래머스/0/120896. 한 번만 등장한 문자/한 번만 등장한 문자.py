07ee939771fb186a93ceedcedcb3f72dc5cd33db
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 15:53:34 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,3 +1,9 @@
 def solution(s):
-    r=''.join(sorted([c for c in s if s.count(c)==1]))
+    # method1 count function
+    # r=''.join(sorted([c for c in s if s.count(c)==1]))
+    # method2 without count function
+    al=[0]*26
+    for c in s:
+        al[ord(c)-ord('a')]+=1
+    r=''.join(chr(ord('a')+i) for i,n in enumerate(al) if n==1)
     return r
