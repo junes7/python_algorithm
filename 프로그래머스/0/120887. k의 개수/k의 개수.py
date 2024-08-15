350b38d1fee56ff4f45d9794e37163b8bb487345
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 10:03:51 +0900

    [level 0] Title: k의 개수, Time: 94.00 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,3 +1,6 @@
 def solution(i, j, k):
-    r=sum([str(n).count(str(k)) for n in range(i,j+1)])
+    # method1 count function
+    # r=sum([str(n).count(str(k)) for n in range(i,j+1)])
+    # method2 double for statement without count function
+    r=sum([1 for n in range(i,j+1) for c in str(n) if c==str(k)])
     return r
