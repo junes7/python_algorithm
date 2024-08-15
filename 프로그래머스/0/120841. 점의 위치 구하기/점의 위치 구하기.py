876439434c686217a39c4d684ec8f28ab5fcb084
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 17:21:36 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,10 +1,10 @@
 def solution(dot):
     # method1 ternary operator
-    r=(1 if dot[1]>0 else 4) if dot[0]>0 else (2 if dot[1]>0 else 3)
+    # r=(1 if dot[1]>0 else 4) if dot[0]>0 else (2 if dot[1]>0 else 3)
     # method2 if~else
-    # r=0
-    # if dot[0]>0:
-    #     r=1 if dot[1]>0 else 4
-    # else:
-    #     r=2 if dot[1]>0 else 3
+    r=0
+    if dot[0]>0:
+        r=1 if dot[1]>0 else 4
+    else:
+        r=2 if dot[1]>0 else 3
     return r
