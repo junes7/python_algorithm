41505fea5522e91d10880b563d7018680b32d80f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 19:38:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,3 +1,9 @@
 def solution(numbers):
-    r=numbers.replace('zero','0').replace('one','1').replace('two','2').replace('three','3').replace('four','4').replace('five','5').replace('six','6').replace('seven','7').replace('eight','8').replace('nine','9')
+    r=numbers
+    # method1 replace function
+    # r=r.replace('zero','0').replace('one','1').replace('two','2').replace('three','3').replace('four','4').replace('five','5').replace('six','6').replace('seven','7').replace('eight','8').replace('nine','9')
+    # method2 for statement with replace function
+    num=["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for idx,eng in enumerate(num):
+        r=r.replace(eng,str(idx))
     return int(r)
