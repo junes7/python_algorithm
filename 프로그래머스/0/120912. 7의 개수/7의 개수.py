f34100c8bc4c90748d0df082ef6825dba85ef36b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 09:43:39 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,6 +1,6 @@
 def solution(array):
     # method1 without count
-    # r=sum([1 for n in array for i in str(n) if i=='7'])
+    r=sum([1 for n in array for i in str(n) if i=='7'])
     # method2 with count
-    r=str(array).count('7')
+    # r=str(array).count('7')
     return r
