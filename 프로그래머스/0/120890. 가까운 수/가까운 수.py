c2b0d336701495d6666c74f2ecddbe448e164ec8
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 17:06:06 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,6 +1,8 @@
 def solution(array, n):
-    arr=sorted(array)
-    d=[abs(n-i) for i in arr]
-    # print(d)
-    r=arr[d.index(min(d))]
+    # method1
+    # arr=sorted(array)
+    # d=[abs(n-i) for i in arr]
+    # r=arr[d.index(min(d))]
+    # method2 sorted function
+    r=sorted(array,key=lambda x:(abs(x-n),x))[0]
     return r
