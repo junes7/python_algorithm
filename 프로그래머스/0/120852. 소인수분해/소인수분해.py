10e7450082a2f038ae73fc2e1be4b87f69af84bc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 18:24:05 +0900

    [level 0] Title: 소인수분해, Time: 0.77 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,9 +1,19 @@
 def solution(n):
     r=[]
-    for i in range(2,n+1):
-        k=[]
-        if not n%i:
-            k=[j for j in range(2,i+1) if not i%j]
-            if len(k)==1:
-                r+=[i]
+    # method1 약수의 약수 길이로 소수 판단
+    # for i in range(2,n+1):
+    #     k=[]
+    #     if not n%i:
+    #         k=[j for j in range(2,i+1) if not i%j]
+    #         if len(k)==1:
+    #             r+=[i]
+    # method2 나누기 이용
+    d=2
+    while n>1:
+        if not n%d:
+            n/=d
+            if d not in r:
+                r+=[d]
+        else:
+            d+=1
     return r
