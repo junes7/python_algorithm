4597953a3a6b8ca651bd20a48c016232e22664d9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 14:23:31 +0900

    [level 0] Title: 합성수 찾기, Time: 0.20 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,3 +1,3 @@
 def solution(n):
-    r=len([len([i for i in range(1,k+1) if not k%i]) for k in range(4,n+1) if len([i for i in range(1,k+1) if not k%i])>=3])        
+    r=len([k for k in range(4,n+1) if len([i for i in range(1,k+1) if not k%i])>=3])
     return r
