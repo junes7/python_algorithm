6b94d804c3dfa6ac7e04659e15073a775640563a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 17:38:39 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,9 +1,12 @@
 def solution(my_string):
-    s=my_string.split()
-    r=int(s[0])
-    for i in range(1,len(s),2):
-        if s[i]=='+':
-            r+=int(s[i+1])
-        else:
-            r-=int(s[i+1])
+    # method1
+    # s=my_string.split()
+    # r=int(s[0])
+    # for i in range(1,len(s),2):
+    #     if s[i]=='+':
+    #         r+=int(s[i+1])
+    #     else:
+    #         r-=int(s[i+1])
+    # method2
+    r=eval(my_string)
     return r
