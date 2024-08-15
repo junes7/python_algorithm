bc1c92ef8734588315aa0d45f91ddfebd04eefd2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-15 12:27:39 +0900

    [level 0] Title: A로 B 만들기, Time: 1.48 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,3 +1,14 @@
 def solution(before, after):
-    r=1 if sorted(before)==sorted(after) else 0
+    # method1 sorted 정렬 함수
+    # r=1 if sorted(before)==sorted(after) else 0
+    # method2
+    b=list(before)
+    r=0
+    for i in after:
+        try:
+            del b[b.index(i)]
+        except:
+            r=0
+    if len(b)==0:
+        r=1
     return r
