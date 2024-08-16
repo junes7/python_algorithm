a76e86444d04a1d21a921d3ccd190d18db52b9f3
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-16 18:04:24 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,6 +1,6 @@
 def solution(sides):
-    # if longest side is s[-1] and if longest side is the rest
     # s=sorted(sides)
+    # if longest side is s[-1] and if longest side is the rest
     # (s[-1]-(s[-1]-s[0]))+(s[-1]+s[0]-s[-1]-1)
     r=2*min(sides)-1
     return r
