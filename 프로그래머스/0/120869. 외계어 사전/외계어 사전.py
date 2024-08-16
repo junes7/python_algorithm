e20b5a21ea5b69bcd66b5c8ae4c501fc2fe5b5d4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-16 18:48:45 +0900

    [level 0] Title: 외계어 사전, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,11 +1,18 @@
 def solution(spell, dic):
-    r=2
+    # method1 포함 횟수와 길이 비교
+    # r=2
+    # for c in dic:
+    #     s=0
+    #     for i in spell:
+    #         if i in c:
+    #             s+=1
+    #     if s==len(spell):
+    #         r=1
+    #         break
+    # method2 set함수 이용
+    r,s=2,set(spell)
     for c in dic:
-        s=0
-        for i in spell:
-            if i in c:
-                s+=1
-        if s==len(spell):
+        if not s-set(c):
             r=1
             break
     return r
