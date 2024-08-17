434e5789ae9d608dde4f3f3e38a67e8f67601d85
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-17 10:37:46 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/README.md
+++ 프로그래머스/0/120878. 유한소수 판별하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 유한소수 판별하기 - 120878 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.16 ms
+메모리: 10.1 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 17일 10:03:55
+2024년 08월 17일 10:37:43
 
 ### 문제 설명
 
--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,16 +1,24 @@
 def solution(a,b):
     p,r=[],1
     d=b//gcd(a,b)
-    print(d)
-    for i in range(2,d+1):
-        if d%i==0:
-            t=[]
-            for j in range(2,i+1):
-                if i%j==0:
-                    t+=[j]
-            if len(t)==1:
-                p+=[i]
-    print(p)
+    # method1 길이로 소수 판별
+    # for i in range(2,d+1):
+    #     if d%i==0:
+    #         t=[]
+    #         for j in range(2,i+1):
+    #             if i%j==0:
+    #                 t+=[j]
+    #         if len(t)==1:
+    #             p+=[i]
+    # method2 modulus로 소수 판별
+    k=2
+    while d>1:
+        if d%k==0:
+            if k not in p:
+                p+=[k]
+            d/=k
+        else:
+            k+=1
     for i in p:
         if i not in [2,5]:
             r=2
