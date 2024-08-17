ed8fbe58882c5092239e8521d96a9f625a818aaa
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-17 12:00:36 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.26 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,17 +1,15 @@
 def solution(num, total):
     r=[]
+    # 중간값
     q=total//num
     # method1 중간값을 시작값으로 두고 순차적으로 줄이며 일치하는 합 찾기
-    # while True:
-    #     r=[]
-    #     for i in range(num):
-    #         r+=[q+i]
-    #     print(q,sum(r))
-    #     if len(r)==num and sum(r)==total:
-    #         break
-    #     q-=1
-    # method2 차이로 시작 값 구하기
-    r=[q+i for i in range(num)]
-    d=(sum(r)-total)//num if sum(r)-total>0 else sum(r)-total
-    r=[(q-d)+i for i in range(num)]
+    while True:
+        r=[q+i for i in range(num)]
+        if len(r)==num and sum(r)==total:
+            break
+        q-=1
+    # method2 합의 차이로 정확한 시작 값 구하기
+    # r=[q+i for i in range(num)]
+    # d=(sum(r)-total)//num if sum(r)-total>0 else sum(r)-total
+    # r=[(q-d)+i for i in range(num)]
     return r
