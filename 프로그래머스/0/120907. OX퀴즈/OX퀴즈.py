247ab6a913e372532ed9800426923861de641444
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-17 15:25:25 +0900

    [level 0] Title: OX퀴즈, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -2,10 +2,13 @@ def solution(quiz):
     r=[]
     for q in quiz:
         e=q.split()
-        if e[1]=='+' and int(e[0])+int(e[2])==int(e[-1]):
-            r+=['O']
-        elif e[1]=='-' and int(e[0])-int(e[2])==int(e[-1]):
-            r+=['O']
-        else:
-            r+=['X']  
+        # method1 인덱스 사용
+        # if e[1]=='+' and int(e[0])+int(e[2])==int(e[-1]):
+        #     r+=['O']
+        # elif e[1]=='-' and int(e[0])-int(e[2])==int(e[-1]):
+        #     r+=['O']
+        # else:
+        #     r+=['X']
+        # method2 eval 함수 사용
+        r+=['O' if eval(e[1].join([e[0],e[2]]))==int(e[-1]) else 'X']
     return r
