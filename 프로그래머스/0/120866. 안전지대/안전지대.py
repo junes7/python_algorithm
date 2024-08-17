5ace535dd0408f101ab244bfa2e688b1a579c9f3
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-17 19:57:48 +0900

    [level 0] Title: 안전지대, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,24 @@
+def solution(board):
+    r=0
+    n=len(board)
+    # 위험 지역 2로 변환
+    for i in range(n):
+        for j in range(n):
+            if board[i][j]==1:
+                if n==1:
+                    sx,ex,sy,ey=0,0,0,0
+                elif n>1:
+                    sx=0 if i==0 else i-1 if i==n-1 else i-1
+                    ex=i+1 if i==0 else i if i==n-1 else i+1
+                    sy=0 if j==0 else j-1 if j==n-1 else j-1
+                    ey=j+1 if j==0 else j if j==n-1 else j+1          
+                for k in range(sx,ex+1):
+                    for l in range(sy,ey+1):
+                        if board[k][l]!=1:
+                            board[k][l]=2
+    # 안전 지역 세기       
+    for i in range(n):
+        for j in range(n):
+            if not board[i][j]:
+                r+=1
+    return r
