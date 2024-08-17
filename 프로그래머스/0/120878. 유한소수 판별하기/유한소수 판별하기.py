aa571f7cf47cd61256ea70403863b30679d97831
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-17 10:42:27 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,7 +1,7 @@
 def solution(a,b):
     p,r=[],1
     d=b//gcd(a,b)
-    # method1 길이로 소수 판별
+    # method1 길이로 소인수 판별
     # for i in range(2,d+1):
     #     if d%i==0:
     #         t=[]
@@ -10,7 +10,7 @@ def solution(a,b):
     #                 t+=[j]
     #         if len(t)==1:
     #             p+=[i]
-    # method2 modulus로 소수 판별
+    # method2 modulus로 소인수 판별
     k=2
     while d>1:
         if d%k==0:
