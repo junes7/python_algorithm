c0ab8a0b2fdb083882ed5b2020290a257dbf0dda
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-18 15:48:51 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
@@ -1,3 +1,9 @@
 def solution(arr):
-    r=sum(arr)/len(arr)
+    # method1 with sum function
+    # r=sum(arr)/len(arr)
+    # method2 without sum function
+    s=0
+    for i in arr:
+        s+=i
+    r=s/len(arr)
     return r
