5be46434d9324551a0df0644b1fe3b88667f4ffd
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-18 12:56:55 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,5 +1,8 @@
 def solution(n):
-    r=0
-    for c in str(n):
-        r+=int(c)
+    # method1 without sum function
+    # r=0
+    # for c in str(n):
+    #     r+=int(c)
+    # method2 with sum function
+    r=sum([int(c) for c in str(n)])
     return r
