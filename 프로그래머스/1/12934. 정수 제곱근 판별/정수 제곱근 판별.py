2f10f462854d8afab0ed656654d0e4046399b94c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-18 16:11:34 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,4 +1,7 @@
 def solution(n):
     t=n**0.5
-    r=(t+1)**2 if int(t)==t else -1
+    # method1 int함수 사용
+    # r=(t+1)**2 if int(t)==t else -1
+    # method2 modulus 사용
+    r=(t+1)**2 if t%1==0 else -1
     return r
