5e3c83f100a7d82dc7122a60ad5ecb5acf141861
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-18 10:13:30 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,10 @@
+def solution(dots):
+    l=len(dots)
+    r=[]
+    # 두 점을 이은 선분의 기울기 계산
+    for i in range(l):
+        for j in range(i+1,l):
+            r+=[(dots[j][0]-dots[i][0])/(dots[j][1]-dots[i][1])]
+    # 평행 선분 찾기 위해 동일 기울기 카운트
+    ans=[1 if r[i]==r[-1-i] else 0 for i in range(len(r)//2)]
+    return max(ans)
