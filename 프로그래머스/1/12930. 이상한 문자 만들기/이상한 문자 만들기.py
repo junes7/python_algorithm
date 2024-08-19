b97c2d9c9bf52a2740974dcd2e6027573b0ba24b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-19 15:41:32 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,14 +1,12 @@
 def solution(s):
     r=''
-    s=list(map(str,s.split(' ')))
+    s=s.split(' ')
     for i,c in enumerate(s):
-        c=list(c)
         for j in range(len(c)):
-            if not j%2:
-                if 97<=ord(c[j]) and ord(c[j])<=122:
-                    c[j]=chr(ord(c[j])-32)
+            if not j&1:
+                r+=c[j].upper()
             else:
-                if 65<=ord(c[j]) and ord(c[j])<=90:
-                    c[j]=chr(ord(c[j])+32)
-        s[i]=''.join(c)   
-    return ' '.join(s)
+                r+=c[j].lower()
+        if i<len(s)-1:
+            r+=' '
+    return r
