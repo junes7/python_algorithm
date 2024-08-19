956822cc95fcec0227e49fb41b2b32d0dd29473e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-19 22:45:00 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,8 +1,15 @@
 def solution(number):
-    r,l=0,len(number)
-    for i in range(l-2):
-        for j in range(i+1,l-1):
-            for k in range(j+1,l):
-                if number[i]+number[j]+number[k]==0:
-                    r+=1
+    # method1 triple for statement
+    # r,l=0,len(number)
+    # for i in range(l-2):
+    #     for j in range(i+1,l-1):
+    #         for k in range(j+1,l):
+    #             if number[i]+number[j]+number[k]==0:
+    #                 r+=1
+    # method2 with combinations function
+    r=0
+    from itertools import combinations
+    for i in combinations(number,3):
+        if sum(i)==0:
+            r+=1
     return r
