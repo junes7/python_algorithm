c238a2e44b7e1ac6a1283f6fcceb413276c27b28
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-20 19:23:52 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,8 +1,6 @@
 def solution(cards1, cards2, goal):
-    import pandas as pd
-    r=[]
-    idx=[-1]*2
-    for i,c in enumerate(goal):
+    r,idx=[],[-1]*2
+    for c in goal:
         if c in cards1:
             idx[0]+=1
             r+=[cards1[idx[0]]]
