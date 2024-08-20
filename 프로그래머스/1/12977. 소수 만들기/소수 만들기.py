2020f6992c9eb6f1de760fa4c0ffbcb71d1565dc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-20 21:48:30 +0900

    [level 1] Title: 소수 만들기, Time: 684.32 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,14 @@
+def solution(nums):
+    r=[]
+    from itertools import combinations
+    for i in combinations(nums,3):
+        n,d,t=sum(i),2,[]
+        while n>1:
+            if not n%d:
+                t+=[d]
+                n//=d
+            else:
+                d+=1
+        if len(t)==1:
+            r+=[n]
+    return sum(r)
