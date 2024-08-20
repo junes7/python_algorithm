29a748aea45f503071899b6701671952ffcceacc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-20 16:31:38 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,5 +1,3 @@
 def solution(n, arr1, arr2):
-    r=[]
-    for i in range(n):
-        r+=[str(bin(arr1[i]|arr2[i])[2:]).zfill(n).replace('1','#').replace('0',' ')]
+    r=[str(bin(arr1[i]|arr2[i])[2:]).zfill(n).replace('1','#').replace('0',' ') for i in range(n)]
     return r
