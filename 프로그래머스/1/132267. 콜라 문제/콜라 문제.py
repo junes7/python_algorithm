4742aeb9af91f42564600c2052dd1dde18018e80
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-20 17:48:58 +0900

    [level 1] Title: 콜라 문제, Time: 0.46 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,6 @@
 def solution(a, b, n):
     r=0
-    while n>=2:
-        if n//a==0:
-            break
-        r+=(n//a)*b
-        n=(n//a)*b+n%a
+    while n//a:
+            r+=(n//a)*b
+            n=(n//a)*b+n%a
     return r
