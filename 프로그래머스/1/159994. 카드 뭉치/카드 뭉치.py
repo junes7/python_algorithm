be00b4923d90bfa6cd846323bcf988c09999a5a4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-20 19:36:36 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,10 +1,21 @@
 def solution(cards1, cards2, goal):
-    r,idx=[],[-1]*2
+    # method1 카드 요소 순서대로 문장 조합 후 goal문장과 비교
+    # r,idx=[],[-1]*2
+    # for c in goal:
+    #     if c in cards1:
+    #         idx[0]+=1
+    #         r+=[cards1[idx[0]]]
+    #     elif c in cards2:
+    #         idx[1]+=1
+    #         r+=[cards2[idx[1]]]
+    # return "Yes" if goal==r else "No"
+    # method2 del함수 이용
+    r="Yes"
     for c in goal:
-        if c in cards1:
-            idx[0]+=1
-            r+=[cards1[idx[0]]]
-        elif c in cards2:
-            idx[1]+=1
-            r+=[cards2[idx[1]]]
-    return "Yes" if goal==r else "No"
+        if len(cards1)>0 and cards1[0]==c:
+            del cards1[0]
+        elif len(cards2)>0 and cards2[0]==c:
+            del cards2[0]
+        else:
+            r="No"
+    return r  
