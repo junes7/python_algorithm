4da4eb90d7d3f272899a1a685d66e18d09f7bc19
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-20 15:34:35 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.73 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,3 +1,9 @@
-def solution(s):
-    answer = []
-    return answer
+def solution(s): 
+    r,t=[],{}
+    for i,c in enumerate(s):
+        if c not in t:   
+            r+=[-1]
+        else:   
+            r+=[i-t[c]]
+        t[c]=i
+    return r
