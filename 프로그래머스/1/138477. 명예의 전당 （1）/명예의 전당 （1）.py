0be2c7600ffe75a31caa2af6dbcb1148254cf717
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-20 18:41:22 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.95 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,13 @@
+def solution(k, score):
+    r,t=[],[]
+    for i,s in enumerate(score):
+        if i<k:
+            t+=[s]
+        else:
+            for j,n in enumerate(t):
+                if s>n:
+                    t[j]=s
+                    break
+        t.sort()
+        r+=[min(t)]
+    return r
