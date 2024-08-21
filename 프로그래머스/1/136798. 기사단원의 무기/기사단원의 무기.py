9b69ef61bc2a157f411de6698d98f0803890dab1
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-21 14:48:06 +0900

    [level 1] Title: 기사단원의 무기, Time: 928.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,12 +1,12 @@
 def solution(number, limit, power):
+    # 1의 약수는 1 이므로
     r=1
     for n in range(2,number+1):
-        # 에라토스테네스의 체 이용
         t=[]
         for i in range(1,int(n**0.5)+1):
             if not n%i:
                 t+=[i]
                 if i**2!=n:
-                    t+=[n//i]
+                    t+=[n//i]           
         r+=power if len(t)>limit else len(t)
     return r
