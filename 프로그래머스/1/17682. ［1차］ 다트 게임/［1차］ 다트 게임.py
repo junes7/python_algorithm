03dedafaa2e9260def1205f720e84e3c4d998afd
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-22 11:44:02 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,18 +1,31 @@
 def solution(dartResult):
-    r=[]
-    for i,c in enumerate(dartResult):
-        if c in ['S','D','T']:
-            if dartResult[i-2] in ['0','1','2','3']:
-                n=int(dartResult[i-2:i])
-            else:
-                n=int(dartResult[i-1])
-            r+=[{'S':n**1,'D':n**2,'T':n**3}.get(c)]
-        if c=='#':
-            r[-1]=-1*r[-1]
-        elif c=='*':
-            if len(r)==1:
-                r[-1]=r[-1]*2
-            else:
-                r[-1]=r[-1]*2
-                r[-2]=r[-2]*2
-    return sum(r)
+    # method1 포함 여부와 딕셔너리를 이용해 구현
+    # r=[]
+    # for i,c in enumerate(dartResult):
+    #     if c in ['S','D','T']:
+    #         if dartResult[i-2] in ['0','1','2','3']:
+    #             n=int(dartResult[i-2:i])
+    #         else:
+    #             n=int(dartResult[i-1])
+    #         r+=[{'S':n**1,'D':n**2,'T':n**3}[c]]
+    #     if c=='#':
+    #         r[-1]=-1*r[-1]
+    #     elif c=='*':
+    #         if len(r)==1:
+    #             r[-1]=r[-1]*2
+    #         else:
+    #             r[-1]=r[-1]*2
+    #             r[-2]=r[-2]*2
+    #     r=sum(r)
+    # method2 re라이브러리의 compile과 findall함수 이용
+    import re
+    b={'S':1,'D':2,'T':3}
+    s={'':1,'*':2,'#':-1}
+    p=re.compile('(\d+)([SDT])([#*]?)')
+    dart=p.findall(dartResult)
+    for i in range(len(dart)):
+        if dart[i][2]=='*' and i>0:
+            dart[i-1]*=2
+        dart[i]=int(dart[i][0])**b[dart[i][1]]*s[dart[i][2]]
+    r=sum(dart)
+    return r
