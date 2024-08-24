345da12293edf138f119321ef1b19aa1cf6ce44a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-24 17:44:51 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -5,9 +5,13 @@ def solution(board, h, w):
     dh=[0,1,-1,0]
     dw=[1,0,0,-1]
     for i in range(4):
+        # 우상하좌 순서로 좌표 이동
         h_check=h+dh[i]
         w_check=w+dw[i]
+        # h_check가 0 이상 n 미만이고 w_check가 0 이상 n 미만이라면
         if (0<=h_check and h_check<n) and (0<=w_check and w_check<n):
+            # board[h][w]와 board[h_check][w_check]의 값이 동일하다면
             if board[h][w]==board[h_check][w_check]:
+                # count값 1 증가
                 cnt+=1
     return cnt
