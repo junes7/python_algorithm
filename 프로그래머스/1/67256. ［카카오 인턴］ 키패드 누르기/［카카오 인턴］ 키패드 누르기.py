0c82f3c140a07d2166b63715631472d29a8df0bc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-24 13:18:48 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -2,6 +2,7 @@ def solution(numbers, hand):
     # 검색을 빠르게 하기 위해 딕셔너리에 좌표값 설정
     kpad={1:(0,0),2:(0,1),3:(0,2),4:(1,0),5:(1,1),6:(1,2),7:(2,0),8:(2,1),9:(2,2),0:(3,1)}
     r=''
+    # 초기 좌우 좌표값 설정
     lx,ly,rx,ry=3,0,3,2
     for n in numbers:
         if n in [1,4,7]:
@@ -12,6 +13,7 @@ def solution(numbers, hand):
             r+='R'
         else:
             nx,ny=kpad[n]
+            # 상하좌우로만 이동 가능하니 맨하탄 거리(Manhattan Distance)이용
             dl=abs(lx-nx)+abs(ly-ny)
             dr=abs(rx-nx)+abs(ry-ny)
             if dl==dr:
