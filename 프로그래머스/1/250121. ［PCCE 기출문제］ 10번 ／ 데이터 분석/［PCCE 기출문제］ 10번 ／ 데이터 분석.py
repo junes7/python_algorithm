c5e31973a01e4f38e88463ecdc94f6c044e81d41
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-24 18:39:12 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.23 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -3,7 +3,9 @@ def solution(data, ext, val_ext, sort_by):
     # 기준 열 해시 코드
     th={'code':0,'date':1,'maximum':2,'remain':3}
     for i,d in enumerate(data):
+        # 기준 열의 값이 기준 값보다 작을 때 배열에 추가
         if d[th[ext]]<val_ext:    
             r+=[d]
+    # 정렬 기준으로 데이터 재정렬
     r.sort(key=lambda x:x[th[sort_by]])
     return r
