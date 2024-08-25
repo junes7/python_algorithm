a54c064936f1821104240a3572cdf093adfc4a8d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-25 15:34:53 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.25 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/README.md
+++ 프로그래머스/1/118666. 성격 유형 검사하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 성격 유형 검사하기 - 118666 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.24 ms
+메모리: 10.2 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 25일 15:26:04
+2024년 08월 25일 15:34:51
 
 ### 문제 설명
 
--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -4,15 +4,18 @@ def solution(survey, choices):
     # 4개 성격 유형 지표 리스트 선언
     b=['R','T','C','F','J','M','A','N']
     t=dict(zip(b,[0]*8))
+    # 문자열 배열과 선택한 선택지 리스트를 zip함수로 묶고 반복문을 돌면서 성격 유형 지표의 값을 채워줍니다.
     for i,c in zip(survey,choices):
         if c<4:
             t[i[0]]+=4-c
         elif c>4:
             t[i[1]]+=c-4
+    # 두 유형별 지표 리스트를 돌면서
     for i in range(0,len(b)//2):
-        # 값이 동일하거나 오른쪽 값이 작을 때
+        # 값이 동일하거나 왼쪽 값이 클 때 왼쪽 값 문자열에 추가
         if t[b[2*i]]>=t[b[2*i+1]]:
             r+=b[2*i]
+        # 값이 오른쪽 값이 클 때 오른쪽 값 문자열에 추가
         elif t[b[2*i]]<t[b[2*i+1]]:
             r+=b[2*i+1]
     return r
