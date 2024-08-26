fa962bbeab722b2c4e8ba11336b11d70db375a68
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-26 20:55:46 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,16 +1,25 @@
 def solution(wallpaper):
     w=wallpaper
-    lx,ly,rx,ry=50,50,0,0
+    # method1 without min,max function
+    # lx,ly,rx,ry=50,50,0,0
+    # for i in range(len(w)):
+    #     for j in range(len(w[0])):
+    #         if w[i][j]=='#':
+    #             if lx>i:
+    #                 lx=i
+    #             if ly>j:
+    #                 ly=j
+    #             if rx<i:
+    #                 rx=i
+    #             if ry<j:
+    #                 ry=j
+    # r=[lx,ly,rx+1,ry+1]
+    # method2 with min,max function
+    l,r=[],[]
     for i in range(len(w)):
         for j in range(len(w[0])):
             if w[i][j]=='#':
-                if lx>i:
-                    lx=i
-                if ly>j:
-                    ly=j
-                if rx<i:
-                    rx=i
-                if ry<j:
-                    ry=j
-    r=[lx,ly,rx+1,ry+1]
+                l+=[i]
+                r+=[j]
+    r=[min(l),min(r),max(l)+1,max(r)+1]
     return r
