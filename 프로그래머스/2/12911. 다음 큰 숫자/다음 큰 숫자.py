688b4a0ef6a284d570754deb061430da7b581200
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-27 14:14:53 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -3,7 +3,10 @@ def solution(n):
     ocnt=str(bin(n)).count('1')
     r=n
     while True:
+        # n값 1 증가
         r+=1
+        # 증가된 값을 이진수로 치환했을 때 1의 개수와
+        # n을 이진수로 치환했을 때 1의 개수가 일치할 때 루프를 빠져나옵니다.
         if str(bin(r)).count('1')==ocnt:
             break
     return r
