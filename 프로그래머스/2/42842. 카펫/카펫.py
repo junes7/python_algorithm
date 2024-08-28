4ddabe458e3b5902eb0bae9bea2125724175e85f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-28 15:20:38 +0900

    [level 2] Title: 카펫, Time: 117.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,14 @@
 def solution(brown, yellow):
-    answer = []
-    total = brown + yellow
-    for col in range(3, total+1):
-        row = total//col
-        if row * col == total and row >= col and (row-2) * (col-2) == yellow:
-            answer.append(row)
-            answer.append(col)
-            break
-    return answer
+    r=[]
+    # 카펫의 총 크기
+    total=brown+yellow
+    # 노란 격자 위 아래로 갈색 격자가 존재하기 때문에 column의 최소 개수는 3입니다.
+    # 3부터 총 크기까지 반복문을 돌면서
+    for col in range(3,total+1):
+        row=total//col
+        # 너비*높이가 카펫 크기와 동일하고 너비가 높이보다 크거나 같을 때 
+        # 맨 위, 맨 아래 2줄, 맨 앞, 맨 뒤 2줄을 빼면 노란색 격자가 되기에
+        # (너비-2)*(높이-2)==노란색 격자 크기가 되어야 합니다.
+        if row*col==total and row>=col and (row-2)*(col-2)==yellow:
+            r+=[row,col]
+    return r
