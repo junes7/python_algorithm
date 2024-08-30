82f8d4b7b75cb9a836ac9f85e0f9c1e5880894dc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-30 23:15:31 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,14 @@
 def solution(clothes):
     r,cl=1,{}
+    # 옷 종류와 의상 이름 리스트의 딕셔너리 생성
     # c는 의상 이름, t는 의상 종류
     for c,t in clothes:
         if cl.get(t)==None:
             cl[t]=[c]
         else:
             cl[t]+=[c]
+    # 옷 종류 길이를 각각 곱해줍니다.
     for t in cl:
         r*=len(cl[t])+1
+    # 결과값에서 1을 빼주어 조합의 수를 구해줍니다.
     return r-1
