ced42b6d9b38a9380ae1eb755acae8a9d8255040
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-30 22:42:29 +0900

    [level 2] Title: n^2 배열 자르기, Time: 50.49 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390#) 
 
 ### 성능 요약
 
-메모리: 18.5 MB, 시간: 48.19 ms
+메모리: 18.4 MB, 시간: 50.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 30일 22:37:06
+2024년 08월 30일 22:42:27
 
 ### 문제 설명
 
--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -3,7 +3,14 @@ def solution(n, left, right):
     n의 범위가 10^7이기 때문에 범위대로 다 값을 넣으려고 하면 시간 초과가 발생합니다.
     범위를 left에서 right사이로 줄여 주어야 시간 초과가 발생 안 합니다.
     '''
+    # 리스트 범위 설정
     r=[0]*(right-left+1)
+    # 왼쪽에서 오른쪽까지 돌면서
     for i in range(left,right+1):
+        '''
+        몫과 나머지 중 가장 큰 값에 1을 더하면
+        n*n행렬을 한 줄로 만들었을 때 범위의 값이 나오게 됩니다.
+        인덱스는 0부터 시작하기에 left를 빼주어 shift해주었습니다.
+        '''
         r[i-left]=max(i//n,i%n)+1
     return r
