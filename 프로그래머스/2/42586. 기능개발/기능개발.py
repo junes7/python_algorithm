a44ce4c61c9d39171c5f3235f6acc1c2dac9adef
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-08-31 15:39:21 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,3 +1,19 @@
 def solution(progresses, speeds):
-    answer = []
-    return answer
+    th,t=0,{}
+    # 작업 진도와 작업 속도 배열의 개수가 같으므로 zip함수로 묶어주었습니다.
+    for p,s in zip(progresses,speeds):
+        # 실수로 계산했을 때 값이 몫보다 크다면 몫에 1을 더해줍니다.
+        if (100-p)/s>(100-p)//s:
+            d=(100-p)//s+1
+        # 실수로 계산했을 때 값이 몫과 같다면 그대로 몫을 넣어줍니다.
+        else:
+            d=(100-p)//s
+        # 계산한 날짜가 기준값보다 작거나 같을 때 기준값을 키로 카운트해줍니다.
+        if d<=th:
+            t[th]+=1
+        # 계산한 날짜가 기준값보다 클 때 기준값을 계산 날짜로 변경하고 계산 날짜를 키로서 해시에 추가해줍니다.
+        else: # d>th
+            th=d
+            t[d]=1
+    r=list(t.values())
+    return r
