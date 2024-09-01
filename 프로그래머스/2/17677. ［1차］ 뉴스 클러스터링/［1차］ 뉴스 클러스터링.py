2e6329c5ec35d57e3d7b36eda27232d55e42a764
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-09-01 18:31:15 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 8.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -2,14 +2,20 @@ def solution(str1, str2):
     r=0
     s1,s2=[],[]
     no,deno=0,0
+    # 두 글자씩 끊어서 다중집합 원소 생성, 영문자 쌍만 유효
     for i in range(0,len(str1)-1):
+        # 영문자 쌍인지 확인
         if str1[i:i+2].isalpha():
             s1+=[str1[i:i+2].lower()]
     for j in range(0,len(str2)-1):
+        # 영문자 쌍인지 확인
         if str2[j:j+2].isalpha():
             s2+=[str2[j:j+2].lower()]
+    # 중복 요소 제거해 합친 후
     for e in set(s1+s2):
+        # 각 집합 별 요소 개수 센후 최소값은 분수, 최대값은 분모
         no+=min(s1.count(e),s2.count(e))
         deno+=max(s1.count(e),s2.count(e))
+    # 유사도 값 계산
     r=int(no/deno*65536) if deno else 65536
     return r
