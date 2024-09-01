16f9513733e7e28aa2104978b52d15086ec668a1
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-09-01 23:34:34 +0900

    [level 2] Title: 전화번호 목록, Time: 118.52 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,13 @@
 def solution(phone_book):
-    answer = True
+    r=True
+    # 전화번호부 오름차순 정렬
     phone_book.sort()
+    # 전화번호부 리스트를 돌면서
     for i in range(1, len(phone_book)):
-        L = len(phone_book[i-1])
-        if phone_book[i-1][:L] == phone_book[i][:L]:
-            return False
-    return answer
+        # 이전 값 접두어 길이로 설정
+        L=len(phone_book[i-1])
+        # 이전 전화번호랑(접두어랑) 현재 전화번호 접두어 길이만큼 비교했을 때 같으면 False넣어주고 반복문 빠져나온다.
+        if phone_book[i-1][:L]==phone_book[i][:L]:
+            r=False
+            break
+    return r
