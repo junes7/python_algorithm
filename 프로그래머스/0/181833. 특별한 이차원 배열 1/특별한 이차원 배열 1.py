f9997f1fc054fdbde1606bdece95cf552c31f592
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-09-21 23:52:05 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.46 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,3 +1,3 @@
 def solution(n):
-    answer = [[1 if i==j else 0 for j in range(n)] for i in range(n)]
-    return answer
+    r=[[1 if i==j else 0 for j in range(n)] for i in range(n)]
+    return r
