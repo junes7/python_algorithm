9c11d733cc47d782f01e81937372796487276ded
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-09-21 23:51:33 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,6 +1,6 @@
 def solution(myString, pat):
     # method1
-    # answer=0 if myString.lower().find(pat.lower())==-1 else 1
+    r=0 if myString.lower().find(pat.lower())==-1 else 1
     # method2
-    r=int(pat.lower() in myString.lower())
+    # r=int(pat.lower() in myString.lower())
     return r
