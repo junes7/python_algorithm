a1c3e483125913b381d98d5380d89cb00a6210e5
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-09-24 23:58:23 +0900

    [level 0] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
@@ -1,11 +1,11 @@
 def solution(strArr):
     # method1
-    # answer=[]
-    # for i in range(len(strArr)):
-    #     if i%2==0:
-    #         answer.append(strArr[i].lower())
-    #     else:
-    #         answer.append(strArr[i].upper())
+    r=[]
+    for i in range(len(strArr)):
+        if i%2==0:
+            r.append(strArr[i].lower())
+        else:
+            r.append(strArr[i].upper())
     # method2
-    answer=[strArr[i].lower() if i%2==0 else strArr[i].upper() for i in range(len(strArr))]
-    return answer
+    # answer=[strArr[i].lower() if i%2==0 else strArr[i].upper() for i in range(len(strArr))]
+    return r
