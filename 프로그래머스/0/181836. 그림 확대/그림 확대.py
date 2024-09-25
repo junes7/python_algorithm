8cac61dc7a4a6d6dece3d96418993b155ed7a9a2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-09-25 23:59:41 +0900

    [level 0] Title: 그림 확대, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -1,14 +1,14 @@
 def solution(p, k):
     # method1
-    answer = []
+    r=[]
     for i in range(len(p)):
         t=''
         for j in range(len(p[i])):
             t+=p[i][j]*k
         for _ in range(k):
-            answer.append(t)
+            r.append(t)
     # method2
-    answer=[]
+    r=[]
     for i in range(len(p)):
-        answer+=[p[i].replace('.','.'*k).replace('x','x'*k)]*k
-    return answer
+        r+=[p[i].replace('.','.'*k).replace('x','x'*k)]*k
+    return r
