fe4e40761d48b6d5fd7c464cd479690f6d274ae9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-09-25 23:58:00 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,5 +1,5 @@
 def solution(a, b, c, d):
-    answer=0
+    # answer=0
     # method1
     # d_list=sorted([a,b,c,d])
     # print(d_list)
@@ -20,19 +20,25 @@ def solution(a, b, c, d):
     # else:
     #     answer=d_list[0]
     # method2
+    # l=sorted([a,b,c,d])
+    # c=[l.count(t) for t in l]
+    # if max(c)==4:
+    #     answer=1111*l[0]
+    # elif max(c)==3:
+    #     answer=(10*l[c.index(3)]+l[c.index(1)])**2
+    # elif max(c)==2:
+    #     if min(c)==1:
+    #         answer=eval('*'.join(str(l[i]) for i,x in enumerate(c) if x==1))
+    #     else:
+    #         answer=(max(l)+min(l))*abs(max(l)-min(l))
+    # else:
+    #     answer=l[0]
+    # method3
     l=sorted([a,b,c,d])
     c=[l.count(t) for t in l]
-    if max(c)==4:
-        answer=1111*l[0]
-    elif max(c)==3:
-        print(c.index(1),c.index(3))
-        answer=(10*l[c.index(3)]+l[c.index(1)])**2
-    elif max(c)==2:
-        if min(c)==1:
-            answer=eval('*'.join(str(l[i]) for i,x in enumerate(c) if x==1))
-        else:
-            answer=(max(l)+min(l))*abs(max(l)-min(l))
-    else:
-        answer=l[0]
-    
-    return answer
+    r={4:1111*l[0],
+            3:(10*l[c.index(max(c))]+l[c.index(min(c))])**2,
+            2:eval('*'.join(str(l[i]) for i,x in enumerate(c) if x==1))
+            if min(c)==1 else max(l)**2-min(l)**2,
+            1:l[0]}.get(max(c),0)                                                                     
+    return r
