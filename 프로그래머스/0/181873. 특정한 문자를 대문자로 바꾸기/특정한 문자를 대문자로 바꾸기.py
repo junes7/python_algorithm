3ffce5ff75b36b050e8c0087d9e129dac06c5db8
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-09-27 23:56:31 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
@@ -1,3 +1,3 @@
 def solution(my_string, alp):
-    answer = ''.join(c.upper() if c==alp else c for c in my_string)
-    return answer
+    r=''.join(c.upper() if c==alp else c for c in my_string)
+    return r
