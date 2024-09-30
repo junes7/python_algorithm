de26b7aa7f0ad4bcc73d72e978d0004b17c8b8a4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-09-30 23:52:44 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 15.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -2,5 +2,5 @@ def solution(my_string, queries):
     for s,e in queries:
         temp=list(my_string[s:e+1])
         my_string=my_string[:s]+''.join(c for c in temp[::-1])+my_string[e+1:]
-    answer=my_string
-    return answer
+    r=my_string
+    return r
