38b7cd4c6aa2aeb7e736c48ce57dff3bea89777c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-02 23:51:22 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,3 +1,3 @@
 def solution(my_string, is_prefix):
-    answer=1 if is_prefix in [my_string[0:1+i] for i in range(len(my_string))] else 0
-    return answer
+    r=1 if is_prefix in [my_string[0:1+i] for i in range(len(my_string))] else 0
+    return r
