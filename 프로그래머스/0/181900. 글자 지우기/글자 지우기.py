ea5ffcf1b1db29103db45ddc9e8991ce95e18658
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-03 23:50:39 +0900

    [level 0] Title: 글자 지우기, Time: 0.04 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,7 +1,7 @@
 def solution(my_string, indices):
     # method1
-    # idx_set=set([i for i in range(len(my_string))])
-    # answer=''.join(my_string[i] for i in sorted(idx_set.difference(set(indices))))
+    idx_set=set([i for i in range(len(my_string))])
+    r=''.join(my_string[i] for i in sorted(idx_set.difference(set(indices))))
     # method2
-    r=''.join(my_string[i] for i in range(len(my_string)) if i not in indices)
+    # r=''.join(my_string[i] for i in range(len(my_string)) if i not in indices)
     return r
