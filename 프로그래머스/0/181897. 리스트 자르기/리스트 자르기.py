9f4f3109603c2b8ebe8e0d1cc8b4b2e5708690c1
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-03 23:51:21 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -1,4 +1,5 @@
 def solution(n, slicer, num_list):
     a,b,c=slicer
-    answer={2:num_list[a:],3:num_list[a:b+1],4:num_list[a:b+1:c]}.get(n,num_list[:b+1])
-    return answer
+    print(num_list)
+    r={2:num_list[a:],3:num_list[a:b+1],4:num_list[a:b+1:c]}.get(n,num_list[:b+1])
+    return r
