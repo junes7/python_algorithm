d96f3318df10ead89f2c5aea75ae36dc1f616974
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-04 22:40:00 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,10 +1,10 @@
 def solution(num_list):
     # method1
-    # r=-1
-    # for i,n in enumerate(num_list):
-    #     if n<0:
-    #         r=i
-    #         break
+    r=-1
+    for i,n in enumerate(num_list):
+        if n<0:
+            r=i
+            break
     # method2
-    r=([i for i,n in enumerate(num_list) if n<0]or[-1])[0]
+    # r=([i for i,n in enumerate(num_list) if n<0]or[-1])[0]
     return r
