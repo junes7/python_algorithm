62fc3a798bcd31674fa2556edc9479a1c9be93fd
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-04 22:49:50 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -1,7 +1,7 @@
 def solution(myString, pat):
+    r=''
     for i in range(len(myString)-1,0,-1):
         if myString[i]==pat[-1]:
-            e=i
+            r=myString[:i+1]        
             break
-    answer=myString[:e+1]
-    return answer
+    return r
