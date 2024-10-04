0646f94350cb302fc61fe587e82d8590c313f486
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-04 22:53:10 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 7.09 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,3 +1,3 @@
 def solution(myString):
-    answer = [i for i in myString.split('x') if len(i)!=0]
-    return sorted(list(answer))
+    r=sorted([i for i in myString.split('x') if len(i)!=0])
+    return r
