a37738f7a6dcf4a43201e8ad76869f309222ed38
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-05 23:56:55 +0900

    [level 0] Title: 세로 읽기, Time: 0.25 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,3 +1,3 @@
 def solution(my_string, m, c):
-    answer = ''.join(my_string[m*i:m*i+m][c-1:c] for i in range(len(my_string)//m))
-    return answer
+    r=''.join(my_string[m*i:m*i+m][c-1:c] for i in range(len(my_string)//m))
+    return r
