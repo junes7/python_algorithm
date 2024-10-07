1d5e65017eeed85a156b3284791f8660ce9ed20e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-07 23:55:42 +0900

    [level 0] Title: 코드 처리하기, Time: 12.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,11 +1,11 @@
 def solution(code):
-#     answer,mode='',0
-#     for i in range(len(code)):
-#         if code[i]=='1':
-#             mode^=1
-#         else:
-#             answer+=code[i] if i%2==mode else ''
+    r,mode='',0
+    for i in range(len(code)):
+        if code[i]=='1':
+            mode^=1
+        else:
+            r+=code[i] if i%2==mode else ''
             
-#     return "EMPTY" if len(answer)==0 else answer
-    r=''.join(code.split('1'))[::2] or 'EMPTY'
-    return r
+    return "EMPTY" if len(r)==0 else r
+    # r=''.join(code.split('1'))[::2] or 'EMPTY'
+    # return r
