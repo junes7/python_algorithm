e4d9f0a468d6f398c1d7b2482b89f9e886841d2c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-07 23:52:16 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,13 +1,13 @@
 def solution(arr, idx):
     # method1
-    r=-1
-    for i in range(idx,len(arr)):
-        if arr[i]==1:
-            r=i
-            break
+    # r=-1
+    # for i in range(idx,len(arr)):
+    #     if arr[i]==1:
+    #         r=i
+    #         break
     # method2
-    # try:
-    #     r=arr.index(1,idx)
-    # except:
-    #     r=-1    
+    try:
+        r=arr.index(1,idx)
+    except:
+        r=-1    
     return r
