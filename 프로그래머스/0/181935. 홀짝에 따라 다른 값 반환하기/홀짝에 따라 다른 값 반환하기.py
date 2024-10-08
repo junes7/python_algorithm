77723d64f7be46aa5592a4dc8c9ed45104219339
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-08 23:52:33 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,5 +1,5 @@
 def solution(n):
-    answer = 0
+    r = 0
     for i in range(1 if n%2==1 else 2,n+1,2):
-        answer+=i if n%2==1 else i**2
-    return answer
+        r+=i if n%2==1 else i**2
+    return r
