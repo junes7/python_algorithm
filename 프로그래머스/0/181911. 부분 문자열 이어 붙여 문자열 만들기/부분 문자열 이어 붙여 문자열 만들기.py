8b472bac5e55885250fc5b32fbd7c83a87b46091
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-11 23:59:37 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -1,3 +1,6 @@
 def solution(my_strings, parts):
-    r=''.join(my_strings[i][s:e+1] for i,(s,e) in enumerate(parts))
+    # r=''.join(my_strings[i][s:e+1] for i,(s,e) in enumerate(parts))
+    r=''
+    for i,(s,e) in enumerate(parts):
+        r+=my_strings[i][s:e+1]
     return r
