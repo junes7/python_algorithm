549bc28f79cfad64e8fac59cbb46ab277c9b285a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-11 23:58:18 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -1,3 +1,6 @@
 def solution(start_num, end_num):
-    r=[i for i in range(start_num,end_num+1)]
+    # r=[i for i in range(start_num,end_num+1)]
+    r=[]
+    for i in range(start_num,end_num+1):
+        r+=[i]
     return r
