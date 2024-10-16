aa7333519d22c946eddb4d3ef494fc0d7953979b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-16 23:46:39 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -1,13 +1,13 @@
 def solution(num_list):
     # method1
-    r=0
-    for n in num_list:
-        print(bin(n))
-        cnt=0
-        while n>1:
-            n=n//2 if not n%2 else (n-1)//2
-            cnt+=1
-        r+=cnt
+    # r=0
+    # for n in num_list:
+    #     print(bin(n))
+    #     cnt=0
+    #     while n>1:
+    #         n=n//2 if not n%2 else (n-1)//2
+    #         cnt+=1
+    #     r+=cnt
     # method2
-    # r=sum([len(bin(n))-3 for n in num_list])     
+    r=sum([len(bin(n))-3 for n in num_list])     
     return r
