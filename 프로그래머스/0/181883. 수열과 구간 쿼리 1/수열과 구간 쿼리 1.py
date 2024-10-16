519e64ceed72120820d662efe9d8d3ce8a778be9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-16 23:45:17 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 20.91 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -1,6 +1,6 @@
 def solution(arr, queries):
-    answer=arr
+    r=arr
     for s,e in queries:
         for i in range(s,e+1):
-            answer[i]+=1
-    return answer
+            r[i]+=1
+    return r
