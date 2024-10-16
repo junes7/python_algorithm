b21493efacd512946e429a3a9c5ff392644502bc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-16 23:42:33 +0900

    [level 0] Title: 배열 만들기 5, Time: 13.58 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -1,3 +1,3 @@
 def solution(intStrs, k, s, l):
-    answer = [int(n[s:s+l]) for n in intStrs if int(n[s:s+l])>k]
-    return answer
+    r=[int(n[s:s+l]) for n in intStrs if int(n[s:s+l])>k]
+    return r
