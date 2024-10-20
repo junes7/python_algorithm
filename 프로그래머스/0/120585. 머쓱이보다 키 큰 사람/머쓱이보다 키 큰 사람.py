6ae6d2bf0cf86611dad0530a07471b0974241c1d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-20 23:52:08 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,6 +1,6 @@
 def solution(array, height):
     # method1
-    answer=len([n for n in array if n >height])
+    # answer=len([n for n in array if n >height])
     # method2
-    # answer=sum([1 for n in array if n>height])
+    answer=sum([1 for n in array if n>height])
     return answer
