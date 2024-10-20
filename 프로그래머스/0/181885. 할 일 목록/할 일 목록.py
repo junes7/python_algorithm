24115d557936cfbc885ef38aa322a4dc3f921cbe
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-20 23:51:29 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,6 @@
 def solution(todo_list, finished):
     # method1
-    # answer=[todo_list[i] for i in range(len(todo_list)) if finished[i]==False]
+    answer=[todo_list[i] for i in range(len(todo_list)) if finished[i]==False]
     # method2
-    answer=[c for i,c in enumerate(todo_list) if not finished[i]]
+    # answer=[c for i,c in enumerate(todo_list) if not finished[i]]
     return answer
