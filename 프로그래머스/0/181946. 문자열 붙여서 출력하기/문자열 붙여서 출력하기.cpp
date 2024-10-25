fb76940e91278c6e9d91072450758d00ca6a5096
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-25 23:59:46 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 2.22 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str1, str2;
+    cin >> str1 >> str2;
+    str1.append(str2);
+    cout << str1 << endl;
+    return 0;
+}
