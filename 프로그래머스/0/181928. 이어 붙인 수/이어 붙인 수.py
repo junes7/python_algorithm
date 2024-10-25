ac4ada280faa12aa3470e0b692ea929bccf855e3
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-25 23:59:11 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,10 +1,10 @@
 def solution(num_list):
     odd,even='',''
-    r=0
+    ansr=0
     for n in num_list:
         if n%2!=0:
             odd+=str(n)
         else:
             even+=str(n)
-    r=int(odd)+int(even)
-    return r
+    ansr=int(odd)+int(even)
+    return ansr
