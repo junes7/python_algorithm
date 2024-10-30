a52835e99ac7807d4b929f2a8ec0bb43a88e675b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-10-31 00:00:05 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 89.71 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
     int a,b;
     cin >> a >> b;
-    // cout << a << " + " << b << " = " << (a + b) << endl;
-    printf("%d + %d = %d\n", a,b,a+b);
+    cout << a << " + " << b << " = " << (a + b) << endl;
+    // printf("%d + %d = %d\n", a,b,a+b);
     return 0;
 }
