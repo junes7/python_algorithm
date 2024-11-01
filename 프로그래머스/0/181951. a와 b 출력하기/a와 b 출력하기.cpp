8e322723694942d16bb038e956f603d21ed12c60
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-01 23:56:21 +0900

    [level 0] Title: a와 b 출력하기, Time: 2.06 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -6,7 +6,7 @@ int main(void) {
     int a;
     int b;
     cin >> a >> b;
-    // cout << "a = " << a << endl << "b = " << b << endl;
-    printf("a = %d\nb = %d\n",a,b);
+    cout << "a = " << a << endl << "b = " << b << endl;
+    // printf("a = %d\nb = %d\n",a,b);
     return 0;
 }
