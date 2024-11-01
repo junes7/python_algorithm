1e566b8898dc7b4e2259510f7452523eb4e1391b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-01 23:59:07 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 4.46 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -9,10 +9,13 @@ int main(void) {
     for(int i = 0; i < str.length(); i++) {
         // ascii 코드를 사용했을 때
         if(str[i] >= 65 && str[i] <= 90) {
-            str[i] += 32;
+            // str[i] += 32;
+            str[i]=tolower(str[i]);
         } else if(str[i] >= 97 && str[i] <= 122) {
-            str[i] -= 32;
+            // str[i] -= 32;
+            str[i]=toupper(str[i]);
         }
+        
     }
     cout << str << endl;
     return 0;
