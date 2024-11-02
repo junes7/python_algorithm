e695380eaf01bdb165a21a9791db132c8b4511ed
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-02 23:31:07 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -6,9 +6,5 @@ using namespace std;
 int solution(int num1, int num2) {
     int r=0;
     r=(num1==num2)?1:-1;
-    // if(num1==num2)
-    //     r=1;
-    // else
-    //     r=-1;
     return r;
 }
