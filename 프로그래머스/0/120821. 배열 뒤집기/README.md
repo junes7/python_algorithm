5e0ce297a644956bff58aa2ef4fa212477974198
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-09 00:07:06 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/README.md
+++ 프로그래머스/0/120821. 배열 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 뒤집기 - 120821 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120821?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120821) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.01 ms
+메모리: 4.17 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 08일 23:58:00
+2024년 11월 09일 00:07:04
 
 ### 문제 설명
 
--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 vector<int> solution(vector<int> num_list) {
     vector<int> r;
-    for(int i=num_list.size()-1;i>-1;i--) {
-        r.push_back(num_list[i]);
+    // for(int i=num_list.size()-1;i>-1;i--) {
+    //     r.push_back(num_list[i]);
+    // }
+    int l=num_list.size();
+    for(int i=0;i<l;i++) {
+        r.push_back(num_list[l-1-i]);
     }
     return r;
 }
