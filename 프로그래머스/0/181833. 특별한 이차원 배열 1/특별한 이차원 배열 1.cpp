22030189a8b5432547a3df7a5eb0438fdf5d4df6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-11 23:57:20 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 4.27 ms, Memory: 5.96 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -8,10 +8,11 @@ vector<vector<int>> solution(int n) {
     for(int i=0;i<n;i++) {
         vector<int> arr;
         for(int j=0;j<n;j++) {
-            if(i==j)
-                arr.push_back(1);
-            else
-                arr.push_back(0);
+            // if(i==j)
+            //     arr.push_back(1);
+            // else
+            //     arr.push_back(0);
+            arr.push_back((i==j)?1:0);
         }
         r.push_back(arr);
     }
