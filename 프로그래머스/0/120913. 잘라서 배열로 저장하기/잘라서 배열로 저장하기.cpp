97774344e3413b3e3e5aec5680d420f31c92afa3
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-13 23:48:02 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.cpp
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 vector<string> solution(string my_str, int n) {
     vector<string> r;
-    cout<<my_str.length()<<endl;
     int l=my_str.length();
     int len=(l%n==0)?l/n:(l/n+1);
     for(int i=0;i<len;i++) {
