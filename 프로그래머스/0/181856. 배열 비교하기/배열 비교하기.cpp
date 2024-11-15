83fec6696f18470e4383694a124a2a1b44248b01
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-16 00:31:53 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cpp
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cpp
@@ -4,26 +4,25 @@
 using namespace std;
 
 int solution(vector<int> arr1, vector<int> arr2) {
-    int r=0;
-    int s1=0, s2=0;
+    int r=0, s1=0, s2=0;
     if(arr1.size()!=arr2.size()) {
-        if(arr1.size() > arr2.size())
-            r=1;
-        else
-            r=-1;
+        // if(arr1.size() > arr2.size())
+        //     r=1;
+        // else
+        //     r=-1;
+        r=(arr1.size()>arr2.size())?1:-1;
     } else {
         for(int i=0;i<arr1.size();i++) {
             s1+=arr1[i];
             s2+=arr2[i];
         }
-        if(s1==s2)
-            r=0;
-        else if(s1>s2)
-            r=1;
-        else
-            r=-1;
+        // if(s1==s2)
+        //     r=0;
+        // else if(s1>s2)
+        //     r=1;
+        // else
+        //     r=-1;
+        r=(s1==s2)?0:((s1>s2)?1:-1);
     }
-    
-    
     return r;
 }
