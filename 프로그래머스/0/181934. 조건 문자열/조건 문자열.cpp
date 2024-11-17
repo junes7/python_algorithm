991c147f692e7e23e6bc994a58d53cc548badaa3
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-17 15:38:12 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int solution(string ineq, string eq, int n, int m) {
     int r = 0;
-    if(ineq=="<") {
-        r=(eq=="=")?n<=m:n<m;
-    } else {
-        r=(eq=="=")?n>=m:n>m;
-    }
+    // if(ineq=="<") {
+    //     r=(eq=="=")?n<=m:n<m;
+    // } else {
+    //     r=(eq=="=")?n>=m:n>m;
+    // }
+    r=(ineq=="<")?((eq=="=")?n<=m:n<m):((eq=="=")?n>=m:n>m);
     return r;
 }
