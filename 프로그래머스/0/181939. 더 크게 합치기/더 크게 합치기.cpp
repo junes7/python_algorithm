fc6d4dba8ca4673087d50633547044bbdceeb1ce
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-17 15:54:53 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -4,14 +4,7 @@
 using namespace std;
 
 int solution(int a, int b) {
-    string ch1=to_string(a);
-    string ch2=to_string(b);
-    string s;
-    if(stoi(ch1+ch2) > stoi(ch2+ch1))
-        s=ch1+ch2;        
-    else
-        s=ch2+ch1;
-    int r=stoi(s);
-    
+    string ch1=to_string(a), ch2=to_string(b);
+    int r=stoi((stoi(ch1+ch2) > stoi(ch2+ch1))?(ch1+ch2):(ch2+ch1));
     return r;
 }
