48e4bd3753737aeb27e0a8d48fd621e0952445fb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-20 19:22:37 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/README.md
+++ 프로그래머스/0/181853. 뒤에서 5등까지/README.md
@@ -1,10 +1,10 @@
 # [level 0] 뒤에서 5등까지 - 181853 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181853?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181853) 
 
 ### 성능 요약
 
-메모리: 4.28 MB, 시간: 0.01 ms
+메모리: 4.18 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 16일 00:27:26
+2024년 11월 20일 19:22:36
 
 ### 문제 설명
 
--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -6,7 +6,16 @@ using namespace std;
 
 vector<int> solution(vector<int> num_list) {
     vector<int> r;
-    sort(num_list.begin(),num_list.end(),less<int>());
+    // sort(num_list.begin(),num_list.end(),less<int>());
+    for(int i=0;i<num_list.size()-1;i++) {
+        for(int j=0; j<num_list.size()-i-1;j++) {
+            if(num_list[j]>num_list[j+1]) {
+                int t=num_list[j];
+                num_list[j]=num_list[j+1];
+                num_list[j+1]=t;
+            }
+        }
+    }
     for(int i=0;i<5;i++) {
         r.push_back(num_list[i]);
     }
