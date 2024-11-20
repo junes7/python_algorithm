21391aafbca0a29b63c6cc57c936b890742d18bb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-20 18:40:34 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/README.md
+++ 프로그래머스/0/120811. 중앙값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중앙값 구하기 - 120811 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120811?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120811) 
 
 ### 성능 요약
 
-메모리: 4.17 MB, 시간: 0.01 ms
+메모리: 4.21 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 04일 00:25:50
+2024년 11월 20일 18:40:32
 
 ### 문제 설명
 
--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cpp
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 int solution(vector<int> array) {
     int r=0;
-    sort(array.begin(),array.end(),less<int>());
+    // sort(array.begin(),array.end(),less<int>());
+    // Bubble Sort
+    for(int i=0;i<array.size()-1;i++) {
+        for(int j=0;j<array.size()-i-1;j++) {
+            if(array[j]>array[j+1]) {
+                int t=array[j];
+                array[j]=array[j+1];
+                array[j+1]=t;
+            }
+        }
+    }
     r=array[array.size()/2];
     return r;
 }
