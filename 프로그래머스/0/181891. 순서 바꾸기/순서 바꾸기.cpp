08d7e3a3343eebb87e13ec76b066d3980961cd6e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-22 00:21:32 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> r;
+    for(int i=n;i<num_list.size();i++) {
+        r.push_back(num_list[i]);
+    }
+    for(int i=0;i<n;i++) {
+        r.push_back(num_list[i]);
+    }
+    return r;
+}
