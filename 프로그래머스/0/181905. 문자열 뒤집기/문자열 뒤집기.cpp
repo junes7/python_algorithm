7471af5ccc14fdf4a8fa8a9e241b3b2dfa77db18
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-23 10:34:59 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int s, int e) {
+    string r = "";
+    int t=0;
+    for(int i=0;i<my_string.length();i++) {
+        if(i>=s && i<=e) {
+            r+=my_string[e-t++];
+        } else {
+            r+=my_string[i];
+        }
+    }
+    return r;
+}
