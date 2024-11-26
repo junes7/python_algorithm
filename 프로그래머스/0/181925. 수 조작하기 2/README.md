b1fe407dbc10ba05cbf315b0d1e8c6988734f77d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-26 17:57:56 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.33 ms, Memory: 6.12 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/README.md
+++ 프로그래머스/0/181925. 수 조작하기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수 조작하기 2 - 181925 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181925) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181925?language=cpp) 
 
 ### 성능 요약
 
-메모리: 13.9 MB, 시간: 16.11 ms
+메모리: 6.12 MB, 시간: 1.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 07일 07:45:06
+2024년 11월 26일 17:57:53
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string r = "";
+    for(int i=1;i<numLog.size();i++) {
+        if(numLog[i]-numLog[i-1]==1)
+            r+='w';
+        else if(numLog[i]-numLog[i-1]==-1)
+            r+='s';
+        else if(numLog[i]-numLog[i-1]==10)
+            r+='d';
+        else
+            r+='a';
+    }
+    return r;
+}
