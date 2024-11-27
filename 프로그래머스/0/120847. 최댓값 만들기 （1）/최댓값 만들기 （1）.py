21a8ed14fd6dc4ba3165ae531e8819aafd8df7b9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-27 23:07:05 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.49 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,3 +1,3 @@
 def solution(numbers):
-    r=max([numbers[i]*numbers[j] for i in range(len(numbers)) for j in range(i+1,len(numbers))])
+    r=max([numbers[i]*numbers[j] for i in range(len(numbers)-1) for j in range(i+1,len(numbers))])
     return r
