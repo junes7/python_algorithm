fe96d4a5838d69821f97aa66be5fc2ef36d822e4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-29 12:31:01 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -6,9 +6,7 @@ string solution(string my_string) {
     for(int i=0;i<my_string.length();i++) {
         for(int j=i+1;j<my_string.length();j++) {
             if(my_string[i]==my_string[j]) {
-                my_string.erase(my_string.begin()+j);
-                j--;
-                    
+                my_string.erase(my_string.begin()+j--);
             }
         }
     }
