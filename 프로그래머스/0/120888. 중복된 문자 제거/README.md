71a2b812f17421a62e7c97480f399e1e38f90939
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-29 12:28:47 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/README.md
+++ 프로그래머스/0/120888. 중복된 문자 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중복된 문자 제거 - 120888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 14일 21:46:53
+2024년 11월 29일 12:28:45
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string my_string) {
+    for(int i=0;i<my_string.length();i++) {
+        for(int j=i+1;j<my_string.length();j++) {
+            if(my_string[i]==my_string[j]) {
+                my_string.erase(my_string.begin()+j);
+                j--;
+                    
+            }
+        }
+    }
+    return my_string;
+}
