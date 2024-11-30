4c41bfb48e87b0fb23d3f7170b94cdc0351756ce
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-30 23:00:20 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> emergency) {
+    vector<int> r, temp;
+    for(int i=0;i<emergency.size();i++) {
+        temp.push_back(emergency[i]);
+    }
+    sort(temp.begin(),temp.end(),greater<int>());
+    for(int i=0;i<emergency.size();i++) {
+        if(find(temp.begin(),temp.end(),emergency[i])!=temp.end()) {
+            r.push_back(find(temp.begin(),temp.end(),emergency[i])-temp.begin()+1);
+        }
+    }
+    return r;
+}
