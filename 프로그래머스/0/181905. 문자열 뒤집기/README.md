52056782adfb7f961b9480ecee43f2c1aace04cf
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-11-30 15:44:24 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/README.md
+++ 프로그래머스/0/181905. 문자열 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 뒤집기 - 181905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181905) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.02 ms
+메모리: 4.18 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 23일 10:34:56
+2024년 11월 30일 15:44:21
 
 ### 문제 설명
 
--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -7,11 +7,12 @@ string solution(string my_string, int s, int e) {
     string r = "";
     int t=0;
     for(int i=0;i<my_string.length();i++) {
-        if(i>=s && i<=e) {
-            r+=my_string[e-t++];
-        } else {
-            r+=my_string[i];
-        }
+        // if(i>=s && i<=e) {
+        //     r+=my_string[e-t++];
+        // } else {
+        //     r+=my_string[i];
+        // }
+        r+=(i>=s & i<=e)?my_string[e-t++]:my_string[i];
     }
     return r;
 }
