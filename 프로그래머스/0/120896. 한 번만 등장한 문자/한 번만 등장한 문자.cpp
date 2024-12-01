d968ddc750289153380ac444a1ab6e6fd6252bc5
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-01 16:33:39 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+string solution(string s) {
+    string r="";
+    map<char,int> dic;
+    for(int i=0;i<s.size();i++) {
+        // if(dic[s[i]]==0) {
+        //     dic[s[i]]=1;
+        // } else {
+        //     dic[s[i]]++;
+        // }
+        dic[s[i]]=(dic[s[i]]==0)?1:dic[s[i]]+1;
+    }
+    for(pair<char,int> i:dic) {
+        if(i.second==1)
+            r+=i.first;
+    }
+    return r;
+}
