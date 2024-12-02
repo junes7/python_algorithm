038526f791b786c5e9e476ba0f12c1d27fed6ec1
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-02 18:05:14 +0900

    [level 0] Title: 커피 심부름, Time: 0.05 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.cpp
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.cpp
@@ -6,10 +6,11 @@ using namespace std;
 int solution(vector<string> order) {
     int r=0;
     for(int i=0;i<order.size();i++) {
-        if(order[i].find("latte")!=-1)
-            r+=5000;
-        else
-            r+=4500;
+        // if(order[i].find("latte")!=-1)
+        //     r+=5000;
+        // else
+        //     r+=4500;
+        r+=(order[i].find("latte")!=-1)?5000:4500;
     }
     return r;
 }
