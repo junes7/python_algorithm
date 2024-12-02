b8776eb1ef586543fb87c0f416925e2e304eec9c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-02 20:20:31 +0900

    [level 0] Title: 이진수 더하기, Time: 0.02 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int TwoToTen(int num) {
+    int result=0, mul=1;
+    while(num>0) {
+        if(num%10==1) result+=mul;
+        mul*=2;
+        num/=10;
+    }
+    return result;
+}
+
+string solution(string bin1, string bin2) {
+    string r="";
+    int num=TwoToTen(stoi(bin1))+TwoToTen(stoi(bin2));
+    // TenToTwo
+    if(num==0)
+        r+='0';
+    while(num>0) {
+        if(num%2==1) r+='1';
+        else r+='0';
+        num/=2;
+    }
+    reverse(r.begin(),r.end());
+    return r;
+}
