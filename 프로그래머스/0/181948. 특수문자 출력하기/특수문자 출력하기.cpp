629522cedda237fa00eab6c36cfbc429892abab2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-02 18:12:27 +0900

    [level 0] Title: 특수문자 출력하기, Time: 2.09 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.cpp
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
 int main(void) {
     // string str = "!@#$%^&*(\\\'\"<>?:;";
-    char str[]="!@#$%^&*(\\\'\"<>?:;";
+    // char str[]="!@#$%^&*(\\\'\"<>?:;";
+    int len=strlen("!@#$%^&*(\\\'\"<>?:;");
+    char *str=new char[len+1];   
+    strcpy(str,"!@#$%^&*(\\\'\"<>?:;");
     cout << str << endl;
+    delete[] str;
     return 0;
 }
