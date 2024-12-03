93333ab87ffd36d5073a1fa5a80c5f5854c47562
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-03 12:17:31 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 0.40 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/README.md
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 여러 번 뒤집기 - 181913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181913) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181913?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 15.05 ms
+메모리: 4.2 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 30일 23:52:41
+2024년 12월 03일 12:17:29
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string my_string, vector<vector<int>> queries) {
+    string r="";
+    int len;
+    for(int i=0;i<queries.size();i++) {
+        len=queries[i][1]-queries[i][0];
+        if((queries[i][1]-queries[i][0])%2!=0)
+            len+=1;
+        for(int j=0;j<len/2;j++) {
+            char t=my_string[queries[i][0]+j];
+            my_string[queries[i][0]+j]=my_string[queries[i][1]-j];
+            my_string[queries[i][1]-j]=t;
+        }
+    }
+    r=my_string;
+    return r;
+}
