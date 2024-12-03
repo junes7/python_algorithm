91df0e2e8975c56f07686004ab4da34a572fdef4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-03 18:12:14 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 1.26 ms, Memory: 4.05 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> r;
+    int min,max=*max_element(arr.begin(),arr.end());
+    for(int i=0;i<queries.size();i++) {
+        min=max+1;
+        cout<<min<<endl;
+        for(int j=queries[i][0];j<queries[i][1]+1;j++) {
+            if(arr[j]>queries[i][2] and arr[j]<min) {
+                min=arr[j];
+            }
+        }
+        
+        r.push_back((min==max+1)?-1:min);
+    }
+    return r;
+}
