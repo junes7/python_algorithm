5245a20e832afd3d4ae868fbeea2185e72f0ecd6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-04 17:50:26 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> rank, vector<bool> attendance) {
+    vector<pair<int,int>> attRank;
+    for(int i=0;i<rank.size();i++) {
+        if(attendance[i])
+            attRank.push_back({rank[i],i});
+    }
+    sort(attRank.begin(),attRank.end());
+    int r=10000*attRank[0].second+100*attRank[1].second+attRank[2].second;
+    return r;
+}
