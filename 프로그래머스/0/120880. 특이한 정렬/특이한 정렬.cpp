172bd6f049a5a266915172e9c3bf543fff6e59d8
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-05 12:53:31 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
@@ -2,15 +2,20 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int t;
-bool compare(const int a, const int b) {
-    if(abs(t-a)==abs(t-b))
-        return a>b;
-    return abs(t-a)<abs(t-b);
-}
+// int t;
+// bool compare(const int a, const int b) {
+//     if(abs(t-a)==abs(t-b))
+//         return a>b;
+//     return abs(t-a)<abs(t-b);
+// }
 
 vector<int> solution(vector<int> numlist, int n) {
-    t=n;
-    sort(numlist.begin(),numlist.end(),compare);
+    // t=n;
+    // sort(numlist.begin(),numlist.end(),compare);
+    sort(numlist.begin(),numlist.end(),[n](const int a, const int b) -> bool {
+        if(abs(n-a)==abs(n-b))
+            return a>b;
+        return abs(n-a)<abs(n-b);
+    });
     return numlist;
 }
