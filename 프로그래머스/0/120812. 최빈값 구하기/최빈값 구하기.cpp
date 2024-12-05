ada370b9c23bf169eb710579c096d6ee99f5159f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-06 00:19:14 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    map<int,int> cnt;
+    int r=0,mnum;
+    for(int i=0;i<array.size();i++) {
+        cnt[array[i]]++;
+    }
+    vector<pair<int,int>> v(cnt.begin(),cnt.end());
+    sort(v.begin(),v.end(),[](pair<int,int>&a,pair<int,int>&b) -> bool {
+        return a.second>b.second;
+    });
+    mnum=v[0].second;
+    for(pair<int,int> i:v) {
+        if(i.second==mnum)
+            r+=1;
+    }
+    r=(r>1)?-1:v[0].first;
+    return r;
+}
