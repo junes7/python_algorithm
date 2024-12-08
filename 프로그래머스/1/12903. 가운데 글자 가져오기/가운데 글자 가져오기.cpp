a68ec503eb5d1af3ca36f55d87828e33147b67ea
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-09 01:59:57 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string r="";
+    int med=s.size()/2;
+    if(s.size()%2!=0) {
+        r+=s[med];
+    } else {
+        r+=s[med-1];
+        r+=s[med];
+    }
+    return r;
+}
