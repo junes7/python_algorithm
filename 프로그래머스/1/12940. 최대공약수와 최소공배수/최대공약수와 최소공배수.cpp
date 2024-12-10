53eeb1973e910bae3bfd217c4b46555ed041e288
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-10 11:35:19 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -1,13 +1,10 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
 int gcd(const int x, const int y) {
-    if(x%y==0)
-        return y;
-    else
-        return gcd(y,x%y);
+    return (x%y==0)?y:gcd(y,x%y);
 }
 
 vector<int> solution(int n, int m) {
