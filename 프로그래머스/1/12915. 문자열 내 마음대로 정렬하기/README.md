7790fe535e28b836b5a350a971423de450c682f5
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-11 12:47:14 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.03 ms
+메모리: 4.2 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 19일 23:45:30
+2024년 12월 11일 12:47:13
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(vector<string> strings, int n) {
+    stable_sort(strings.begin(),strings.end(),less<string>());
+    stable_sort(strings.begin(),strings.end(),[n](const string& a, const string& b) -> bool {
+        return a[n]<b[n];
+    });
+    vector<string> r=strings;
+    return r;
+}
