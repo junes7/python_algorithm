2d23719c1f5c1376c7aa6ccba020bcfec79e3a92
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-11 12:08:54 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.26 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/README.md
+++ 프로그래머스/1/134240. 푸드 파이트 대회/README.md
@@ -1,10 +1,10 @@
 # [level 1] 푸드 파이트 대회 - 134240 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 4.21 MB, 시간: 0.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 20일 15:54:40
+2024년 12월 11일 12:08:52
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string r="",t="";
+    for(int i=1;i<food.size();i++) {
+        if(food[i]/2==0)
+            continue;
+        else
+            for(int j=0;j<food[i]/2;j++)
+                t+=to_string(i);
+    }
+    r+=t+'0';
+    reverse(t.begin(),t.end());
+    r+=t;
+    return r;
+}
