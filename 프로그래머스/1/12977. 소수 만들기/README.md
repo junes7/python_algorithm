a699cacf9080168c8dd0714527dd7a5a11e2460e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-12 22:24:15 +0900

    [level 1] Title: 소수 만들기, Time: 18.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/README.md
+++ 프로그래머스/1/12977. 소수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 만들기 - 12977 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 451.82 ms
+메모리: 4.14 MB, 시간: 18.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 20일 21:51:24
+2024년 12월 12일 22:24:13
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> nums) {
+    int r=0,sum,d;
+    vector<int> t;
+    for(int i=0;i<nums.size()-2;i++) {
+        for(int j=i+1;j<nums.size()-1;j++) {
+            for(int k=j+1;k<nums.size();k++) {
+                sum=0,d=2,t={};
+                sum=nums[i]+nums[j]+nums[k]; 
+                while(sum>1) {
+                    if(sum%d==0) {
+                        t.push_back(d);
+                        sum/=d;
+                    } else {
+                        d+=1;
+                    }
+                }
+                if(t.size()==1)
+                    r+=1;
+            }
+        }
+    }
+    
+
+    return r;
+}
