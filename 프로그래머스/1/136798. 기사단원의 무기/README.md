5c1431670b5020f436ed30847f644a50558761cc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-13 10:48:53 +0900

    [level 1] Title: 기사단원의 무기, Time: 123.59 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/README.md
+++ 프로그래머스/1/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 928.35 ms
+메모리: 4.22 MB, 시간: 123.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 21일 14:48:04
+2024년 12월 13일 10:48:51
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int number, int limit, int power) {
+    int r=0;
+    vector<int> t;
+    for(int i=1;i<number+1;i++) {
+        t={};
+        for(int j=1;j<pow(i,0.5)+1;j++) {
+            if(i%j==0) {
+                if(find(t.begin(),t.end(),j)==t.end())
+                    t.push_back(j);
+                if(pow(j,2)!=i)
+                    if(find(t.begin(),t.end(),i/j)==t.end())
+                        t.push_back(i/j);
+            }
+        }
+        r+=(t.size()>limit)?power:t.size();
+    }
+    return r;
+}
