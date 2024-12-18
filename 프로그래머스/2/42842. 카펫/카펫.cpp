68199395d589cffdb7ebaeef258600f5a72a20a4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-18 22:48:38 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> r;
+    int total=brown+yellow,row;
+    for(int col=3;col<total+1;col++) {
+        row=total/col;
+        if((row-2)*(col-2)==yellow)
+            break;
+    }
+    r={row,total/row};
+    return r;
+}
