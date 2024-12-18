185a1d45c3461f8bdaeb9df3a0f44a277f2e3317
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-18 22:58:58 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -8,13 +8,13 @@ def solution(arr):
     # 작은 수부터 차례대로 구하게 하기 위해 오름차순 정렬해줍니다.
     arr.sort()
     # 배열 처음 값을 초기 값으로 설정한 최소공배수 변수와 배열 길이 변수 선언
-    r,l=arr[0],len(arr)
+    r=arr[0]
     # 배열 길이-1만큼 돌면서(한번에 2개씩 처리하기에 한번 덜 반복하게 됩니다.)
-    for i in range(l-1):
+    for i in range(len(arr)-1):
         # A와 B의 최대공약수 구한다.
         gc=gcd(r,arr[i+1])
         # A와 B에 각각 최대공약수 나눠준 후 최대공약수 곱해서 최소공배수 구해준다.
-        lcm=r//gc*arr[i+1]//gc*gc
+        lcm=r*arr[i+1]//gc
         # 다음 값의 최소공배수 비교 대상으로 사용하기 위해 r에 구한 최소공배수 값 넣어준다.
         r=lcm
     return r
