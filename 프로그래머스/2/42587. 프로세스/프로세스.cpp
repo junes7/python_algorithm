ac5f4259d2b2a11fdc1358e90bcc7aef83b445fa
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-22 12:59:41 +0900

    [level 2] Title: 프로세스, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -6,24 +6,21 @@
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
-    int now_idx;
+    int now_idx,cnt=0;
     queue<int> q;
-    vector<int> s;
     for(int i=0;i<priorities.size();i++) {
         q.push(i);
     }
     while(!q.empty()) {
         now_idx=q.front();
         q.pop();
-        if(priorities[now_idx]!=*max_element(priorities.begin(),priorities.end())) {
+        if(priorities[now_idx]<*max_element(priorities.begin(),priorities.end())) {
             q.push(now_idx);
         } else {
-            s.push_back(now_idx);
+            if(now_idx==location)
+                return cnt+1;
             priorities[now_idx]=0;
+            cnt++;
         }
-    }
-    for(int i=0;i<s.size();i++) {
-        if(s[i]==location)
-            return i+1;
     }
 }
