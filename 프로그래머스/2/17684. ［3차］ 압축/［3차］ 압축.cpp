78c1b6c7e40ebef869ff62c593048d6bcb07d4e1
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-25 19:28:38 +0900

    [level 2] Title: [3차] 압축, Time: 0.43 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string msg) {
+    vector<int> r;
+    map<string,int> dic;
+    string t;
+    int cnt,i=0,idx=27;
+    for(int i=0;i<26;i++) {
+        t=(char)i+65;
+        dic[t]=i+1;
+    }
+    while(i<msg.length()) {
+        t="",cnt=0; 
+        while(i+cnt<msg.size() and dic.find(t+msg[i+cnt])!=dic.end()) {    
+            t+=msg[i+cnt];
+            cnt++;
+        }
+        r.push_back(dic[t]);
+        if(i+cnt<msg.size()) {
+            dic[t+msg[i+cnt]]=idx++;
+        }
+        i+=cnt;
+    }
+    return r;
+}
