b5e3cf04c5c7645c037e39e99886a560eb496817
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-28 22:34:22 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 4.53 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.c
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <ctype.h>
+// #include <ctype.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* my_string) {
