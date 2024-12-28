3ebb239610361c3c0d3a9d5f15923e5641935f55
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-28 15:30:28 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/README.md
+++ 프로그래머스/0/120583. 중복된 숫자 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중복된 숫자 개수 - 120583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120583?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120583) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.01 ms
+메모리: 4.23 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 03일 23:56:14
+2024년 12월 28일 15:30:26
 
 ### 문제 설명
 
--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
@@ -1,13 +1,15 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> array, int n) {
     int r=0;
-    for(int i=0;i<array.size();i++) {
-        if(array[i]==n)
-            r+=1;
-    }
+    // for(int i=0;i<array.size();i++) {
+    //     if(array[i]==n)
+    //         r+=1;
+    // }
+    r=count(array.begin(),array.end(),n);
     return r;
 }
