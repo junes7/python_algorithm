4ef2306784d10402a0d9b05c599a94ec2a542e38
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-29 00:05:23 +0900

    [level 0] Title: rny_string, Time: 0.01 ms, Memory: 4.47 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/README.md
+++ 프로그래머스/0/181863. rny＿string/README.md
@@ -1,10 +1,10 @@
 # [level 0] rny_string - 181863 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181863?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181863?language=c) 
 
 ### 성능 요약
 
-메모리: 3.7 MB, 시간: 0.03 ms
+메모리: 4.47 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 16일 00:58:17
+2024년 12월 29일 00:05:21
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* rny_string) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* r=(char*)malloc(sizeof(char)*(strlen(rny_string)*2+1));
+    int cnt=0;
+    for(int i=0;i<strlen(rny_string)+1;i++) {
+        if(rny_string[i]=='m') {
+            r[cnt++]='r';
+            r[cnt++]='n';
+        } else {
+            r[cnt++]=rny_string[i];
+        }
+    }
+    r[cnt]='\0';
+    return r;
+}
