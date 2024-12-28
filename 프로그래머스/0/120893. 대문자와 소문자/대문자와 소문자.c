ccfd13b9e75fd19fb1675072176be2961c3e8ea4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-28 22:34:00 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.c
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.c
@@ -8,8 +8,8 @@ char* solution(const char* my_string) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     char* r=(char*)malloc(sizeof(char)*(strlen(my_string)+1));
     for(int i=0;i<strlen(my_string);i++) {
-        r[i]=my_string[i]>96?my_string[i]-32:my_string[i]+32;
-        // r[i]=islower(my_string[i])?toupper(my_string[i]):tolower(my_string[i]);
+        // r[i]=my_string[i]>96?my_string[i]-32:my_string[i]+32;
+        r[i]=islower(my_string[i])?toupper(my_string[i]):tolower(my_string[i]);
     }
     r[strlen(my_string)]='\0';
     return r;
