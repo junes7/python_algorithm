d39d55a9a2b5d113770e5930956369a6aadcf8e6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-28 21:35:58 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -6,10 +6,11 @@ using namespace std;
 string solution(string my_string) {
     string r= "";
     for(int i=0;i<my_string.length();i++) {
-        if(isupper(my_string[i]))
-            r+=tolower(my_string[i]);
-        else
-            r+=toupper(my_string[i]);
+        // if(isupper(my_string[i]))
+        //     r+=tolower(my_string[i]);
+        // else
+        //     r+=toupper(my_string[i]);
+        r+=isupper(my_string[i])?tolower(my_string[i]):toupper(my_string[i]);
     }
     
     return r;
