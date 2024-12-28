c4f90371d2e7fbb9bf1d0e57e8a0def23ff793f2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-28 23:46:37 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/README.md
+++ 프로그래머스/0/120905. n의 배수 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 0] n의 배수 고르기 - 120905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120905?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120905?language=c) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 16일 00:17:54
+2024년 12월 28일 23:46:35
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numlist_len은 배열 numlist의 길이입니다.
+int* solution(int n, int numlist[], size_t numlist_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* r=(int*)malloc(sizeof(int)*numlist_len);
+    int cnt=0;
+    for(int i=0;i<numlist_len;i++) {
+        if(numlist[i]%n==0)
+            r[cnt++]=numlist[i];
+    }
+    return r;
+}
