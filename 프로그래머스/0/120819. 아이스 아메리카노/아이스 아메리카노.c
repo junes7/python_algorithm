0041dace834a2fec011d1cda2fcbabe24a29a1c4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-28 17:29:00 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.c
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.c
@@ -5,7 +5,11 @@
 int* solution(int money) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* r=(int*)malloc(sizeof(int)*2);
-    r[0]=money/5500;
-    r[1]=money%5500;
+    // method1 array element
+    // r[0]=money/5500;
+    // r[1]=money%5500;
+    // method2 pointer
+    *r=money/5500;
+    *(r+1)=money%5500;
     return r;
 }
