866865cb56fa75acfbaf1177ae33c4ad4743e2f9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-28 09:30:12 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 double solution(vector<int> numbers) {
     double r=0;
+    // method1 accumulate function
     int s=accumulate(numbers.begin(),numbers.end(),0);
+    // method2 for loop
     // for(int i=0;i<numbers.size();i++) {
     //     s+=numbers[i];
     // }
