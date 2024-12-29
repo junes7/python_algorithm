cbe62d093801a11868d24222861be20e9ca80954
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-29 17:58:08 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/README.md
+++ 프로그래머스/0/181887. 홀수 vs 짝수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 홀수 vs 짝수 - 181887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181887?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181887) 
 
 ### 성능 요약
 
-메모리: 4.12 MB, 시간: 0.01 ms
+메모리: 4.13 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 21일 23:40:06
+2024년 12월 29일 17:58:06
 
 ### 문제 설명
 
--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
@@ -4,13 +4,18 @@
 using namespace std;
 
 int solution(vector<int> num_list) {
-    int r=0, oddSum=0, evenSum=0;
+    vector<int> r={0,0};
     for(int i=0;i<num_list.size();i++) {
-        if(i%2==0)
-            evenSum+=num_list[i];
-        else
-            oddSum+=num_list[i];
+        r[i%2]+=num_list[i];
     }
-    r=(evenSum>=oddSum)?evenSum:oddSum;
-    return r;
+    return r[0]>=r[1]?r[0]:r[1];
+    // int r=0,oddSum=0, evenSum=0;
+    // for(int i=0;i<num_list.size();i++) {
+    //     if(i%2==0)
+    //         oddSum+=num_list[i];
+    //     else
+    //         evenSum+=num_list[i];
+    // }
+    // r=(evenSum>=oddSum)?evenSum:oddSum;
+    // return r;
 }
