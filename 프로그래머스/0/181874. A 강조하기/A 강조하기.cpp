5c3fff42e615a84dc9fcdc549ec4aa840ce1346e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-29 11:15:03 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -6,7 +6,7 @@ using namespace std;
 string solution(string myString) {
     string r = "";
     for(int i=0;i<myString.length();i++) {
-        r.push_back(myString[i]=='a' || myString[i]=='A'?toupper(myString[i]):tolower(myString[i]));
+        r+=myString[i]=='a' || myString[i]=='A'?toupper(myString[i]):tolower(myString[i]);
     }
     return r;
 }
