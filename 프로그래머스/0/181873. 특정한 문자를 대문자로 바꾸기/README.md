71b3f372477e39bab265e0d59df9267d08fda3d7
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-29 11:07:10 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.39 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/README.md
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특정한 문자를 대문자로 바꾸기 - 181873 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181873?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181873?language=c) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.02 ms
+메모리: 4.39 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 01:23:09
+2024년 12월 29일 11:07:08
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string, const char* alp) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* r=(char*)malloc(sizeof(char)*(strlen(my_string)+1));
+    for(int i=0;i<=strlen(my_string);i++)
+        r[i]=my_string[i]==alp[0]?toupper(my_string[i]):tolower(my_string[i]);
+    return r;
+}
