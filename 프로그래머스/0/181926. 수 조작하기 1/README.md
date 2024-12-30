15c451f7741ba1790eae150bbdb148d1a2b54da6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-30 11:13:14 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.73 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/README.md
+++ 프로그래머스/0/181926. 수 조작하기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수 조작하기 1 - 181926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926?language=c) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.67 ms
+메모리: 4.21 MB, 시간: 0.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 30일 11:06:40
+2024년 12월 30일 11:13:12
 
 ### 문제 설명
 
--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.c
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.c
@@ -5,20 +5,32 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(int n, const char* control) {
     int i=0;
-    while(*(control+i)!=NULL) {
-        switch(*(control+i++)) {
-            case 'w':
-                n+=1;
-                break;
-            case 's':
-                n-=1;
-                break;
-            case 'd':
-                n+=10;
-                break;
-            case 'a':
-                n-=10;
-        }            
+    // method1 while loop and pointer
+    // while(*(control+i)!=NULL) {
+    //     switch(*(control+i++)) {
+    //         case 'w':
+    //             n+=1;
+    //             break;
+    //         case 's':
+    //             n-=1;
+    //             break;
+    //         case 'd':
+    //             n+=10;
+    //             break;
+    //         case 'a':
+    //             n-=10;
+    //     }            
+    // }
+    // method2 for loop
+    for(int i=0;i<strlen(control);i++) {
+        if(control[i]=='w')
+            n+=1;
+        else if(control[i]=='s')
+            n-=1;
+        else if(control[i]=='d')
+            n+=10;
+        else
+            n-=10;
     }
     return n;
 }
