7ed50ea5b659877642654b81ea797d284ca37548
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-30 12:54:48 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 324.64 ms, Memory: 228 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+int* solution(int arr[], size_t arr_len, int k) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* r=(int*)malloc(sizeof(int)*arr_len);
+    for(int i=0;i<arr_len;i++)
+        r[i]=k%2==1?arr[i]*k:arr[i]+k;
+    return r;
+}
