35b867433c410f365e1985834f6194b27e65f83d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-30 18:26:20 +0900

    [level 0] Title: 부분 문자열, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.c
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.c
@@ -4,8 +4,6 @@
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* str1, const char* str2) {
-    int r=0;
-    if(strstr(str2,str1)!=NULL)
-        r=1;
+    int r=strstr(str2,str1)!=NULL?1:0;
     return r;
 }
