8337d490dc10b9a7dc17b3ab4b66c0376037030a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-30 14:29:55 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 string solution(string n_str) {
-    string r = "";
-    int start=n_str.find_first_not_of('0');
-    for(int i=start;i<n_str.length();i++) {
-        r+=n_str[i];
-    }
+    string r="";
+    // int start=n_str.find_first_not_of('0');
+    // for(int i=start;i<n_str.length();i++) {
+    //     r+=n_str[i];
+    // }
+    r=to_string(stoi(n_str));
     return r;
 }
