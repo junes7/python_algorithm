7ff77716bce7a422631e68669e378f67ab4a468b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-30 23:35:32 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/README.md
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/README.md
@@ -1,10 +1,10 @@
 # [level 0] 뒤에서 5등 위로 - 181852 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181852?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181852?language=c) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.02 ms
+메모리: 4.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 26일 18:03:39
+2024년 12월 30일 23:35:30
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b){
+   return *(int*)a-*(int*)b; 
+}
+
+// num_list_len은 배열 num_list의 길이입니다.
+int* solution(int num_list[], size_t num_list_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* r=(int*)malloc(sizeof(int)*num_list_len);
+    qsort(num_list,num_list_len,sizeof(int),compare);
+    for(int i=5;i<num_list_len;i++)
+        r[i-5]=num_list[i];
+    return r;
+}
