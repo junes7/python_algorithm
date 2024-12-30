bbc286a1bda6152b09d44b0709fd142c7105dfed
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-30 15:51:13 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -5,6 +5,13 @@
 using namespace std;
 
 int solution(vector<int> num_list, int n) {
-    int r=find(num_list.begin(),num_list.end(),n)!=num_list.end()?1:0;
+    // int r=find(num_list.begin(),num_list.end(),n)!=num_list.end()?1:0;
+    int r=0;
+    for(int i=0;i<num_list.size();i++) {
+        if(num_list[i]==n) {
+            r=1;
+            break;
+        }
+    }
     return r;
 }
