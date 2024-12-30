f3cb222e9ee1ac4076f53233dd9e6d8a0e3983b0
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-30 18:54:42 +0900

    [level 0] Title: ad 제거하기, Time: 0.44 ms, Memory: 3.83 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cpp
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<string> solution(vector<string> strArr) {
     vector<string> r;
     for(int i=0;i<strArr.size();i++) {
-        if(strArr[i].find("ad")==-1)
+        if(strArr[i].find("ad")==string::npos)
             r.push_back(strArr[i]);
     }
     return r;
