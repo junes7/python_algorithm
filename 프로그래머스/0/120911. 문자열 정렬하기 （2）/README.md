fe25f6286a52d7e276087a1796b5fc073870c91a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 22:11:33 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.03 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/README.md
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 정렬하기 (2) - 120911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120911?language=c) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.02 ms
+메모리: 4.19 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 28일 13:08:12
+2024년 12월 31일 22:11:31
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char compare(const void* a, const void* b) {
+    return strcmp((char*)a,(char*)b);
+}
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* r=(char*)malloc(sizeof(char)*(strlen(my_string)+1));
+    for(int i=0;i<strlen(my_string);i++)
+        r[i]=isupper(my_string[i])?tolower(my_string[i]):my_string[i];
+    r[strlen(my_string)]='\0';
+    qsort(r,strlen(r),sizeof(char),compare);
+    return r;
+}
