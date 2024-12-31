b099e1445451c9fc730e4b36a6a1bb63b77e2e35
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 17:59:43 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -3,14 +3,19 @@
 
 using namespace std;
 
+int gcd(int x,int y) {
+    return (x%y==0)?y:gcd(y,x%y);
+}
+
 int solution(int n) {
-    int r=0,i=1;
-    while(true) {
-        if((6*i)%n==0) {
-            r=i;
-            break;   
-        }
-        i++;
-    }
+    // int r=0,i=1;
+    // while(true) {
+    //     if((6*i)%n==0) {
+    //         r=i;
+    //         break;   
+    //     }
+    //     i++;
+    // }
+    int r=n/gcd(6,n);
     return r;
 }
