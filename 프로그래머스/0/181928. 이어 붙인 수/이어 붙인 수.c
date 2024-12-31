a80afeb77c3f9e799dd874ef412bab478884e973
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 17:43:27 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.c
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.c
@@ -4,14 +4,18 @@
 
 // num_list_len은 배열 num_list의 길이입니다.
 int solution(int num_list[], size_t num_list_len) {
-    int r=0,oddi=0,eveni=0;
-    char* odd=(char*)malloc(sizeof(char)*5);
-    char* even=(char*)malloc(sizeof(char)*5);
+    // int r=0,oddi=0,eveni=0;
+    // char* odd=(char*)malloc(sizeof(char)*5);
+    // char* even=(char*)malloc(sizeof(char)*5);
+    // for(int i=0;i<num_list_len;i++)
+    //     if(num_list[i]%2==0)
+    //         even[eveni++]=num_list[i]+48;
+    //     else
+    //         odd[oddi++]=num_list[i]+48;
+    // r=atoi(even)+atoi(odd);
+    int r=0, num[2]={0,0};
     for(int i=0;i<num_list_len;i++)
-        if(num_list[i]%2==0)
-            even[eveni++]=num_list[i]+48;
-        else
-            odd[oddi++]=num_list[i]+48;
-    r=atoi(even)+atoi(odd);
+        num[num_list[i]%2]=num[num_list[i]%2]*10+num_list[i];
+    r=num[0]+num[1];
     return r;
 }
