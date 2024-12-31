c4ff14ffff333014ba4caaf5297a2bd000f4039e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 21:50:17 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.cpp
+++ 프로그래머스/0/120891. 369게임/369게임.cpp
@@ -5,10 +5,15 @@ using namespace std;
 
 int solution(int order) {
     int r=0;
-    string t=to_string(order);
-    for(int i=0;i<t.length();i++) {
-        if(t[i]=='3' or t[i]=='6' or t[i]=='9')
+    // string t=to_string(order);
+    // for(int i=0;i<t.length();i++) {
+    //     if(t[i]=='3' or t[i]=='6' or t[i]=='9')
+    //         r+=1;
+    // }
+    while(order>1) {
+        if(order%10>0 && order%10%3==0)
             r+=1;
+        order/=10;
     }
     return r;
 }
