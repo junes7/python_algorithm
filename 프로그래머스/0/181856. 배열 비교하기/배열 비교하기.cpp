c26db3e30e4cc21469e9d5ac6f20f4a936d43771
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 10:56:58 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cpp
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cpp
@@ -1,27 +1,22 @@
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 int solution(vector<int> arr1, vector<int> arr2) {
     int r=0,s1=0,s2=0;
     if(arr1.size()!=arr2.size()) {
-        // if(arr1.size() > arr2.size())
-        //     r=1;
-        // else
-        //     r=-1;
         r=(arr1.size()>arr2.size())?1:-1;
     } else {
-        for(int i=0;i<arr1.size();i++) {
-            s1+=arr1[i];
-            s2+=arr2[i];
-        }
-        // if(s1==s2)
-        //     r=0;
-        // else if(s1>s2)
-        //     r=1;
-        // else
-        //     r=-1;
+        // method1 for loop
+        // for(int i=0;i<arr1.size();i++) {
+        //     s1+=arr1[i];
+        //     s2+=arr2[i];
+        // }
+        // method2 accumulate function
+        s1=accumulate(arr1.begin(),arr1.end(),0);
+        s2=accumulate(arr2.begin(),arr2.end(),0);
         r=(s1==s2)?0:(s1>s2)?1:-1;
     }
     return r;
