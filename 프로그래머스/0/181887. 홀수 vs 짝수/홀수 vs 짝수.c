96783a5cedcd4ffdcf476f0e604a0720c95e3eef
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 14:39:50 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.c
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.c
@@ -5,8 +5,7 @@
 // num_list_len은 배열 num_list의 길이입니다.
 int solution(int num_list[], size_t num_list_len) {
     int r[]={0,0};
-    for(int i=0;i<num_list_len;i++) {
+    for(int i=0;i<num_list_len;i++)
         r[i%2]+=num_list[i];
-    }
     return r[0]>=r[1]?r[0]:r[1];
 }
