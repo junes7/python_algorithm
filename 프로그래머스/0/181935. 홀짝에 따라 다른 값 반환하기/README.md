236babe8400750c39aa19aa2fbb26d39eb74759e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 12:10:50 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/README.md
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 홀짝에 따라 다른 값 반환하기 - 181935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181935?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181935) 
 
 ### 성능 요약
 
-메모리: 3.68 MB, 시간: 0.01 ms
+메모리: 4.16 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 17일 15:46:44
+2024년 12월 31일 12:10:48
 
 ### 문제 설명
 
--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cpp
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cpp
@@ -5,15 +5,17 @@
 using namespace std;
 
 int solution(int n) {
-    int r = 0;
-    if(n%2==0) {
-        for(int i=2;i<n+1;i=i+2) {
-            r+=pow(i,2);
-        }
-    } else {
-        for(int i=1;i<n+1;i=i+2) {
-            r+=i;
-        }
-    }
+    int r=0;
+    // if(n%2==0) {
+    //     for(int i=2;i<n+1;i=i+2) {
+    //         r+=pow(i,2);
+    //     }
+    // } else {
+    //     for(int i=1;i<n+1;i=i+2) {
+    //         r+=i;
+    //     }
+    // }
+    for(int i=n%2;i<n+1;i+=2)
+        r+=n%2==0?pow(i,2):i;
     return r;
 }
