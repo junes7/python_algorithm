eafbb8c81d92c7f544baec5481e681eb9620080f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 12:57:13 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -1,9 +1,11 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(int slice, int n) {
-    int r=n/slice+((n%slice==0)?0:1);
+    int r=n/slice+(n%slice>0);
+    // int r=n/slice+((n%slice==0)?0:1);
     return r;
 }
