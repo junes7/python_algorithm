97fb344bef83776abe5932cf0d902f2eaece604b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 17:10:08 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -6,18 +6,18 @@ using namespace std;
 int solution(vector<int> num_list) {
     
     int r=0;
-    // string odd="", even="";
-    // for(int i=0;i<num_list.size();i++) {
-        // if(==0) {
-        //     even+=to_string(num_list[i]);    
-        // } else {
-        //     odd+=to_string(num_list[i]);
-        // }
-    // }
-    // r=stoi(odd)+stoi(even);
-    vector<string> num={"",""};
-    for(int i=0;i<num_list.size();i++)
-        num[num_list[i]%2]+=to_string(num_list[i]);
-    r=stoi(num[0])+stoi(num[1]);
+    string odd="", even="";
+    for(int i=0;i<num_list.size();i++) {
+        if(num_list[i]%2==0) {
+            even+=to_string(num_list[i]);    
+        } else {
+            odd+=to_string(num_list[i]);
+        }
+    }
+    r=stoi(odd)+stoi(even);
+    // vector<string> num={"",""};
+    // for(int i=0;i<num_list.size();i++)
+    //     num[num_list[i]%2]+=to_string(num_list[i]);
+    // r=stoi(num[0])+stoi(num[1]);
     return r;
 }
