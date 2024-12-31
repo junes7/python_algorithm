4190ca3f4f79c0059a1820cb602a23ee432626fb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 18:13:51 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/README.md
+++ 프로그래머스/0/120811. 중앙값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중앙값 구하기 - 120811 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120811?language=c) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120811) 
 
 ### 성능 요약
 
-메모리: 4.18 MB, 시간: 0.02 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 28일 17:36:58
+2024년 12월 31일 18:13:49
 
 ### 문제 설명
 
--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.c
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.c
@@ -2,18 +2,23 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+int compare(const void* a, const void* b) {
+    return *(int*)a-*(int*)b;
+}
+
 // array_len은 배열 array의 길이입니다.
 int solution(int array[], size_t array_len) {
     int r=0,t=0;
-    for(int i=0;i<array_len-1;i++) {
-        for(int j=0;j<array_len-1-i;j++) {
-            if(array[j]>array[j+1]) {
-                t=array[j];
-                array[j]=array[j+1];
-                array[j+1]=t;
-            }
-        }
-    }
+    // for(int i=0;i<array_len-1;i++) {
+    //     for(int j=0;j<array_len-1-i;j++) {
+    //         if(array[j]>array[j+1]) {
+    //             t=array[j];
+    //             array[j]=array[j+1];
+    //             array[j+1]=t;
+    //         }
+    //     }
+    // }
+    qsort(array,array_len,sizeof(int),compare);
     r=array[array_len/2];
     return r;
 }
