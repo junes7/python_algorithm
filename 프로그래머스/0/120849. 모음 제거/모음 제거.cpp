ee910f484f94167b262e9b84d80d570ecea3f57f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2024-12-31 15:14:57 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.cpp
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cpp
@@ -9,17 +9,17 @@ string solution(string my_string) {
     string vow="aeiou";
     for(int i=0;i<my_string.size();i++) {
         // method1 use for loop
-        // flag=true;
-        // for(int j=0;j<vow.size();j++)
-        //     if(my_string[i]==vow[j]) {
-        //         flag=false;
-        //         break;
-        //     }
-        // if(flag)
-        //     r+=my_string[i];
-        // method2 find function
-        if(vow.find(my_string[i])==string::npos)
+        flag=true;
+        for(int j=0;j<vow.size();j++)
+            if(my_string[i]==vow[j]) {
+                flag=false;
+                break;
+            }
+        if(flag)
             r+=my_string[i];
+        // method2 find function
+        // if(vow.find(my_string[i])==string::npos)
+        //     r+=my_string[i];
     }
     return r;
 }
