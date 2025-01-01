d43e466dce70094bdc7dff7f8574cc6e8d5bff3d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-01 18:38:39 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.21 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.c
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.c
@@ -7,10 +7,13 @@ int* solution(int arr[], size_t arr_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* r=(int*)malloc(sizeof(int)*2000);
     memset(r,0,sizeof(int)*2000);
-    int n=0;
-    while(pow(2,n)<arr_len)
-        n++;
+    int n=1;
+    while(n<arr_len)
+        n<<=1;
     for(int i=0;i<arr_len;i++)
         r[i]=arr[i];
+    for(int i=arr_len;i<n;i++)
+        r[i]=0;
+    r=(int*)realloc(r,sizeof(int)*n);
     return r;
 }
