9a237a718195f5dd54b7db53c46076cbbfb4f5fb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-01 11:59:28 +0900

    [level 0] Title: 글자 지우기, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.cpp
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.cpp
@@ -4,9 +4,22 @@
 using namespace std;
 
 string solution(string my_string, vector<int> indices) {
-    sort(indices.begin(),indices.end(),greater<int>());
-    for(int i:indices) {
-        my_string.erase(i,1);
+    sort(indices.begin(),indices.end(),less<int>());
+    bool flag;
+    string r="";
+    // for(int i:indices) {
+    //     my_string.erase(i,1);
+    // }
+    
+    for(int i=0;i<my_string.size();i++) {
+        flag=true;
+        for(int j=0;j<indices.size();j++)
+            if(i==indices[j]) {
+                flag=false;
+                break;
+            }
+        if(flag)
+            r+=my_string[i];    
     }
-    return my_string;
+    return r;
 }
