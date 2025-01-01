4b5a01ec387df6cb62dc2b3ff0b9a52d529b4823
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-01 16:54:33 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.36 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.c
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.c
@@ -6,7 +6,7 @@
 // flag_len은 배열 flag의 길이입니다.
 int* solution(int arr[], size_t arr_len, bool flag[], size_t flag_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* r=(int*)malloc(sizeof(int)*(3000));
+    int* r=(int*)malloc(sizeof(int)*(2000));
     int idx=0;
     for(int i=0;i<flag_len;i++) {
         if(flag[i]) {
@@ -16,6 +16,6 @@ int* solution(int arr[], size_t arr_len, bool flag[], size_t flag_len) {
             idx-=arr[i];
         }
     }
-    // r=(int*)realloc(r,sizeof(int)*idx);
+    r=(int*)realloc(r,sizeof(int)*idx);
     return r;
 }
