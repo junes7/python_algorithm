0364aaff857edab029b93041d28650a3bdae8e7f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-03 00:14:13 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 4.46 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.c
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.c
@@ -6,10 +6,9 @@
 char** solution(const char* my_str, int n) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     char** r=(char**)malloc(sizeof(char*)*101);
-    int l=strlen(my_str),len=l%n==0?l/n:l/n+1,d=0,cnt=0;
+    int l=strlen(my_str),len=l%n==0?l/n:l/n+1,cnt=0;
     for(int i=0;i<len;i++) {
         r[i]=(char*)malloc(sizeof(char)*(n+1));
-        // d=n*i+n>=l?l-n*i+1:n;
         cnt=0;
         for(int j=n*i;j<n*i+n;j++)
             r[i][cnt++]=my_str[j];
