1280717c5f8380f87fe491d490a68966bc745ebf
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-02 16:54:37 +0900

    [level 0] Title: k의 개수, Time: 0.85 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/README.md
+++ 프로그래머스/0/120887. k의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] k의 개수 - 120887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 15.69 ms
+메모리: 4.15 MB, 시간: 0.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 01일 12:57:56
+2025년 01월 02일 16:54:35
 
 ### 문제 설명
 
--- 프로그래머스/0/120887. k의 개수/k의 개수.cpp
+++ 프로그래머스/0/120887. k의 개수/k의 개수.cpp
@@ -4,17 +4,27 @@
 using namespace std;
 
 int solution(int i, int j, int k) {
-    int r=0;
+    int r=0,tem;
     string ch="";
+    // for(int t=i;t<j+1;t++) {
+    //     if(to_string(t).find(to_string(k))!=-1) {
+    //         ch=to_string(t);
+    //         for(int u=0;u<ch.size();u++) {
+    //             if(ch[u]==char(48+k)) {
+    //                 r+=1;
+    //             }
+    //         }
+    //     }
+    // }
     for(int t=i;t<j+1;t++) {
-        if(to_string(t).find(to_string(k))!=-1) {
-            ch=to_string(t);
-            for(int u=0;u<ch.size();u++) {
-                if(ch[u]==char(48+k)) {
-                    r+=1;
-                }
-            }
+        tem=t;
+        while(tem>=10) {
+            if(tem%10==k)
+                r+=1;
+            tem/=10;
         }
+        if(tem==k)
+            r+=1;
     }
     return r;
 }
