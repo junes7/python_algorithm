5d62179e38ef5f602f06cf21e504f2e7a8a77c67
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-02 16:14:13 +0900

    [level 0] Title: 2의 영역, Time: 23.99 ms, Memory: 23.9 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.c
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.c
@@ -7,26 +7,20 @@ int* solution(int arr[], size_t arr_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* r=(int*)malloc(sizeof(int)*arr_len);
     memset(r,0,sizeof(int)*arr_len);
-    int start=0,end=0;
-    for(int i=0;i<=arr_len;i++) {
-        if(arr[i]==2) {   
-            start=i;
-            break;
-        }   
+    int idx[2]={-1,-1};
+    for(int i=0;i<arr_len+1;i++) {
+        if(arr[i]==2 && idx[0]==-1) {
+            idx[0]=i;
+        }
+        if(arr[arr_len-1-i]==2 && idx[1]==-1) {
+            idx[1]=arr_len-1-i;
+        }
     }
-    for(int i=arr_len-1;i>=0;i--) {
-        if(arr[i]==2) {   
-            end=i;
-            break;
-        }   
-    }
-    // printf("%d %d",start,end);
-    if(start==0 && end==0)
+    if(idx[0]==-1 && idx[1]==-1)
         r[0]=-1;
     else {
-        for(int i=start;i<end+1;i++) {
-            r[i-start]=arr[i];
-        }
+        for(int i=idx[0];i<idx[1]+1;i++)
+            r[i-idx[0]]=arr[i];
     }
     return r;
 }
--- 프로그래머스/0/181894. 2의 영역/README.md
+++ 프로그래머스/0/181894. 2의 영역/README.md
@@ -1,10 +1,10 @@
 # [level 0] 2의 영역 - 181894 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181894?language=c) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181894?language=c#) 
 
 ### 성능 요약
 
-메모리: 22.3 MB, 시간: 38.62 ms
+메모리: 23.9 MB, 시간: 23.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 02일 15:10:41
+2025년 01월 02일 16:14:11
 
 ### 문제 설명
 
