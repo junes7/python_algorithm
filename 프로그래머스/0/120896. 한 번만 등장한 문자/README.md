0ffa9814585a840ed1592998f27f2ebdef70b469
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-02 12:11:08 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/README.md
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 한 번만 등장한 문자 - 120896 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120896) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120896?language=c) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.04 ms
+메모리: 4.21 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 01일 23:31:58
+2025년 01월 02일 12:11:05
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* s) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* r=(char*)malloc(sizeof(char)*(strlen(s)+1));
+    memset(r,0,sizeof(char)*(strlen(s)+1));
+    int cnt=0,al[26]={0};
+    for(int i=0;i<strlen(s);i++)
+        al[s[i]-'a']+=1;
+    for(int i=0;i<sizeof(al)/sizeof(int);i++)
+        if(al[i]==1)
+            r[cnt++]=97+i;
+    return r;
+}
