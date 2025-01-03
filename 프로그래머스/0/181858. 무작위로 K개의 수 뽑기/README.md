fd77b0b81cb59d1d7e8b5a8d1886b344e511509a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-03 23:38:49 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 5.58 ms, Memory: 4.5 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/README.md
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 무작위로 K개의 수 뽑기 - 181858 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181858?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181858?language=c) 
 
 ### 성능 요약
 
-메모리: 4.27 MB, 시간: 7.85 ms
+메모리: 4.5 MB, 시간: 5.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 03일 17:39:35
+2025년 01월 03일 23:38:47
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+int* solution(int arr[], size_t arr_len, int k) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* r=(int*)malloc(sizeof(int)*k);
+    int idx=0,len;
+    bool flag;
+    for(int i=0;i<arr_len;i++) {
+        flag=true;
+        for(int j=0;j<idx;j++) {
+            if(r[j]==arr[i]) {
+                flag=false;
+                break;
+            }
+        }
+        if(flag &&idx<k)
+            r[idx++]=arr[i];
+    }
+    len=k-idx;
+    if(len>0) {
+        for(int i=0;i<len;i++)
+            r[idx++]=-1;
+    }
+    return r;
+}
