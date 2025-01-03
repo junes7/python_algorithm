f9bdf2b35a233a5a57b02de0e33f22339442b15f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-03 12:27:23 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.c
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.c
@@ -4,6 +4,7 @@
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* ineq, const char* eq, int n, int m) {
-    int r=*ineq=='<'?*eq=='='?n<=m:n<m:*eq=='='?n>=m:n>m;
+    // int r=*ineq=='<'?*eq=='='?n<=m:n<m:*eq=='='?n>=m:n>m;
+    int r=!strcmp(ineq,"<")?!strcmp(eq,"=")?n<=m:n<m:!strcmp(eq,"=")?n>=m:n>m;
     return r;
 }
