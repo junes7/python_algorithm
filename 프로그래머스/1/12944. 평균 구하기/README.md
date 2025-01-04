06d066af17bd75e1e0d0aad97aa35da6c5e51c2c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-04 23:26:10 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/README.md
+++ 프로그래머스/1/12944. 평균 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 평균 구하기 - 12944 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12944?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12944) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.01 ms
+메모리: 3.82 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 07일 23:49:37
+2025년 01월 04일 23:26:08
 
 ### 문제 설명
 
--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -1,14 +1,16 @@
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 double solution(vector<int> arr) {
     double r=0;
-    int s=0;
-    for(int i=0;i<arr.size();i++) {
-        s+=arr[i];
-    }
+    // int s=0;
+    // for(int i=0;i<arr.size();i++) {
+    //     s+=arr[i];
+    // }
+    int s=accumulate(arr.begin(),arr.end(),0);
     r=(double)s/arr.size();
     return r;
 }
