8f64ed3c323f0b4685b9efa9f76345396b89dd4e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-05 16:59:50 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.38 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.24 ms
+메모리: 4.2 MB, 시간: 0.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 10일 14:29:07
+2025년 01월 05일 16:59:48
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    return *(int*)a-*(int*)b;
+}
+
+// numbers_len은 배열 numbers의 길이입니다.
+int* solution(int numbers[], size_t numbers_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* r=(int*)malloc(sizeof(int)*(numbers_len*102));
+    memset(r,0,sizeof(int)*(numbers_len*102));
+    bool flag;
+    int idx=0;
+    for(int i=0;i<numbers_len-1;i++) {
+        for(int j=i+1;j<numbers_len;j++) {
+            flag=true;
+            for(int k=0;k<idx;k++) {
+                if(r[k]==numbers[i]+numbers[j]) {
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag)
+                r[idx++]=numbers[i]+numbers[j];
+        }
+    }
+    // r=(int*)realloc(r,sizeof(int)*idx);
+    qsort(r,idx,sizeof(int),compare);
+    return r;
+}
