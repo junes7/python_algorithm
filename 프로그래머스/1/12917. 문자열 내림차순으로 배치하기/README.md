69b25713596cb9b711f505d452a7e07bb380bfbf
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-05 15:20:18 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내림차순으로 배치하기 - 12917 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.06 ms
+메모리: 4.09 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 05일 15:17:15
+2025년 01월 05일 15:20:16
 
 ### 문제 설명
 
--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
@@ -5,15 +5,18 @@
 using namespace std;
 
 string solution(string s) {
-    string r="",upper,lower;
-    for(int i=0;i<s.length();i++) {
-        if(isupper(s[i]))
-            upper+=s[i];
-        else
-            lower+=s[i];
-    }
-    sort(upper.begin(),upper.end(),greater<int>());
-    sort(lower.begin(),lower.end(),greater<int>());
-    r+=lower+upper;
+    string r="";
+    r=s;
+    sort(r.begin(),r.end(),greater<int>());
+    // string upper,lower;
+    // for(int i=0;i<s.length();i++) {
+    //     if(isupper(s[i]))
+    //         upper+=s[i];
+    //     else
+    //         lower+=s[i];
+    // }
+    // sort(upper.begin(),upper.end(),greater<int>());
+    // sort(lower.begin(),lower.end(),greater<int>());
+    // r+=lower+upper;
     return r;
 }
