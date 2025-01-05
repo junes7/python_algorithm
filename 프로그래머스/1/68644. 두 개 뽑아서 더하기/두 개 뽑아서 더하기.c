d9c619637425ee833d763f711d39ea64d843208a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-05 17:02:48 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.40 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.c
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.c
@@ -9,8 +9,8 @@ int compare(const void* a, const void* b) {
 // numbers_len은 배열 numbers의 길이입니다.
 int* solution(int numbers[], size_t numbers_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* r=(int*)malloc(sizeof(int)*(numbers_len*102));
-    memset(r,0,sizeof(int)*(numbers_len*102));
+    int* r=(int*)malloc(sizeof(int)*(numbers_len*200));
+    memset(r,0,sizeof(int)*(numbers_len*200));
     bool flag;
     int idx=0;
     for(int i=0;i<numbers_len-1;i++) {
@@ -26,7 +26,6 @@ int* solution(int numbers[], size_t numbers_len) {
                 r[idx++]=numbers[i]+numbers[j];
         }
     }
-    // r=(int*)realloc(r,sizeof(int)*idx);
     qsort(r,idx,sizeof(int),compare);
     return r;
 }
