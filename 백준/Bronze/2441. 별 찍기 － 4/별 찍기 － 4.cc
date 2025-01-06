de66d184d4fe27e6bf05835723d6751beb73a6cd
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-06 23:37:13 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<i;j++)
+            printf(" ");
+        for(int j=0;j<n-i;j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
