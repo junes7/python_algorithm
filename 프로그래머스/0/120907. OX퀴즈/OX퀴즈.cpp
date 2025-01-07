3ce882e8e38854c3e0ad69cc847d967694d6cfbb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-07 18:06:21 +0900

    [level 0] Title: OX퀴즈, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.cpp
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 
 vector<string> solution(vector<string> quiz) {
-    vector<string> r;
+    vector<string> r,t;
     string buffer;
-    vector<string> t;
     for(int i=0;i<quiz.size();i++) {
-        stringstream ss(quiz[i]);
         t={};
+        stringstream ss(quiz[i]);
         while(getline(ss,buffer,' ')) {
             t.push_back(buffer);
         }
         if(t[1]=="+") {
-            if(stoi(t[0])+stoi(t[2])==stoi(t[t.size()-1]))
-                r.push_back("O");
-            else
-                r.push_back("X");
+            // if(stoi(t[0])+stoi(t[2])==stoi(t[t.size()-1]))
+            //     r.push_back("O");
+            // else
+            //     r.push_back("X");
+            r.push_back(stoi(t[0])+stoi(t[2])==stoi(t[t.size()-1])?"O":"X");
         } else {
-            if(stoi(t[0])-stoi(t[2])==stoi(t[t.size()-1]))
-                r.push_back("O");
-            else
-                r.push_back("X");
+            // if(stoi(t[0])-stoi(t[2])==stoi(t[t.size()-1]))
+            //     r.push_back("O");
+            // else
+            //     r.push_back("X");
+            r.push_back(stoi(t[0])-stoi(t[2])==stoi(t[t.size()-1])?"O":"X");
         }
-        
     }
     return r;
 }
--- 프로그래머스/0/120907. OX퀴즈/README.md
+++ 프로그래머스/0/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level 0] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
 
 ### 성능 요약
 
-메모리: 4.02 MB, 시간: 0.05 ms
+메모리: 4.15 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 06일 00:47:26
+2025년 01월 07일 18:06:19
 
 ### 문제 설명
 
