b1c6bcae32e6dafca4503ecaf1721a4001d657c0
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-09 00:45:42 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int x,y;
+    scanf("%d %d",&x,&y);
+    if(x>0 && y>0)
+        printf("%d\n",1);
+    else if(x<0 && y>0)
+        printf("%d\n",2);
+    else if(x<0 && y<0)
+        printf("%d\n",3);
+    else
+        printf("%d\n",4);
+    return 0;
+}
