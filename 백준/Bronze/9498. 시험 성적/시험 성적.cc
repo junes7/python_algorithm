9a3fca3c99c658a917c418e8a231b3004da88859
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-08 23:56:50 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int s;
+    cin>>s;
+    if(s>=90)
+        cout<<"A"<<endl;
+    else if(s>=80)
+        cout<<"B"<<endl;
+    else if(s>=70)
+        cout<<"C"<<endl;
+    else if(s>=60)
+        cout<<"D"<<endl;
+    else
+        cout<<"F"<<endl;
+    return 0;
+}
