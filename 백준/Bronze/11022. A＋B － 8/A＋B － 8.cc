8ec6745648ca51eb5207cb7faa2f1909b0e8df65
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-08 23:45:31 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n, a, b;
+    cin >> n;
+    for (int i = 1; i < n + 1; i++) {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << endl;
+    }
+    return 0;
+}
