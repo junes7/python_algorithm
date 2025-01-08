d222d644170656ea12d4e73ff768ee2d3d0236d4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-08 23:47:46 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int n,a,b;
+    scanf("%d",&n);
+    for (int i=1;i<n+1;i++) {
+        scanf("%d %d",&a,&b);
+        printf("Case #%d: %d + %d = %d\n",i,a,b,a+b);
+    }
+    return 0;
+}
