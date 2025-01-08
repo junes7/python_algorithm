660328443e15ba1e0251c9fcb9b93db677b5c979
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-08 12:52:04 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    long long s = 0, num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    s = num1 + num2 + num3;
+    cout << s << endl;
+    return 0;
+}
