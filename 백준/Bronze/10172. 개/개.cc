ad7059176d8755949505e98ac84f3b1f2e778345
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-08 14:18:56 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string t = "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n";
+    cout << t << endl;
+    return 0;
+}
