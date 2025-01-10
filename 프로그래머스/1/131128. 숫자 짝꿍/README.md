3fe50a0d91305b0ad1992d0a4e329206660fb469
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-10 18:33:42 +0900

    [level 1] Title: 숫자 짝꿍, Time: 7.35 ms, Memory: 29.1 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/README.md
+++ 프로그래머스/1/131128. 숫자 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 짝꿍 - 131128 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128?language=c) 
 
 ### 성능 요약
 
-메모리: 29 MB, 시간: 92.71 ms
+메모리: 29.1 MB, 시간: 7.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 14일 23:49:21
+2025년 01월 10일 18:33:40
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* X, const char* Y) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* r=(char*)malloc(sizeof(char)*strlen(X));
+    memset(r,0,sizeof(char)*strlen(X));
+    int xnum[10],ynum[10],minn,idx=0;
+    memset(xnum,0,sizeof(int)*10);
+    memset(ynum,0,sizeof(int)*10);
+    for(int i=0;i<strlen(X);i++) {
+        xnum[X[i]-48]+=1;
+    }
+    for(int i=0;i<strlen(Y);i++) {
+        ynum[Y[i]-48]+=1;
+    }
+    for(int i=9;i>=0;i--) {
+        minn=xnum[i]<ynum[i]?xnum[i]:ynum[i];
+        for(int j=0;j<minn;j++) {
+            r[idx++]=i+48;
+        }
+    }
+    // printf("idx: %d\n",idx);
+    if(idx==0) {
+        strcpy(r,"-1");
+    }
+    if(r[0]=='0' && idx>1) {
+        strcpy(r,"0");
+    }
+    return r;
+}
