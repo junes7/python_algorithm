0481e7dae0fc3c5b5b9def875b51621aca3181ca
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-10 17:18:22 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,6 +1,6 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=c) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 10일 17:17:48
+2025년 01월 10일 17:18:20
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.c
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.c
@@ -7,14 +7,27 @@ char* solution(const char* s, const char* skip, int index) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     char* r=(char*)malloc(sizeof(char)*strlen(s)),ch[2];
     strcpy(r,s);
-    ch[1]='\0';
-    for(int i=0;i<strlen(s);i++) {
+    bool flag;
+    printf("%s\n",r);
+    for(int i=0;i<strlen(r);i++) {
         for(int j=0;j<index;j++) {
             r[i]=(r[i]-97+1)%26+97;
+            flag=false;
             ch[0]=r[i];
-            while(strstr(skip,ch)!=NULL) {
-                r[i]=(r[i]-97+1)%26+97;
-                ch[0]=r[i];
+            ch[1]='\0';
+            // printf("%s %s\n",ch,strstr(skip,ch));
+            while(true) {
+                for(int k=0;k<strlen(skip);k++) {
+                    if(skip[k]==r[i]) {
+                        flag=true;
+                        break;
+                    }
+                }
+                // printf("%s %s %d\n",ch,r,flag);
+                if(flag)
+                    r[i]=(r[i]-97+1)%26+97;
+                else
+                    break;
             }
         }
     }
