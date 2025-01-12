27a262034ea04f90e34745932a637147fb3557fb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-12 18:09:11 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n/4;i++) {
+        printf("long ");
+    }
+    printf("int");
+    return 0;
+}
