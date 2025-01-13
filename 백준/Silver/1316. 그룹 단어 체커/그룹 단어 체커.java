585c91e3240c27db137d7c1819956c9fa2b3558f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-13 21:34:08 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 72 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException{
+		int count = 0;
+		int N = Integer.parseInt(br.readLine());
+		for(int i=0; i<N; i++) {
+			if(check()==true) {
+				count++;
+			}	
+		}
+		System.out.println(count);
+	}
+	public static boolean check() throws IOException {
+		boolean[] check = new boolean[26];
+		String str = br.readLine();
+		int prev = 0;
+		for(int i=0; i<str.length(); i++) {
+			int now = str.charAt(i);
+			if(prev != now) {
+				if(check[now - 'a'] == false) {	
+					check[now - 'a'] = true;
+					prev = now;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				continue;
+			}	
+		}
+		return true;
+	}
+}
