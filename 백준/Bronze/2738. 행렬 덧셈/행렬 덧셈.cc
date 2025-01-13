5d332dc7b57a49a7d16829c776a86551169df3aa
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-13 12:52:14 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    int n, m, elem, idx = 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n >> m;
+    vector<vector<vector<int>>> mat;
+    vector<vector<int>> med;
+    vector<int> temp;
+    string t;
+    getline(cin, t);
+    for (int i = 0; i < 2; i++) {
+        med = {};
+        for (int j = 0; j < n; j++) {
+            temp = {};
+            getline(cin, t);
+            stringstream ss(t);
+            while (ss >> elem) {
+                temp.push_back(elem);
+            }
+            med.push_back(temp);
+        }
+        mat.push_back(med);
+    }
+    for (int j = 0; j < n; j++) {
+        for (int k = 0; k < m; k++) {
+            mat[0][j][k] += mat[1][j][k];
+            cout << mat[0][j][k] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
