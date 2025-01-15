af8660ec55ef265d21ca6e69ceaee4fc3e64f82e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-15 22:27:06 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a, b;
+    while (1) {
+        cin >> a >> b;
+        if (a == 0 && b == 0)
+            break;
+        if (a % b == 0)
+            cout << "multiple" << endl;
+        else if (b % a == 0)
+            cout << "factor" << endl;
+        else
+            cout << "neither" << endl;
+    }
+    return 0;
+}
