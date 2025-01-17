021ace5de3cceb14d00d6658915363c9b70e0651
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-17 23:31:42 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2108 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int st, end, j;
+    scanf("%d %d", &st, &end);
+    bool *arr = (bool *)malloc(sizeof(bool) * 1000000);
+    for (int i = 0; i < end + 1; i++) {
+        arr[i] = true;
+    }
+    for (int i = 2; i < (int)sqrt(end) + 1; i++) {
+        if (arr[i] == true) {
+            j = 2;
+            while (i * j <= end) {
+                arr[i * j] = false;
+                j += 1;
+            }
+        }
+    }
+    if(st==1) st+=1;
+    for (int i = st; i < end + 1; i++) {
+        if (arr[i] == true)
+            printf("%d\n", i);
+    }
+    return 0;
+}
