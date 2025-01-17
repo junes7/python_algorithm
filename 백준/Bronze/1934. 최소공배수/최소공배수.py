b07692e4ca41ba4496d7d26b5a4567bb4e897629
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-17 18:26:07 +0900

    [Bronze I] Title: 최소공배수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,5 +1,8 @@
-import sys,math
+import sys
+def gcd(x,y):
+    return y if x%y==0 else gcd(y,x%y)
+
 n=int(sys.stdin.readline())
 for i in range(n):
     a,b=map(int,sys.stdin.readline().split())
-    print(a*b//math.gcd(a,b))
+    print(a*b//gcd(a,b))
