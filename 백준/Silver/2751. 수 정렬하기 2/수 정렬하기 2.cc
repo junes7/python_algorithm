e6b8f7a4c7733b156e0cdb5b7785d14cdf18f8cd
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-19 19:03:56 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1740 ms, Memory: 48884 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(void) {
+    int n, elem;
+    cin >> n;
+    map<int, int> num;
+    for (int i = 0; i < n; i++) {
+        cin >> elem;
+        if (num.find(elem) == num.end()) {
+            num.insert({elem, 1});
+        } else {
+            num[elem] += 1;
+        }
+    }
+    map<int, int>::iterator iter;
+    for (iter = num.begin(); iter != num.end(); iter++) {
+        cout << iter->first << "\n";
+    }
+    return 0;
+}
