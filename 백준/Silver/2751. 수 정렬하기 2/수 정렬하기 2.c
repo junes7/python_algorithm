57cb55dd2b5e26e449293c61eb3d2d6951c04bc0
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-19 16:17:49 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 420 ms, Memory: 8928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int compare(const void *a, const void *b) {
+    return *(int *)a - *(int *)b;
+}
+int main(void) {
+    int n, elem, *num = (int *)malloc(sizeof(int) * 1000000);
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &elem);
+        num[i] = elem;
+    }
+    qsort(num, n, sizeof(int), compare);
+    for (int i = 0; i < n; i++)
+        printf("%d\n", num[i]);
+    return 0;
+}
