0161eee8540da1284aee32b9de99ae8149c5f609
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-22 23:17:44 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 76 ms, Memory: 5716 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int compare(const void *pa, const void *pb) {
+    const int *a = *(int **)pa;
+    const int *b = *(int **)pb;
+    if (a[0] == b[0])
+        return a[1] > b[1];
+    return a[0] > b[0];
+}
+int main(void) {
+    int n, **num = (int **)malloc(sizeof(int *) * 100000);
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        num[i] = (int *)malloc(sizeof(int) * 2);
+        scanf("%d %d", &num[i][0], &num[i][1]);
+    }
+    qsort(num, n, sizeof(int *), compare);
+    for (int i = 0; i < n; i++) {
+        printf("%d %d\n", num[i][0], num[i][1]);
+    }
+    return 0;
+}
