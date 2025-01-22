9ec88df80cdec0e1233060ad35ce6f4021bd419d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-22 21:52:26 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 3040 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
-#include <set>
 using namespace std;
 
 int main(void) {
@@ -10,20 +9,16 @@ int main(void) {
     cin.tie(NULL);
     cin >> n;
     map<int, int> num;
-    set<int> el;
     for (int i = 0; i < n; i++) {
         cin >> elem;
-        el.insert(elem);
         if (num.find(elem) == num.end())
             num.insert(make_pair(elem, 1));
         else
             num[elem] += 1;
     }
-    set<int>::iterator iter;
-    for (iter = el.begin(); iter != el.end(); iter++) {
-        for (int j = 0; j < num[*iter]; j++) {
-            cout << *iter << "\n";
-        }
-    }
+    map<int, int>::iterator iter;
+    for (iter = num.begin(); iter != num.end(); iter++)
+        for (int j = 0; j < iter->second; j++)
+            cout << iter->first << "\n";
     return 0;
 }
