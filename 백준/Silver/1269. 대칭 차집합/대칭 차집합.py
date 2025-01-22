fe72affd7b854226d8d924408ea2c101b26e5a5c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-22 11:53:12 +0900

    [Silver IV] Title: 대칭 차집합, Time: 272 ms, Memory: 112820 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -1,6 +1,5 @@
 import sys
 n,m=map(int,sys.stdin.readline().split())
-a=list(map(int,sys.stdin.readline().split()))
-b=list(map(int,sys.stdin.readline().split()))
-diffl,diffr=list(set(a)-set(b)),list(set(b)-set(a))
-print(len(diffl)+len(diffr))
+a=set(map(int,sys.stdin.readline().split()))
+b=set(map(int,sys.stdin.readline().split()))
+print(len(a-b|b-a))
