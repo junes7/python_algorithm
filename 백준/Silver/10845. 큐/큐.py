af11778fc88fae733bbaa2df52d369c9e83e38f1
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-25 17:48:23 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+n=int(sys.stdin.readline())
+dq=deque()
+for i in range(n):
+    cmd=list(sys.stdin.readline().split())
+    if cmd[0]=="push":
+        dq+=[int(cmd[1])]
+    elif cmd[0]=="pop":
+        print(-1 if len(dq)==0 else dq.popleft())
+    elif cmd[0]=="size":
+        print(len(dq))
+    elif cmd[0]=="empty":
+        print(1 if len(dq)==0 else 0)
+    elif cmd[0]=="front":
+        print(-1 if len(dq)==0 else dq[0])
+    elif cmd[0]=="back":
+        print(-1 if len(dq)==0 else dq[-1])
