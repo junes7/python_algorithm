2fdb0fea0be65b9db5c2c20975362d21c7c6a428
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-27 22:21:26 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,10 +1,9 @@
-n=int(input())
-num=[[25,0],[10,0],[5,0],[1,0]]
+import sys
+n=int(sys.stdin.readline())
+coins=[25,10,5,1]
 for i in range(n):
-    t=int(input())
-    for j in range(len(num)):
-        num[j][1]=t//num[j][0]
-        t%=num[j][0]
-    for i in range(len(num)):
-        print(num[i][1],end=" ");
+    rem=int(sys.stdin.readline())
+    for coin in coins:
+        print(rem//coin,end=' ')
+        rem%=coin
     print()
