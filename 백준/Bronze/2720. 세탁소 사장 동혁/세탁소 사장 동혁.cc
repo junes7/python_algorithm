7aea31adabbd1e562c825a7f64b8ffed5671da3e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-27 22:26:31 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -3,19 +3,18 @@
 using namespace std;
 
 int main(void) {
-    vector<vector<int>> num = {{25, 0}, {10, 0}, {5, 0}, {1, 0}};
-    int n, t;
+    int n, rem;
+    vector<int> coins = {25, 10, 5, 1};
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> t;
-        for (int j = 0; j < num.size(); j++) {
-            num[j][1] = t / num[j][0];
-            t %= num[j][0];
+        cin >> rem;
+        for (int j = 0; j < coins.size(); j++) {
+            cout << rem / coins[j] << ' ';
+            rem %= coins[j];
         }
-        for (int j = 0; j < num.size(); j++) {
-            cout << num[j][1] << ' ';
-        }
-        cout << endl;
+        cout << "\n";
     }
     return 0;
 }
