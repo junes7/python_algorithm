4a862ce980050208f1d56533a09fdbaa6ad03965
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-27 22:31:03 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.c
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-
 int main(void) {
-    int n, t, num[4][2] = {{25, 0}, {10, 0}, {5, 0}, {1, 0}};
+    int n, rem, coins[4] = {25, 10, 5, 1};
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &t);
-        for (int i = 0; i < 4; i++) {
-            num[i][1] = t / num[i][0];
-            t %= num[i][0];
+        scanf("%d", &rem);
+        for (int j = 0; j < sizeof(coins) / sizeof(int); j++) {
+            printf("%d ", rem / coins[j]);
+            rem %= coins[j];
         }
-        for (int i = 0; i < 4; i++)
-            printf("%d ", num[i][1]);
         printf("\n");
     }
     return 0;
