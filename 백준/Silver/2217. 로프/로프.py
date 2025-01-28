719d89d2da537eef0b60404e7e99ac20490aa9f1
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-28 19:45:29 +0900

    [Silver IV] Title: 로프, Time: 100 ms, Memory: 37048 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -2,7 +2,7 @@ import sys
 n=int(sys.stdin.readline())
 ropes=sorted([int(sys.stdin.readline()) for i in range(n)])
 best=0
-for i in range(n-1,-1,-1):
+for i in range(n):
     if best<ropes[i]*(n-i):
         best=ropes[i]*(n-i)
 print(best)
