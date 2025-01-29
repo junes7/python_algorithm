89f07b93e36e048393e57aef0b46311ba2fb31ec
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-29 23:22:14 +0900

    [Silver IV] Title: 수 찾기, Time: 452 ms, Memory: 49136 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,20 +1,18 @@
 def binarySearch(board,target,start,end):
-    if start > end:
-        return 0
-    mid=(start+end)//2
-    if board[mid]==target:
-        return 1
-    elif board[mid]<target:
-        return binarySearch(board,target,mid+1,end)
-    else:
-        return binarySearch(board,target,start,mid-1)
-
+    while start<=end:
+        mid=(start+end)//2
+        if board[mid]==target:
+            return 1
+        elif board[mid]<target:
+            start=mid+1
+        else:
+            end=mid-1
+    return 0
 import sys
 n=int(sys.stdin.readline())
 a=list(map(int,sys.stdin.readline().split()))
 m=int(sys.stdin.readline())
 tar=list(map(int,sys.stdin.readline().split()))
 a.sort()
-start,end=0,n-1
 for i in tar:
-    print(binarySearch(a,i,start,end))
+    print(binarySearch(a,i,0,n-1))
