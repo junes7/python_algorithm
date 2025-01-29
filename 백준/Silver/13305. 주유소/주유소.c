71feeadd59d284ca118121eff2a3fcb5c56ee633
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-29 14:50:47 +0900

    [Silver III] Title: 주유소, Time: 36 ms, Memory: 2680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.c
+++ 백준/Silver/13305. 주유소/주유소.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void) {
-    int n, elem, minprice, money = 0;
-    scanf("%d", &n);
-    int *distances = (int *)malloc(sizeof(int) * (n - 1));
-    int *prices = (int *)malloc(sizeof(int) * n);
+    long n, elem;
+    long long minprice, money = 0;
+    scanf("%ld", &n);
+    long *distances = (long *)malloc(sizeof(long) * (n - 1));
+    long *prices = (long *)malloc(sizeof(long) * n);
     for (int i = 0; i < n - 1; i++) {
-        scanf("%d", &elem);
+        scanf("%ld", &elem);
         distances[i] = elem;
     }
     for (int i = 0; i < n; i++) {
-        scanf("%d", &elem);
+        scanf("%ld", &elem);
         prices[i] = elem;
     }
     minprice = prices[0];
@@ -19,6 +20,6 @@ int main(void) {
         if (minprice >= prices[i])
             minprice = prices[i];
     }
-    printf("%d", money);
+    printf("%lld", money);
     return 0;
 }
