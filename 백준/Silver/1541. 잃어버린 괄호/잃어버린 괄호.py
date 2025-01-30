017cea9807083c28b03cb71e691b483ff29b9ef9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-30 12:11:04 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,14 +1,9 @@
 import sys
-op,num=[],[]
+num=[]
 t=sys.stdin.readline().split()[0]
-if t.find('-')==-1:
-    num+=list(map(int,t.split('+')))
-    op=['+']
-else:
-    t=t.split('-')
-    op=['-']
-    num+=[int(i) if i.isdigit() else sum(list(map(int,i.split('+')))) for i in t]
+t=t.split('-')
+num+=[int(i) if i.isdigit() else sum(list(map(int,i.split('+')))) for i in t]
 s=num[0]
 for i in range(1,len(num)):
-    s=s+num[i] if op[0]=='+' else s-num[i]
+    s-=num[i]
 print(s)
