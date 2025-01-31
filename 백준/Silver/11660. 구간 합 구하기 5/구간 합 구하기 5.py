461e27983b588afbe9c855f8cadd99d9f9157c16
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-01-31 16:41:17 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 968 ms, Memory: 73364 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -4,7 +4,7 @@ num=[[0]*(n+1)]
 num+=[[0]+list(map(int,sys.stdin.readline().rstrip().split())) for i in range(n)]
 for i in range(1,n+1):
     for j in range(1,n+1):
-        num[i][j]=num[i][j]+num[i-1][j]+num[i][j-1]-num[i-1][j-1];
+        num[i][j]+=num[i-1][j]+num[i][j-1]-num[i-1][j-1]
 for i in range(m):
     pt=list(map(int,sys.stdin.readline().rstrip().split()))
     print(num[pt[2]][pt[3]]-num[pt[2]][pt[1]-1]-num[pt[0]-1][pt[3]]+num[pt[0]-1][pt[1]-1])
