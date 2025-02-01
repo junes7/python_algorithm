e4bdd690be0e7f82844159f46aecd6ab23ec1b82
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-02 00:25:12 +0900

    [Gold IV] Title: 좋은 친구, Time: 192 ms, Memory: 5108 KB -BaekjoonHub

---
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void) {
+    int n, k;
+    long long cnt = 0;
+    string t;
+    vector<int> name, len_name;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        name.push_back(t.size());
+    }
+    for (int i = 0; i < 21; i++) {
+        len_name.push_back(0);
+    }
+    for (int i = 0; i < n; i++) {
+        if (k < i)
+            len_name[name[i - k - 1]] -= 1;
+        cnt += len_name[name[i]];
+        len_name[name[i]] += 1;
+    }
+    cout << cnt;
+    return 0;
+}
