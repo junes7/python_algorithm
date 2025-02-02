caf4f35e9753c769ca96e3100291ff4c72c68cf6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-02 12:57:34 +0900

    [Silver II] Title: DNA 비밀번호, Time: 320 ms, Memory: 114136 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -8,19 +8,18 @@ def add_dna(d):
     for i in range(4):
         if d==chk_dna[i]:
             cnt_dna[i]+=1
-def del_dna(d):
-    for i in range(4):
-        if d==chk_dna[i]:
-            cnt_dna[i]-=1
 def check_dna():
     for i in range(4):
         if cnt_dna[i]<max_dna[i]:
             return False
     return True
+def del_dna(d):
+    for i in range(4):
+        if d==chk_dna[i]:
+            cnt_dna[i]-=1
 for i in dna[:p]:
     add_dna(i)
     cnt=1 if check_dna() else 0
-
 for i in range(p,s):
     del_dna(dna[i-p])
     add_dna(dna[i])
