3152e254c499a8288bca921521277e0b6e75fa13
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-03 15:59:26 +0900

    [Silver IV] Title: 수 찾기, Time: 228 ms, Memory: 136456 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,18 +1,11 @@
-def binarySearch(board,target,start,end):
-    while start<=end:
-        mid=(start+end)//2
-        if board[mid]==target:
-            return 1
-        elif board[mid]<target:
-            start=mid+1
-        else:
-            end=mid-1
-    return 0
 import sys
-n=int(sys.stdin.readline())
-a=list(map(int,sys.stdin.readline().split()))
-m=int(sys.stdin.readline())
-tar=list(map(int,sys.stdin.readline().split()))
-a.sort()
+from bisect import bisect_left
+mii=lambda:sys.stdin.readline().rstrip()
+n=int(mii())
+arr=list(map(int,mii().split()))
+m=int(mii())
+tar=list(map(int,mii().split()))
+arr.sort()
 for i in tar:
-    print(binarySearch(a,i,0,n-1))
+    idx=bisect_left(arr,i)
+    print(1 if idx<n and arr[idx]==i else 0)
