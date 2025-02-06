6bfd85435bcd96c8adc9a0641187e1eb16f14a5f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-06 16:42:41 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.c
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 int dfs(char** arr, bool** visited, int y, int x, int n) {
-    visited[y][x] = true;
     int ny, nx, cnt = 1;
+    visited[y][x] = true;
     int dy[4] = {-1, 1, 0, 0};
     int dx[4] = {0, 0, -1, 1};
     for (int i = 0; i < 4; i++) {
-        ny = y + dy[i];
-        nx = x + dx[i];
-        if (0 > ny || ny >= n || 0 > nx || nx >= n)
-            continue;
-        if (visited[ny][nx] == false && arr[ny][nx] == '1') {
-            cnt += dfs(arr, visited, ny, nx, n);
+        ny = y + dy[i], nx = x + dx[i];
+        if (0 <= ny && ny < n && 0 <= nx && nx < n) {
+            if (!visited[ny][nx] && arr[ny][nx] == '1') {
+                cnt += dfs(arr, visited, ny, nx, n);
+            }
         }
     }
     return cnt;
@@ -25,9 +24,10 @@ int compare(const void* pa, const void* pb) {
 int main(void) {
     int n, idx = 0;
     scanf("%d", &n);
+    int r[n * n];
     char** arr = (char**)malloc(sizeof(char*) * n);
     for (int i = 0; i < n; i++) {
-        arr[i] = (char*)malloc(sizeof(char) * n);
+        arr[i] = (char*)malloc(sizeof(char) * (n + 1));
         scanf("%s", arr[i]);
     }
     bool** visited = (bool**)malloc(sizeof(bool*) * n);
@@ -36,10 +36,9 @@ int main(void) {
         for (int j = 0; j < n; j++)
             visited[i][j] = false;
     }
-    int r[n * n];
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (visited[i][j] == false && arr[i][j] == '1') {
+            if (!visited[i][j] && arr[i][j] == '1') {
                 r[idx++] = dfs(arr, visited, i, j, n);
             }
         }
