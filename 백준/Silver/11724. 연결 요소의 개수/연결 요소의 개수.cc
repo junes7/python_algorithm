49dbde707975876bd43d151ea924360a5ca81e03
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-07 22:21:42 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 268 ms, Memory: 6376 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void dfs(vector<vector<int>> &arr, vector<bool> &visited, int node, int n) {
+void dfs(vector<vector<int>> &arr, vector<bool> &visited, int node) {
     visited[node] = true;
     for (int i = 0; i < arr[node].size(); i++) {
         if (!visited[arr[node][i]])
-            dfs(arr, visited, arr[node][i], n);
+            dfs(arr, visited, arr[node][i]);
     }
 }
 int main(void) {
@@ -17,10 +17,10 @@ int main(void) {
         arr[u].push_back(v);
         arr[v].push_back(u);
     }
-    vector<bool> visited(n + 1, false);
+    vector<bool> visited(n + 1);
     for (int i = 1; i < n + 1; i++) {
-        if (visited[i] == false) {
-            dfs(arr, visited, i, n);
+        if (!visited[i]) {
+            dfs(arr, visited, i);
             cnt += 1;
         }
     }
