6c860bf840616dbc00b925f0dc4e68e3024a925e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-13 23:37:50 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -1,15 +1,8 @@
 #include <iostream>
 using namespace std;
-
 int main(void) {
-    int a,b;
-    cin>>a>>b;
-    
-    if(a<b)
-        cout<<"<"<<endl;
-    else if(a>b)
-        cout<<">"<<endl;
-    else
-        cout<<"=="<<endl;
+    int a, b;
+    cin >> a >> b;
+    cout << (a < b ? "<" : (a > b ? ">" : "==")) << "\n";
     return 0;
 }
