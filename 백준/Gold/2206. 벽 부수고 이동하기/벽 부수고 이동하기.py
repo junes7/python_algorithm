7509885739b5ee189580f7aec1f9cc40c9e74a8f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-14 17:35:20 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1640 ms, Memory: 321580 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -2,16 +2,13 @@ import sys
 from collections import deque
 def bfs(n,m,grid):
     directions=[(-1,0),(1,0),(0,-1),(0,1)]
-    visited=[[[False]*2 for _ in range(m)] for _ in range(n)]
     que=deque([(0,0,0,1)])
+    visited=[[[False]*2 for _ in range(m)] for _ in range(n)]
     visited[0][0][0]=True
     while que:
         y,x,wall_broken,dist=que.popleft()
-        if y==n-1 and x==m-1:
-            return dist
+        if y==n-1 and x==m-1: return dist
         for dy,dx in directions:
-            # ny=y+dy
-            # nx=x+dx
             ny,nx=y+dy,x+dx
             if 0<=ny<n and 0<=nx<m:
                 if grid[ny][nx]==0 and not visited[ny][nx][wall_broken]:
