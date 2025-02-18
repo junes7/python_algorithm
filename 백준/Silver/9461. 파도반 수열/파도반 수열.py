b0d5714595850ce1c8884f985a310a5186a6def1
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-19 08:58:05 +0900

    [Silver III] Title: 파도반 수열, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,9 +1,7 @@
-t=int(input())
+import sys
+input=lambda:sys.stdin.readline().rstrip()
 dp=[0,1,1,1,2,2]
-def solve():
-    for i in range(6,100+1):
-        dp.append(dp[i-1]+dp[i-5])
-solve()
-for _ in range(t):
-    n=int(input())
-    print(dp[n])
+for i in range(6,101):
+    dp+=[dp[i-1]+dp[i-5]]
+for _ in range(int(input())):
+    print(dp[int(input())])
