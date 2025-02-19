9a1eba5484684fad6842d77e505eeeef26b91255
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-19 12:29:22 +0900

    [Silver V] Title: 숫자 카드, Time: 300 ms, Memory: 5028 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.c
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.c
@@ -1,40 +1,40 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-int compare(const void* a, const void* b) {
-    return *(int*)a - *(int*)b;
-}
-int main(void) {
-    int n, m, elem, start, end, mid;
+void find_cards(int n, int *cards, int m, int *numbers) {
+    int start, mid, end;
     bool flag;
-    scanf("%d", &n);
-    int* numList = (int*)malloc(sizeof(int) * n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &elem);
-        numList[i] = elem;
-    }
-    scanf("%d", &m);
-    int* guessList = (int*)malloc(sizeof(int) * m);
     for (int i = 0; i < m; i++) {
-        scanf("%d", &elem);
-        guessList[i] = elem;
-    }
-    qsort(numList, n, sizeof(int), compare);
-    for (int i = 0; i < m; i++) {
-        start = 0, end = n - 1;
-        flag = false;
+        start = 0, end = n - 1, flag = false;
         while (start <= end) {
             mid = (start + end) / 2;
-            if (numList[mid] == guessList[i]) {
+            if (cards[mid] == numbers[i]) {
                 flag = true;
                 break;
-            } else if (numList[mid] > guessList[i]) {
-                end = mid - 1;
-            } else {
+            } else if (cards[mid] < numbers[i])
                 start = mid + 1;
-            }
+            else
+                end = mid - 1;
         }
         printf("%d ", flag ? 1 : 0);
     }
+}
+int compare(const void *pa, const void *pb) {
+    const int a = *(int *)pa;
+    const int b = *(int *)pb;
+    return a - b;
+}
+int main(void) {
+    int n, m, *cards, *numbers;
+    scanf("%d", &n);
+    cards = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &cards[i]);
+    scanf("%d", &m);
+    qsort(cards, n, sizeof(int), compare);
+    numbers = (int *)malloc(sizeof(int) * m);
+    for (int i = 0; i < m; i++)
+        scanf("%d", &numbers[i]);
+    find_cards(n, cards, m, numbers);
     return 0;
 }
