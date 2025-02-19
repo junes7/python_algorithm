a366266f392234aeb8e944d53f7b633f7b9a0ba5
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-19 10:14:40 +0900

    [Silver V] Title: 숫자 카드, Time: 476 ms, Memory: 249260 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,19 +1,12 @@
 import sys
-n=int(sys.stdin.readline())
-numList=list(map(int,sys.stdin.readline().split()))
-m=int(sys.stdin.readline())
-guessList=list(map(int,sys.stdin.readline().split()))
-numList.sort()
-for i in range(m):
-    flag=False
-    start,end=0,len(numList)-1
-    while start<=end:
-        mid=(start+end)//2
-        if numList[mid]==guessList[i]:
-            flag=True
-            break
-        elif numList[mid]>guessList[i]:
-            end=mid-1
-        else:
-            start=mid+1
-    print(1 if flag else 0,end=' ')
+input=lambda:sys.stdin.readline().rstrip()
+def find_cards(n,cards,m,numbers):
+    card_set,result=set(cards),[]
+    for num in numbers:
+        result+=[1 if num in card_set else 0]
+    return result
+n=int(input())
+cards=list(map(int,input().split()))
+m=int(input())
+numbers=list(map(int,input().split()))
+print(*find_cards(n,cards,m,numbers))
