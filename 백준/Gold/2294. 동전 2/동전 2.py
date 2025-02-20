f32310374dc1689a62b8b1cd97d915fe9cf06d5a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-20 15:02:52 +0900

    [Gold V] Title: 동전 2, Time: 116 ms, Memory: 116716 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,15 +1,15 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-N,K=map(int,input().split())
-maxn=float("INF")
-coins=[int(input()) for _ in range(N)]
-memo=[[maxn]*(K+1) for _ in range(N)]
-for a in range(N):
-    memo[a][0]=0
-    for b in range(1,K+1):
-        case1,prev_k=maxn,b-coins[a]
+n,k=map(int,input().split())
+maxn=987654321
+coins=[int(input()) for _ in range(n)]
+memo=[[maxn]*(k+1) for _ in range(n)]
+for i in range(n):
+    memo[i][0]=0
+    for j in range(1,k+1):
+        case1,prev_k=maxn,j-coins[i]
         if 0<=prev_k:
-            case1=memo[a][prev_k]+1
-        case2=memo[a-1][b]
-        memo[a][b]=min(case1,case2)
-print(-1 if memo[N-1][K]==maxn else memo[N-1][K])
+            case1=memo[i][prev_k]+1
+        case2=memo[i-1][j]
+        memo[i][j]=min(case1,case2)
+print(-1 if memo[n-1][k]==maxn else memo[n-1][k])
