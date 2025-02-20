4e0556fe1e124205167601c7e3bdd9a2690ee6be
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-20 18:11:27 +0900

    [Silver I] Title: 포도주 시식, Time: 4 ms, Memory: 2836 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int solve(vector<int> &wine, vector<long> &memo, int n) {
+    long case1 = 0, case2 = 0, case3 = 0;
+    if (n == 0)
+        return wine[0];
+    else if (n == 1)
+        return wine[0] + wine[1];
+    else if (n == 2)
+        return max(solve(wine, memo, 1), max(wine[0], wine[1]) + wine[2]);
+    if (memo[n] > -1) return memo[n];
+    case1 = solve(wine, memo, n - 1);
+    case2 = solve(wine, memo, n - 2) + wine[n];
+    case3 = solve(wine, memo, n - 3) + wine[n - 1] + wine[n];
+    memo[n] = max(max(case1, case3), max(case2, case3));
+    return memo[n];
+}
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> wine(n);
+    vector<long> memo(n, -1);
+    for (int i = 0; i < n; i++)
+        cin >> wine[i];
+    cout << solve(wine, memo, n - 1);
+    return 0;
+}
