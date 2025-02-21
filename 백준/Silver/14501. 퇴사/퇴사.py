42f422550a22906e60d836d683f7acaa7e3550b0
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-21 15:08:08 +0900

    [Silver III] Title: 퇴사, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,17 +1,16 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-n=int(input())
 ti,pi=[],[]
-for _ in range(n):
+n=int(input())
+for i in range(n):
     t,p=map(int,input().split())
     ti+=[t]
     pi+=[p]
 dp=[0]*(n+1)
 def solve(day):
-    if n<=day: return 0
+    if n<=day: return 0;
     if dp[day]: return dp[day]
-    case1=solve(day+1)
-    case2=0
+    case1,case2=solve(day+1),0
     if day+ti[day]<=n:
         case2=solve(day+ti[day])+pi[day]
     dp[day]=max(case1,case2)
