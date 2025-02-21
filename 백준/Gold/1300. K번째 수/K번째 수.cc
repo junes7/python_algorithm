cb73cc265ce116e13c1f42199d0a5b438d2c7c13
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-22 01:07:44 +0900

    [Gold I] Title: K번째 수, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+long find_kth_length(long n, long k) {
+    long r, mid, cnt, left = 1, right = n * n;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        cnt = 0;
+        for (int i = 1; i < n + 1; i++)
+            cnt += min(mid / i, n);
+        if (cnt < k)
+            left = mid + 1;
+        else {
+            r = mid;
+            right = mid - 1;
+        }
+    }
+    return r;
+}
+int main(void) {
+    long n, k;
+    cin >> n;
+    cin >> k;
+    cout << find_kth_length(n, k);
+    return 0;
+}
