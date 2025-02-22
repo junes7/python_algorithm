3824ed5ca2b4acce900a2beb4e9079e9277b3bdc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-23 00:18:25 +0900

    [Bronze I] Title: 히든 넘버, Time: 220 ms, Memory: 128592 KB -BaekjoonHub

---
+++ 백준/Bronze/8595. 히든 넘버/히든 넘버.py
@@ -0,0 +1,11 @@
+n = int(input())
+s = input()
+current_number,total_sum=0,0
+for char in s:
+    if char.isdigit():
+        current_number = current_number*10 + int(char)
+    else:
+        total_sum+=current_number
+        current_number = 0
+total_sum += current_number
+print(total_sum)
