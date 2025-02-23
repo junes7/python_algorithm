f75b1876db0c3f6da7c43a21acf505dc79bae42b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-23 14:36:26 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,10 +1,9 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-n=int(input())
-mod=1000000000
+n,mod=int(input()),1000000000
 dp=[[0]*10 for _ in range(n+1)]
-for i in range(1,10):
-    dp[1][i]=1
+for j in range(1,10):
+    dp[1][j]=1
 for i in range(2,n+1):
     for j in range(10):
         if 0<j:
