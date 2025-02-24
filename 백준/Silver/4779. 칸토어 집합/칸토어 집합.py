5355329dd6d9b0211a09448c298935490e62033b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-24 22:11:47 +0900

    [Silver III] Title: 칸토어 집합, Time: 92 ms, Memory: 112164 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,17 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+def solve(n):
+    if n == 1:
+        return "-"
+    else:
+        left = solve(n // 3)
+        center = " " * (n // 3)
+        return left + center + left
+while True:
+    try:
+        N = int(input())
+
+        rst = solve(3 ** N)
+        print(rst)
+    except:
+        break
