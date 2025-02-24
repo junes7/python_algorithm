5f70c81ba767c8e7330b0bcd3d1a84a0a3e552a0
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-24 22:36:11 +0900

    [Silver V] Title: 집합, Time: 944 ms, Memory: 128000 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,21 +1,22 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 m=int(input())
-arr=[0]*(21)
+num=0
 for _ in range(m):
     cmd=input().split()
     if len(cmd)==1:
         if cmd[0]=='all':
-            arr=[1]*(21)
+            num=(1<<21)-1
         elif cmd[0]=='empty':
-            arr=[0]*(21)
+            num=0
     else:
-        idx=int(cmd[1])
+        k=int(cmd[1])
+        idx=1<<(k-1)
         if cmd[0]=='add':
-            arr[idx]=1
+            num |= idx
         elif cmd[0] == 'remove':
-            arr[idx] = 0
+            num &= ~idx
         elif cmd[0] == 'toggle':
-            arr[idx] = 1 - arr[idx]
+            num ^= idx
         elif cmd[0] == 'check':
-            print(arr[idx])
+            print(1 if num&idx else 0)
