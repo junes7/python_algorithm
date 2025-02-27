46ff31512d477611742cd0deef081038f5fbdf7a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-27 23:56:53 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    cout << "     /~\\\n";
+    cout << "    ( oo|\n";
+    cout << "    _\\=/_\n";
+    cout << "   /  _  \\\n";
+    cout << "  //|/.\\|\\\\\n";
+    cout << " ||  \\ /  ||\n";
+    cout << "============\n";
+    cout << "|          |\n";
+    cout << "|          |\n";
+    cout << "|          |";
+    return 0;
+}
