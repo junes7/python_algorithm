3ead955e49eec087aa910a976e8055e59c8b2cf2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-27 23:32:55 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+    printf("  ___  ___  ___\n");
+    printf("  | |__| |__| |\n");
+    printf("  |           |\n");
+    printf("   \\_________/\n");
+    printf("    \\_______/\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |_____|\n");
+    printf("  __/       \\__\n");
+    printf(" /             \\\n");
+    printf("/_______________\\");
+    return 0;
+}
