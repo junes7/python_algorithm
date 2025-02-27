b16f22413d80116504d1258208cafac0905d32ee
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-27 11:46:52 +0900

    [Bronze IV] Title: 주사위 세개, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,14 +1,13 @@
-dice=list(map(int,input().split()))
-a=dict()
-for i in dice:
-    a[i]=1 if i not in a else a[i]+1;
-
-if len(a)==1:
-    key=[k for k,v in a.items() if v==3]
-    print(10000+key[0]*1000)
-elif len(a)==2:
-    key=[k for k,v in a.items() if v==2]
-    print(1000+key[0]*100)
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+a,b,c=map(int,input().split())
+if a==b==c:
+    prize=10000+a*1000
+elif a==b or a==c or b==c:
+    if a==b or a==c:
+        prize=1000+a*100
+    else:
+        prize=1000+b*100
 else:
-    key=[k for k,v in a.items() if v==1]
-    print(max(key)*100)
+    prize=max(a,b,c)*100
+print(prize)
