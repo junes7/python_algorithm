aaa58673d572f3c47b2cf6c5b16b7364689979c3
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-27 23:50:47 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    cout << "       _.-;;-._\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-;;-._|\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-''-._|";
+    return 0;
+}
