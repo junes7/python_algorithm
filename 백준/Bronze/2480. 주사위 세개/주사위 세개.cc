e0b1f5e947b8f6b78cc98cb54c3a2f59885310eb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-27 12:01:39 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -1,37 +1,20 @@
-#include <algorithm>
 #include <iostream>
-#include <map>
 #include <vector>
 using namespace std;
-
 int main(void) {
-    int elem, cnt;
-    vector<int> t;
-    map<int, int> dic;
-    for (int i = 0; i < 3; i++) {
-        cin >> elem;
-        t.push_back(elem);
-    }
-    for (int i = 0; i < 3; i++) {
-        if (dic.find(t[i]) == dic.end()) {
-            dic[t[i]] = 1;
+    int a, b, c, prize;
+    cin >> a >> b >> c;
+    if (a == b && b == c) {
+        prize = 10000 + a * 1000;
+    } else if (a == b || a == c || b == c) {
+        if (a == b || a == c) {
+            prize = 1000 + a * 100;
         } else {
-            dic[t[i]] += 1;
+            prize = 1000 + b * 100;
         }
-    }
-    map<int, int>::iterator it;
-    for (it = dic.begin(); it != dic.end(); it++) {
-        if (it->second == 4 - dic.size()) {
-            cnt = it->first;
-            break;
-        }
-    }
-    if (dic.size() == 1) {
-        cout << 10000 + cnt * 1000 << endl;
-    } else if (dic.size() == 2) {
-        cout << 1000 + cnt * 100 << endl;
     } else {
-        cout << *max_element(t.begin(), t.end()) * 100 << endl;
+        prize = max(max(a, c), max(b, c)) * 100;
     }
+    cout << prize;
     return 0;
 }
