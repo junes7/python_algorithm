a1b5fb7f19b367d6750dc4837d21333b545b3cd9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-02-27 23:42:47 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    cout << ".  .   .\n";
+    cout << "|  | _ | _. _ ._ _  _\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/.";
+    return 0;
+}
