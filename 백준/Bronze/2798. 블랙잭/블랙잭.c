61a83f698b5e0f4e2ce17c885ee45259e4236351
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-01 12:30:35 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.c
+++ 백준/Bronze/2798. 블랙잭/블랙잭.c
@@ -1,38 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int compare(const void *a, const void *b) {
-    return *(int *)b - *(int *)a;
-}
-
 int main(void) {
-    int n, m, elem, idx = 0, *num = (int *)malloc(sizeof(int) * 100);
-    int *suml = (int *)malloc(sizeof(int) * 300000);
+    int n, m, max_sum = 0, cur_sum, *cards;
     scanf("%d %d", &n, &m);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &elem);
-        num[i] = elem;
-    }
-    qsort(num, n, sizeof(int), compare);
+    cards = (int*)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &cards[i]);
     for (int i = 0; i < n - 2; i++) {
         for (int j = i + 1; j < n - 1; j++) {
             for (int k = j + 1; k < n; k++) {
-                elem = num[i] + num[j] + num[k];
-                if (elem == m) {
-                    suml[idx++] = elem;
-                    break;
-                } else if (elem < m) {
-                    suml[idx++] = elem;
-                }
+                cur_sum = cards[i] + cards[j] + cards[k];
+                if (cur_sum <= m)
+                    max_sum = max_sum < cur_sum ? cur_sum : max_sum;
             }
         }
     }
-    elem = suml[0];
-    for (int i = 1; i < idx; i++) {
-        if (elem < suml[i]) {
-            elem = suml[i];
-        }
-    }
-    printf("%d", elem);
+    printf("%d", max_sum);
     return 0;
 }
