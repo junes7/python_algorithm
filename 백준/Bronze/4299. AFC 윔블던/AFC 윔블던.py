6f506d8bf9a806a7796845f59908bc86d08f2e12
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-04 17:47:10 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.py
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.py
@@ -1,7 +1,5 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 total,diff=map(int,input().split())
-if total<diff: print(-1)
-else:
-    if (total+diff)%2!=0: print(-1)
-    else: print((total+diff)//2,(total-diff)//2)
+if total<diff or (total+diff)%2!=0: print(-1)
+else: print((total+diff)//2,(total-diff)//2)
