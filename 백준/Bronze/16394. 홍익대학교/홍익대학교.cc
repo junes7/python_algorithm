fd37008e0c701c07459f8e53996f89fddee74cd6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-05 23:26:43 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    cout << n - 1946;
+    return 0;
+}
