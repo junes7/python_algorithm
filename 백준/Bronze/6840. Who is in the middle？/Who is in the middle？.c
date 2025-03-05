230fb74819aecaa5f2207ce722bc4a5a3242e552
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-05 23:07:16 +0900

    [Bronze V] Title: Who is in the middle?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int compare(const void *pa, const void *pb) {
+    const int a = *(int *)pa;
+    const int b = *(int *)pb;
+    return a - b;
+}
+int main(void) {
+    int *arr = (int *)malloc(sizeof(int) * 3);
+    for (int i = 0; i < 3; i++)
+        scanf("%d", &arr[i]);
+    qsort(arr, 3, sizeof(int), compare);
+    printf("%d", arr[3 / 2]);
+    return 0;
+}
