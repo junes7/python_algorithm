bac82c9fb04af07b21be3b39bd689d51403556df
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-05 09:07:24 +0900

    [Bronze V] Title: Welcome to SMUPC!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.cc
+++ 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.cc
@@ -3,10 +3,7 @@ using namespace std;
 int main(void) {
     int n;
     string st = "WelcomeToSMUPC";
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
     cin >> n;
-    cout << st[--n % 14];
+    cout << st[--n % st.size()];
     return 0;
 }
