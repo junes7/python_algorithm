cdd3e8d67c04b791ccf9f2defa62931471fbd2d7
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-07 00:40:51 +0900

    [Silver V] Title: Image Filter, Time: 188 ms, Memory: 114360 KB -BaekjoonHub

---
+++ 백준/Silver/28345. Image Filter/Image Filter.py
@@ -0,0 +1,30 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+t=int(input())
+for _ in range(t):
+    rlt=[]
+    r,c=map(int,input().split())
+    arr=[list(map(int,input().split())) for _ in range(r)]
+    for i in range(r):
+        rlt.append([])
+        for j in range(c):
+            t,minn,A,B,C=[],float("INF"),0,0,0
+            if i==0 and j>0:
+                A=arr[i][j-1]
+            elif i>0 and j==0:
+                B=arr[i-1][j]
+            elif i>0 and j>0:
+                A=arr[i][j-1]
+                B=arr[i-1][j]
+                C=arr[i-1][j-1]
+            t+=[arr[i][j]-0,arr[i][j]-A,arr[i][j]-B,arr[i][j]-(A+B)//2,arr[i][j]-(A+B-C)]
+            for k in t:
+                if minn>abs(k):
+                    minn=abs(k)
+            for l,k in enumerate(t):
+                if minn==abs(k):
+                    rlt[i].append(l)
+                    rlt[i].append(k)
+                    break
+    for i in range(r):
+        print(*rlt[i])
