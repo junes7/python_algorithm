49998f8d0cce97120382f5cee586d765863a431a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-06 20:40:19 +0900

    [Silver V] Title: 점프는 즐거워, Time: 104 ms, Memory: 110876 KB -BaekjoonHub

---
+++ 백준/Silver/4383. 점프는 즐거워/점프는 즐거워.py
@@ -0,0 +1,21 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+while True:
+    t=input()
+    if t=="": break
+    rlt,flag=[],True
+    n,*arr=map(int,t.split())
+    for i in range(n-1):
+        if abs(arr[i]-arr[i+1])>n-1:
+            flag=False
+            break
+        rlt+=[abs(arr[i]-arr[i+1])]
+    rlt.sort()
+    if flag:
+        for i in range(n-2):
+            if i+1!=rlt[i]:
+                flag=False
+                break
+        print("Jolly" if flag else "Not jolly")
+    else:
+        print("Not jolly")
