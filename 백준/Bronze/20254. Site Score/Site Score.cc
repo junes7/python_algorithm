22c77e59bce2e0bff4d2efb5bf1c12607ddf1765
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-06 16:22:02 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int Ur, Tr, Uo, To;
+    cin >> Ur >> Tr >> Uo >> To;
+    cout << 56 * Ur + 24 * Tr + 14 * Uo + 6 * To;
+    return 0;
+}
