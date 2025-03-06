dbc587096eff0571a715f89580c0384569127bd2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-06 16:25:22 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    int Ur, Tr, Uo, To;
+    scanf("%d %d %d %d", &Ur, &Tr, &Uo, &To);
+    printf("%d", 56 * Ur + 24 * Tr + 14 * Uo + 6 * To);
+    return 0;
+}
