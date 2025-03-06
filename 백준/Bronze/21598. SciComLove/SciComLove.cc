a6536b7af3c9090a74d7f5c96be784d01bbcdd5c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-06 14:49:42 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    string st = "SciComLove";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cout << st << "\n";
+    return 0;
+}
