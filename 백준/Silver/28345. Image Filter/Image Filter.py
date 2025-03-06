cdf4d030c15efba3199f911b993d82cff0b52c0e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-07 00:47:31 +0900

    [Silver V] Title: Image Filter, Time: 184 ms, Memory: 114360 KB -BaekjoonHub

--- 백준/Silver/28345. Image Filter/Image Filter.py
+++ 백준/Silver/28345. Image Filter/Image Filter.py
@@ -8,7 +8,7 @@ for _ in range(t):
     for i in range(r):
         rlt.append([])
         for j in range(c):
-            t,minn,A,B,C=[],float("INF"),0,0,0
+            temp,minn,A,B,C=[],float("INF"),0,0,0
             if i==0 and j>0:
                 A=arr[i][j-1]
             elif i>0 and j==0:
@@ -17,11 +17,11 @@ for _ in range(t):
                 A=arr[i][j-1]
                 B=arr[i-1][j]
                 C=arr[i-1][j-1]
-            t+=[arr[i][j]-0,arr[i][j]-A,arr[i][j]-B,arr[i][j]-(A+B)//2,arr[i][j]-(A+B-C)]
-            for k in t:
+            temp+=[arr[i][j]-0,arr[i][j]-A,arr[i][j]-B,arr[i][j]-(A+B)//2,arr[i][j]-(A+B-C)]
+            for k in temp:
                 if minn>abs(k):
                     minn=abs(k)
-            for l,k in enumerate(t):
+            for l,k in enumerate(temp):
                 if minn==abs(k):
                     rlt[i].append(l)
                     rlt[i].append(k)
