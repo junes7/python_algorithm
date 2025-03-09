946d8910236fc286bb70a3df4179f90a3cbcbf07
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-09 10:54:16 +0900

    [Bronze IV] Title: 주사위, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n, a, b;
+    cin >> n;
+    for (int i = 1; i < n + 1; i++) {
+        cin >> a >> b;
+        cout << "Case " << i << ": " << a + b << "\n";
+    }
+    return 0;
+}
