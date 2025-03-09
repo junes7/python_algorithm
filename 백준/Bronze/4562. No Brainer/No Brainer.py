b7c6d4057d63b78df83fe057401e57d74d3043f5
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-09 17:37:51 +0900

    [Bronze IV] Title: No Brainer, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/4562. No Brainer/No Brainer.py
+++ 백준/Bronze/4562. No Brainer/No Brainer.py
@@ -1,6 +1,5 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-n=int(input())
-for _ in range(n):
+for _ in range(int(input())):
     x,y=map(int,input().split())
     print("NO BRAINS" if x<y else "MMM BRAINS")
