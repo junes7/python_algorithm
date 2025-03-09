8eedde93e463161a7b09028a6fbbc2eb874aecb6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-09 13:22:53 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    cin >> n;
+    for (int i = 1; i < n + 1; i++)
+        cout << "Hello World, Judge " << i << "!\n";
+    return 0;
+}
