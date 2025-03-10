26092e45ef97b9032cd9df94abbad1f5429ed94d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-10 19:30:06 +0900

    [Silver III] Title: 조커 찾기, Time: 808 ms, Memory: 112416 KB -BaekjoonHub

--- 백준/Silver/24393. 조커 찾기/조커 찾기.py
+++ 백준/Silver/24393. 조커 찾기/조커 찾기.py
@@ -1,38 +1,32 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-joker=1
-for i in range(int(input())):
+joker_pos=1
+for _ in range(int(input())):
     A,next=[*map(int,input().split())],0
-    # left deck
-    if joker<=13:
+    if joker_pos<=13:
         left_sum=0
         for j in range(len(A)):
-            # Even 짝수
             if j%2==0:
                 next+=A[j]
-            # Odd 홀수
-            else: # j%2==1
-                if left_sum+A[j]>=joker:
-                    next+=joker-left_sum
+            else:
+                if left_sum+A[j]>=joker_pos:
+                    next+=joker_pos-left_sum
                     break
                 else:
-                    next+=A[j]
                     left_sum+=A[j]
-    # right deck
-    elif joker>13:
-        joker-=13
+                    next+=A[j]
+    else:
+        joker_pos-=13
         right_sum=0
         for j in range(len(A)):
-            # Odd 홀수
             if j%2==1:
                 next+=A[j]
-            # Even 짝수
-            else: # j%2==0
-                if right_sum+A[j]>=joker:
-                    next+=joker-right_sum
+            else:
+                if right_sum+A[j]>=joker_pos:
+                    next+=joker_pos-right_sum
                     break
                 else:
-                    next+=A[j]
                     right_sum+=A[j]
-    joker=next
-print(joker)
+                    next+=A[j]
+    joker_pos=next
+print(joker_pos)
