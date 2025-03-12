433589336217bb14411a4aea63297477a3b0ec85
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-12 23:06:17 +0900

    [Bronze V] Title: Serca, Time: 244 ms, Memory: 111648 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,13 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n=int(input())
+for _ in range(n):
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
