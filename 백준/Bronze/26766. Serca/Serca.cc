7afab9bcb4cf0d148c12893932316addb10287ca
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-12 23:08:44 +0900

    [Bronze V] Title: Serca, Time: 92 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << " @@@   @@@ \n";
+        cout << "@   @ @   @\n";
+        cout << "@    @    @\n";
+        cout << "@         @\n";
+        cout << " @       @ \n";
+        cout << "  @     @  \n";
+        cout << "   @   @   \n";
+        cout << "    @ @    \n";
+        cout << "     @     \n";
+    }
+    return 0;
+}
