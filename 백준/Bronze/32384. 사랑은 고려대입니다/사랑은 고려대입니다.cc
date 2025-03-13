38f5b8fae30921ee88f94904ccc4bd160cc55713
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-13 19:42:31 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cout << "LoveisKoreaUniversity" << ' ';
+    return 0;
+}
