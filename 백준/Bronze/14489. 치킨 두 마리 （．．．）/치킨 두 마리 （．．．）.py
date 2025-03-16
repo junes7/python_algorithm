e51171a606f51a9b519aaac9fd79a352b7ef4b7f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-16 22:37:36 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.py
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.py
@@ -2,7 +2,4 @@ import sys
 input=lambda:sys.stdin.readline().rstrip()
 a,b=map(int,input().split())
 c=int(input())
-if a+b>=c*2:
-    print(a+b-2*c)
-else:
-    print(a+b)
+print(a+b-2*c if a+b>=c*2 else a+b)
