49516320eb2af1c9fc4d2508692f3dcfa0eb3461
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-17 13:03:55 +0900

    [Bronze IV] Title: 과목선택, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11948. 과목선택/과목선택.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void) {
+    int len, sums;
+    vector<int> sci(4), hist(2), total;
+    for (int i = 0; i < 4; i++)
+        scanf("%d", &sci[i]);
+    for (int i = 0; i < 2; i++)
+        scanf("%d", &hist[i]);
+    len = sci.size();
+    for (int i = 0; i < len - 2; i++) {
+        for (int j = i + 1; j < len - 1; j++) {
+            for (int k = j + 1; k < len; k++) {
+                sums = sci[i] + sci[j] + sci[k];
+                total.push_back(hist[0] > hist[1] ? sums + hist[0] : sums + hist[1]);
+            }
+        }
+    }
+    cout << *max_element(total.begin(), total.end());
+    return 0;
+}
