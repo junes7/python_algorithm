eabd72fb13f0487ffb5b3969f3c4ce7dbd3a8f1e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-19 01:44:13 +0900

    [Silver V] Title: Array Rotation, Time: 108 ms, Memory: 111224 KB -BaekjoonHub

--- 백준/Silver/28456. Array Rotation/Array Rotation.py
+++ 백준/Silver/28456. Array Rotation/Array Rotation.py
@@ -11,8 +11,7 @@ for i in range(Q):
         cmd,sel=map(int,st.split())
     if cmd==1:
         sel-=1
-        t=arr[sel].pop()
-        arr[sel]=[t]+arr[sel][:len(arr[sel])]
+        arr[sel]=[arr[sel].pop()]+arr[sel][:len(arr[sel])]
     elif cmd==2:
         arrTemp=[[0]*N for _ in range(N)]
         for i in range(1,N+1):
@@ -20,6 +19,4 @@ for i in range(Q):
                 arrTemp[j-1][N-i]=arr[i-1][j-1]
         arr=arrTemp
 for i in range(N):
-    for j in range(N):
-        print(arr[i][j],end=' ')
-    print()
+    print(*arr[i])
