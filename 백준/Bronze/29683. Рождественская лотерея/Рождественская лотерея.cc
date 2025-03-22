fab4e17e03f26dcb58e03b78ec8e2c1178cfcfc0
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-22 12:23:01 +0900

    [Bronze IV] Title: Рождественская лотерея, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/29683. Рождественская лотерея/Рождественская лотерея.cc
+++ 백준/Bronze/29683. Рождественская лотерея/Рождественская лотерея.cc
@@ -2,7 +2,7 @@
 using namespace std;
 int main(void) {
     int n, elem, A, cnt = 0;
-    scanf("%d %d", &n, &A);
+    cin >> n >> A;
     for (int i = 0; i < n; i++) {
         cin >> elem;
         cnt += elem / A;
