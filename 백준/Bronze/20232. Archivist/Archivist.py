3790e79cda34ce305e690bbb164f63c24c2fd848
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-23 18:17:35 +0900

    [Bronze IV] Title: Archivist, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,8 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+arr=["ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO",
+"SPbSU", "ITMO", "ITMO", "ITMO", "ITMO",
+"ITMO", "PetrSU, ITMO", "SPbSU", "SPbSU",
+"ITMO", "ITMO", "ITMO", "ITMO", "SPbSU",
+"ITMO", "ITMO", "ITMO", "ITMO", "SPbSU", "ITMO"]
+print(arr[int(input())-1995])
