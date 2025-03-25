af75172cfd882cccd42df94840765f4e93d5e12e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-25 23:05:38 +0900

    [Bronze IV] Title: Electric Bill, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25881. Electric Bill/Electric Bill.cc
+++ 백준/Bronze/25881. Electric Bill/Electric Bill.cc
@@ -1,12 +1,11 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 int main(void) {
     int gen, add, n, consu;
-    cin >> gen >> add;
-    cin >> n;
+    scanf("%d %d", &gen, &add);
+    scanf("%d", &n);
     for (int i = 0; i < n; i++) {
-        cin >> consu;
-        cout << consu << ' ' << (consu <= 1000 ? gen * consu : 1000 * gen + (consu - 1000) * add) << "\n";
+        scanf("%d", &consu);
+        printf("%d %d\n", consu, consu <= 1000 ? gen * consu : 1000 * gen + (consu - 1000) * add);
     }
     return 0;
 }
