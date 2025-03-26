c9457ae162870c57621d90907da3c127bae04708
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-26 23:05:14 +0900

    [Bronze IV] Title: 개표, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/30868. 개표/개표.py
+++ 백준/Bronze/30868. 개표/개표.py
@@ -3,12 +3,11 @@ input=lambda:sys.stdin.readline().rstrip()
 n=int(input())
 for _ in range(n):
     num=int(input())
-    arr=[num//5,num%5]
     st=""
-    if arr[0]>0:
-        for _ in range(arr[0]):
+    if num//5>0:
+        for _ in range(num//5):
             st+="++++ "
-    if arr[1]>0:
-        for _ in range(arr[1]):
+    if num%5>0:
+        for _ in range(num%5):
             st+="|"
     print(st)
