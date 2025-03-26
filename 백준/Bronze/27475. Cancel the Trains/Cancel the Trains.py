e8eef3ec09b0c3caaaff853d8751afd6e4f5bda8
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-26 09:16:33 +0900

    [Bronze III] Title: Cancel the Trains, Time: 276 ms, Memory: 110736 KB -BaekjoonHub

---
+++ 백준/Bronze/27475. Cancel the Trains/Cancel the Trains.py
@@ -0,0 +1,14 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+t=int(input())
+for _ in range(t):
+    n,m=map(int,input().split())
+    x=[*map(int,input().split())]
+    y=[*map(int,input().split())]
+    cnt=0
+    for i in y:
+        for j in x:
+            for t in range(102):
+                if i==t and t==j:
+                    cnt+=1
+    print(cnt)
