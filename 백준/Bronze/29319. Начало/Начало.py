8e23265298f2b0c0b71ede5908b7e59dc3cbda38
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-26 12:14:13 +0900

    [Bronze III] Title: Начало, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/29319. Начало/Начало.py
@@ -0,0 +1,14 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n=int(input())
+arr=sorted([*map(int,input().split())])
+called,cnt=[],0
+len=max(arr)
+for i in range(1,len+1):
+    for j in range(n):
+        if i<=arr[j]:
+            if i not in called:
+                called+=[i]
+            else:
+                cnt+=1
+print(cnt) 
