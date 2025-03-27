9f00bbabb7d69b1d04bdccbb5bd4ff29666886c6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-27 11:18:25 +0900

    [Bronze IV] Title: Andando no tempo, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/13580. Andando no tempo/Andando no tempo.py
@@ -0,0 +1,9 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+a,b,c=map(int,input().split())
+if a==b or a==c or b==c:
+    print("S")
+elif a+b==c or a+c==b or b+c==a:
+    print("S")
+else:
+    print("N")
