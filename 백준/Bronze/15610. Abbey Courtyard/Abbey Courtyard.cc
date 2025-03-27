30890680ecd2bd98ab44406ccf4f5c2220a87145
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-27 10:14:38 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main(void) {
+    long long a;
+    cin >> a;
+    cout << fixed;
+    cout.precision(6);
+    cout << sqrt(a) * 4;
+    return 0;
+}
