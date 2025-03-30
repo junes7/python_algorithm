0cd2713280941d9a30cb244ae2688d5fb29fa3a8
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-30 22:34:11 +0900

    [Bronze IV] Title: Poziome serca, Time: 100 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        printf(" @@@   @@@  ");
+    printf("\n");
+    for (int i = 0; i < n; i++)
+        printf("@   @ @   @ ");
+    printf("\n");
+    for (int i = 0; i < n; i++)
+        printf("@    @    @ ");
+    printf("\n");
+    for (int i = 0; i < n; i++)
+        printf("@         @ ");
+    printf("\n");
+    for (int i = 0; i < n; i++)
+        printf(" @       @  ");
+    printf("\n");
+    for (int i = 0; i < n; i++)
+        printf("  @     @   ");
+    printf("\n");
+    for (int i = 0; i < n; i++)
+        printf("   @   @    ");
+    printf("\n");
+    for (int i = 0; i < n; i++)
+        printf("    @ @     ");
+    printf("\n");
+    for (int i = 0; i < n; i++)
+        printf("     @      ");
+    return 0;
+}
