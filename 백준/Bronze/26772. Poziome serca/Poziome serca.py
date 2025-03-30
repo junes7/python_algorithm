04a8a9c67bd7f1d45a537fb2e2ce9130181ea2aa
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-03-30 12:29:10 +0900

    [Bronze IV] Title: Poziome serca, Time: 104 ms, Memory: 129480 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.py
@@ -0,0 +1,12 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n=int(input())
+print(" @@@   @@@  "*n)
+print("@   @ @   @ "*n)
+print("@    @    @ "*n)
+print("@         @ "*n)
+print(" @       @  "*n)
+print("  @     @   "*n)
+print("   @   @    "*n)
+print("    @ @     "*n)
+print("     @      "*n)
