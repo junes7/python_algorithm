be29d2a63675bb6df4b69d6780c5c001ff383b7b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-01 23:51:34 +0900

    [Bronze IV] Title: The Walking Adam, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/18698. The Walking Adam/The Walking Adam.py
@@ -0,0 +1,8 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+for _ in range(int(input())) :
+    cnt = 0
+    for char in input() :
+        if char == 'D' : break
+        cnt += 1
+    print(cnt)
