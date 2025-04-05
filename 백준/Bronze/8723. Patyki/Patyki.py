8645e853159ca60defe310fd7256f59a103cb4b2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-05 23:40:50 +0900

    [Bronze IV] Title: Patyki, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/8723. Patyki/Patyki.py
+++ 백준/Bronze/8723. Patyki/Patyki.py
@@ -1,11 +1,7 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-arr=[*map(int,input().split())]
-arr.sort()
+arr=sorted([*map(int,input().split())])
 if arr[0]**2+arr[1]**2==arr[2]**2:
     print(1)
 else:
-    if arr[0]==arr[1]==arr[2]:
-        print(2)
-    else:
-        print(0)
+    print(2 if arr[0]==arr[1]==arr[2] else 0)
