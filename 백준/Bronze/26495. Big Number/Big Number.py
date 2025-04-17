de649b211b4e254c2ab85a619027c20796bb13a2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-17 19:55:12 +0900

    [Bronze IV] Title: Big Number, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/26495. Big Number/Big Number.py
@@ -0,0 +1,8 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+num={0:"0000\n0  0\n0  0\n0  0\n0000\n",1:"   1\n   1\n   1\n   1\n   1\n",2:"2222\n   2\n2222\n2\n2222\n",
+     3:"3333\n   3\n3333\n   3\n3333\n",4:"4  4\n4  4\n4444\n   4\n   4\n",5:"5555\n5\n5555\n   5\n5555\n",
+     6:"6666\n6\n6666\n6  6\n6666\n",7:"7777\n   7\n   7\n   7\n   7\n",8:"8888\n8  8\n8888\n8  8\n8888\n",
+     9:"9999\n9  9\n9999\n   9\n   9\n"}
+for c in input():
+    print(num[int(c)])
