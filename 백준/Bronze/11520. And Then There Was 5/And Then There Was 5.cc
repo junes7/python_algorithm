0be4ecd862906bc8441aff08085bdcb8ef4e1505
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-19 17:30:07 +0900

    [Bronze III] Title: And Then There Was 5, Time: 264 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11520. And Then There Was 5/And Then There Was 5.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n, p, d;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> p >> d;
+        cout << d << ' ' << 5 << "\n";
+    }
+    return 0;
+}
