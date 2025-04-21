9c0bf8390d48c9ae58babe61fbf2db7fd149093f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-21 23:56:54 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 108 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,7 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+a, b, c, d, e, f = map(int, input().split())
+for i in range(-999, 1000):
+    for j in range(-999, 1000):
+        if (a*i) + (b*j) == c and (d*i) + (e*j) == f:
+            print(i,j)
