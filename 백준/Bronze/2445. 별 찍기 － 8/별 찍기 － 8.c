c35c60cb9431c9930075a53d827f0a66f5e688b8
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-22 23:58:32 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i < n + 1; i++) {
+        for (int j = 0; j < i; j++)
+            printf("*");
+        for (int j = 0; j < 2 * (n - i); j++)
+            printf(" ");
+        for (int j = 0; j < i; j++)
+            printf("*");
+        printf("\n");
+    }
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < (n - i); j++)
+            printf("*");
+        for (int j = 0; j < 2 * i; j++)
+            printf(" ");
+        for (int j = 0; j < (n - i); j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
