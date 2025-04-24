280fae224cefe2ebdf3d48537862f27750ae98ad
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-24 22:03:11 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i < n + 1; i++) {
+        for (int j = 0; j < n - i; j++)
+            printf(" ");
+        for (int j = 0; j < i - 1; j++)
+            printf("* ");
+        printf("*\n");
+    }
+    return 0;
+}
