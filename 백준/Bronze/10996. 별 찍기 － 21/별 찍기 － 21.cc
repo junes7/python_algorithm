d7f8af7399e11c4811f6cffe1396a01835886942
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-27 23:50:25 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - (n / 2); j++)
+            cout << "* ";
+        cout << "\n";
+        for (int j = 0; j < n / 2; j++)
+            cout << " *";
+        cout << "\n";
+    }
+    return 0;
+}
