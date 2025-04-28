e70ddea487485daa656f5f3b5944f08af420e5df
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-28 16:21:04 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
@@ -1,7 +1,5 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 n=int(input())
-for i in range(1,n+1):
-    print('*'*i)
-for i in range(1,n):
-    print('*'*(n-i))
+for i in range(1,2*n):
+    print('*'*(i if i<n else 2*n-i))
