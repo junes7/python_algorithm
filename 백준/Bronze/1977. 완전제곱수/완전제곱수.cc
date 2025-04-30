d7fba568ec3da8d9122ff8420e0c50f7f0cd4237
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-04-30 18:57:22 +0900

    [Bronze II] Title: 완전제곱수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.cc
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.cc
@@ -1,16 +1,13 @@
 #include <cmath>
 #include <iostream>
-#include <vector>
 using namespace std;
 int main(void) {
     int m, n, s = 0, minn;
-    vector<int> t;
     cin >> m;
     cin >> n;
     minn = n;
     for (int i = m; i < n + 1; i++) {
         if (sqrt(i) == int(sqrt(i))) {
-            t.push_back(i);
             if (minn > i) minn = i;
             s += i;
         }
