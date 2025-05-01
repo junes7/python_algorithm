8da7c08463ab4042eae1151a6eb7003edc8c341a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-01 12:01:39 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 100 ms, Memory: 123100 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,14 +1,11 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
+n=int(input())
 def draw_stars(n):
     if n==1: return ['*']
-    stars=draw_stars(n//3)
-    result=[]
-    for star in stars:
-        result.append(star*3)
-    for star in stars:
-        result.append(star+' '*(n//3)+star)
-    for star in stars:
-        result.append(star*3)
+    stars,result=draw_stars(n//3),[]
+    result+=[star*3 for star in stars]
+    result+=[star+' '*(n//3)+star for star in stars]
+    result+=[star*3 for star in stars]
     return result
-print('\n'.join(draw_stars(int(input()))))
+print("\n".join(draw_stars(n)))
