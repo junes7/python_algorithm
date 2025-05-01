6d18f5ef5c426f6e151e919e0b743ff754793476
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-01 15:14:16 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 44 ms, Memory: 6404 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.c
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.c
@@ -2,39 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 char **draw_stars(int n) {
+    int idx;
     char **rlt = (char **)malloc(sizeof(char *) * n);
-    for (int i = 0; i < n; i++) {
-        rlt[i] = (char *)malloc(sizeof(char) * 7000);
-        memset(rlt[i], 0, sizeof(char) * 7000);
-    }
     char **stars = (char **)malloc(sizeof(char *) * n);
-    char *temp = (char *)malloc(sizeof(char) * 7000);
+    char *t = (char *)malloc(sizeof(char) * (n + 1));
+    for (int i = 0; i < n; i++)
+        rlt[i] = (char *)malloc(sizeof(char) * (n + 1));
     if (n == 1) {
-        stars[0] = (char *)malloc(sizeof(char) * (n + 1));
-        stars[0][0] = '*';
-        stars[0][1] = '\0';
-        return stars;
+        rlt[0] = (char *)malloc(sizeof(char) * (n + 1));
+        rlt[0][0] = '*';
+        rlt[0][1] = '\0';
+        return rlt;
     }
     stars = draw_stars(n / 3);
-    for (int i = 0; i < n / 3; i++) {
-        memset(temp, 0, sizeof(char) * 7000);
-        for (int j = 0; j < 3; j++)
-            strcat(temp, stars[i]);
-        strcpy(rlt[i], temp);
-    }
-    for (int i = 0; i < n / 3; i++) {
-        memset(temp, 0, sizeof(char) * 7000);
-        strcat(temp, stars[i]);
-        for (int j = 0; j < n / 3; j++)
-            strcat(temp, " ");
-        strcat(temp, stars[i]);
-        strcpy(rlt[i + 1 * (n / 3)], temp);
-    }
-    for (int i = 0; i < n / 3; i++) {
-        memset(temp, 0, sizeof(char) * 7000);
-        for (int j = 0; j < 3; j++)
-            strcat(temp, stars[i]);
-        strcpy(rlt[i + 2 * (n / 3)], temp);
+    for (int k = 0; k < 3; k++) {
+        for (int i = 0; i < n / 3; i++) {
+            memset(t, 0, sizeof(char) * (n + 1));
+            if (k % 2 == 0) {
+                for (int j = 0; j < 3; j++)
+                    strcat(t, stars[i]);
+            } else {
+                strcat(t, stars[i]);
+                for (int j = 0; j < n / 3; j++)
+                    strcat(t, " ");
+                strcat(t, stars[i]);
+            }
+            strcpy(rlt[i + k * (n / 3)], t);
+        }
     }
     return rlt;
 }
