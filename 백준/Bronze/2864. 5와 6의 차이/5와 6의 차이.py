89cf150a17ef31f32d1d231de10b343623bff81f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-05 23:01:27 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.py
@@ -0,0 +1,10 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+a,b=input().split()
+a=a.replace('6','5')
+b=b.replace('6','5')
+minn=int(a)+int(b)
+a=a.replace('5','6')
+b=b.replace('5','6')
+maxn=int(a)+int(b)
+print(minn,maxn)
