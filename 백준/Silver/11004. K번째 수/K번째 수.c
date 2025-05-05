929b916db30335a25a401aa8d055c75d04ed37b9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-05 17:20:17 +0900

    [Silver V] Title: K번째 수, Time: 1844 ms, Memory: 40176 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int compare(const void *pa, const void *pb) {
+    const int a = *(int *)pa;
+    const int b = *(int *)pb;
+    return a - b;
+}
+int main(void) {
+    int n, k, *arr;
+    scanf("%d %d", &n, &k);
+    arr = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    qsort(arr, n, sizeof(int), compare);
+    printf("%d", arr[k - 1]);
+    return 0;
+}
