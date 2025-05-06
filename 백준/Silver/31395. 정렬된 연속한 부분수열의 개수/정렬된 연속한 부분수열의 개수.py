f048ac747e1eecee67549845949694036b3d94bb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-06 12:56:52 +0900

    [Silver IV] Title: 정렬된 연속한 부분수열의 개수, Time: 136 ms, Memory: 140800 KB -BaekjoonHub

--- 백준/Silver/31395. 정렬된 연속한 부분수열의 개수/정렬된 연속한 부분수열의 개수.py
+++ 백준/Silver/31395. 정렬된 연속한 부분수열의 개수/정렬된 연속한 부분수열의 개수.py
@@ -2,7 +2,7 @@ import sys
 input=lambda:sys.stdin.readline().rstrip()
 n=int(input())
 arr=[*map(int,input().split())]
-res,rlt=[1]*n,0
+res=[1]*n
 for i in range(1,n):
     if arr[i-1]<arr[i]:
         res[i]=res[i-1]+1
