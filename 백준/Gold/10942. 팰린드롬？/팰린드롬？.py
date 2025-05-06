82e8fba5c299f40cb6d6c1cc2331b3c0213ada95
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-06 16:28:37 +0900

    [Gold IV] Title: 팰린드롬?, Time: 756 ms, Memory: 255684 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,15 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+def solution(n,arr,que):
+    dp=[[False]*(n+1) for _ in range(n+1)]
+    for i in range(n+1):
+        for j in range(1,n+1-i):
+            s,e=j,j+i
+            if s==e or (arr[s]==arr[e] and (s+1==e or dp[s+1][e-1])):
+                dp[s][e]=True
+    for s,e in que:
+        print(1 if dp[s][e] else 0)
+n=int(input())
+arr=[0]+[*map(int,input().split())]
+que=[[*map(int,input().split())] for _ in range(int(input()))]
+solution(n,arr,que)
