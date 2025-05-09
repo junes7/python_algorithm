22a73f1aa488965e70dc94c57d8c9d1d09f66714
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-09 23:14:31 +0900

    [Silver V] Title: 줄세우기, Time: 132 ms, Memory: 111420 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -0,0 +1,22 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+for k in range(1,int(input())+1):
+    height_sort = []
+    height = list(map(int, input().split()))
+    ans = 0
+    for i in range(1,len(height)):
+        if i == 1:
+            height_sort.append(height[i])
+            continue
+
+        now = height[i]
+        if now > max(height_sort):
+            height_sort.append(now)
+            continue
+
+        for j in range(len(height_sort)):
+            if height_sort[j] > now:
+                height_sort.insert(j, now)
+                ans += (len(height_sort) - j - 1)
+                break
+    print(k,ans)
