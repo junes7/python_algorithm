5b24839ac01fc6b1513bbc27ee8e4bcb6a034585
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-11 12:32:24 +0900

    [Silver V] Title: 줄 세우기, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11536. 줄 세우기/줄 세우기.py
+++ 백준/Silver/11536. 줄 세우기/줄 세우기.py
@@ -1,14 +1,4 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-n=int(input())
-name=[]
-for i in range(n):
-    name+=[list(input().split())]
-in_name  = sorted(name)
-de_name  = sorted(name, reverse=True)
-if in_name == name:
-    print("INCREASING")
-elif de_name == name:
-    print("DECREASING")
-else:
-    print("NEITHER")
+name=[input() for _ in range(int(input()))]
+print("INCREASING" if name==sorted(name) else "DECREASING" if name==sorted(name,reverse=True) else "NEITHER")
