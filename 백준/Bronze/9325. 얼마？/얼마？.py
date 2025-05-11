9375f07ef2aeb7dd5fbad1f1805bfdc036cf905d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-12 00:00:06 +0900

    [Bronze III] Title: 얼마?, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9325. 얼마？/얼마？.py
@@ -0,0 +1,8 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+for _ in range(int(input())) :
+    s = int(input())
+    for _ in range(int(input())) :
+        q, p = map(int, input().split())
+        s += q * p
+    print(s)
