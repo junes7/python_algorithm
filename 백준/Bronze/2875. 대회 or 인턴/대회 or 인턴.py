d0ee020a37ea4605f7c809c1d4cc868aab172943
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-12 18:50:09 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -1,11 +1,11 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 n,m,k=map(int,input().split())
-team = 0
-while True :
+teams=0
+while True:
     n-=2
     m-=1
-    if n<0 or m<0 or (n+m)<k:
+    if n<0 or m<0 or n+m <k:
         break
-    team+=1
-print(team)
+    teams+=1
+print(teams)
