a2a1b297ea86c30be959bcfeebbf9bb3699150f3
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-12 23:25:08 +0900

    [Bronze III] Title: 수도요금, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10707. 수도요금/수도요금.py
+++ 백준/Bronze/10707. 수도요금/수도요금.py
@@ -2,5 +2,5 @@ import sys
 input=lambda:sys.stdin.readline().rstrip()
 arr=[int(input()) for _ in range(5)]
 total_x=arr[0]*arr[4]
-total_y=arr[1] if arr[4]<=arr[2] else arr[1]+(arr[4]-arr[2])*arr[3]
+total_y=arr[1] + (0 if arr[4]<=arr[2] else (arr[4]-arr[2])*arr[3])
 print(total_x if total_x<total_y else total_y)
