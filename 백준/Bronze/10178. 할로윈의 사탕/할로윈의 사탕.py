2343f3d57eb1effcae0deb4a417bf971ba7c796c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-12 23:32:14 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.py
@@ -0,0 +1,5 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+for _ in range(int(input())):
+    c,v=map(int,input().split())
+    print(f"You get {c//v} piece(s) and your dad gets {c%v} piece(s).")
