766f53258232d711aa803c12440fed01a7166257
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-12 19:35:14 +0900

    [Bronze II] Title: 화성 수학, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -0,0 +1,14 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+t=0.0
+for _ in range(int(input())):
+    arr=input().split()
+    t=float(arr[0])
+    for i in range(1,len(arr)):
+        if arr[i]=='@':
+            t*=3
+        elif arr[i]=='%':
+            t+=5
+        elif arr[i]=='#':
+            t-=7
+    print(f"{t:.2f}")
