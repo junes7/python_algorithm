aa241b2d671dc02bff449293dc91cc8c575e68aa
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-14 18:10:29 +0900

    [Bronze III] Title: Mini Fantasy War, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/12790. Mini Fantasy War/Mini Fantasy War.py
+++ 백준/Bronze/12790. Mini Fantasy War/Mini Fantasy War.py
@@ -2,11 +2,8 @@ import sys
 input=lambda:sys.stdin.readline().rstrip()
 for _ in range(int(input())):
     arr=[*map(int,input().split())]
+    b=[arr[i]+arr[4+i] for i in range(4)]
     for i in range(3):
-        if arr[i]+arr[4+i]<1:
-            arr[i]=0
-            arr[4+i]=0 if i-2==0 else 1
-    cp=0
-    for i in range(2):
-        cp+=1*arr[4*i+0]+5*arr[4*i+1]+2*arr[4*i+2]+2*arr[4*i+3]
-    print(cp)
+        if b[i]<1:
+            b[i]=0 if i-2==0 else 1
+    print(1*b[0]+5*b[1]+2*b[2]+2*b[3])
