1a021ddc87e60c006bb1acc78417c072396dc7d3
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-16 18:00:20 +0900

    [Bronze II] Title: 24, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Bronze/1408. 24/24.cc
+++ 백준/Bronze/1408. 24/24.cc
@@ -3,31 +3,28 @@
 #include <vector>
 using namespace std;
 int main(void) {
-    int idx, cnt = 0, time, h, m, s;
-    vector<vector<int>> t(2, vector<int>(3, 0));
     string st;
-    while (cin >> st) {
-        stringstream ss(st);
+    int idx, time;
+    vector<vector<int>> t(2, vector<int>(3, 0));
+    for (int i = 0; i < 2; i++) {
+        cin >> st;
         idx = 0;
+        stringstream ss(st);
         while (getline(ss, st, ':')) {
-            t[cnt][idx++] = stoi(st);
+            t[i][idx++] = stoi(st);
         }
-        cnt += 1;
     }
-    time = t[1][0] * 3600 + t[1][1] * 60 + t[1][2] - (t[0][0] * 3600 + t[0][1] * 60 + t[0][2]);
+    time = (t[1][0] - t[0][0]) * 3600 + (t[1][1] - t[0][1]) * 60 + (t[1][2] - t[0][2]);
     if (time < 0)
         time += 60 * 60 * 24;
-    h = time / 3600;
-    m = (time % 3600) / 60;
-    s = (time % 3600) % 60;
     cout.width(2);
     cout.fill('0');
-    cout << h << ':';
+    cout << time / 3600 << ':';
     cout.width(2);
     cout.fill('0');
-    cout << m << ':';
+    cout << (time % 3600) / 60 << ':';
     cout.width(2);
     cout.fill('0');
-    cout << s;
+    cout << (time % 3600) % 60;
     return 0;
 }
