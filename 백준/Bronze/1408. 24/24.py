d1974e906bf162d6470d60206d3781d32886c1f2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-16 17:45:30 +0900

    [Bronze II] Title: 24, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/1408. 24/24.py
+++ 백준/Bronze/1408. 24/24.py
@@ -2,10 +2,10 @@ import sys
 input=lambda:sys.stdin.readline().rstrip()
 h1,m1,s1=map(int,input().split(':'))
 h2,m2,s2=map(int,input().split(':'))
-time=h2*3600+m2*60+s2-(h1*3600+m1*60+s1)
+time=(h2-h1)*3600+(m2-m1)*60+(s2-s1)
 if time<0:
     time+=60*60*24
 h3=time//3600
 m3=(time%3600)//60
-s3=time%60
+s3=(time%3600)%60
 print("%02d:%02d:%02d" % (h3,m3,s3))
