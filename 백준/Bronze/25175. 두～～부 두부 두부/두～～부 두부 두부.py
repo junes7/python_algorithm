fcca4c0079bab902405f4338b22712de3c3fa484
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-17 23:56:13 +0900

    [Bronze III] Title: 두~~부 두부 두부, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/25175. 두～～부 두부 두부/두～～부 두부 두부.py
@@ -0,0 +1,9 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n, m, k = map(int, input().split())
+while k < 0:
+    k += n
+result = (((k - 3) % n) + m)
+if result > n:
+    result %= n
+print(result)
