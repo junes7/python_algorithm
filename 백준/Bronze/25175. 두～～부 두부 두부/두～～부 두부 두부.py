60297c34cd9c52606a06c1463bd991d6e4e72801
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-18 21:54:14 +0900

    [Bronze III] Title: 두~~부 두부 두부, Time: 88 ms, Memory: 109540 KB -BaekjoonHub

--- 백준/Bronze/25175. 두～～부 두부 두부/두～～부 두부 두부.py
+++ 백준/Bronze/25175. 두～～부 두부 두부/두～～부 두부 두부.py
@@ -1,9 +1,7 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-n, m, k = map(int, input().split())
-while k < 0:
-    k += n
-result = (((k - 3) % n) + m)
-if result > n:
-    result %= n
-print(result)
+n,m,k=map(int,input().split())
+while k<0: k+=n
+rlt=((k-3)%n)+m
+if rlt>n: rlt%=n
+print(rlt)
