c39cccc15587ebba64466debc9f2876a8f70a23e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-18 19:46:38 +0900

    [Bronze III] Title: FizzBuzz, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14761. FizzBuzz/FizzBuzz.py
+++ 백준/Bronze/14761. FizzBuzz/FizzBuzz.py
@@ -1,5 +1,12 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-x,y,n=map(int,input().split())
-for i in range(1,n+1):
-    print("FizzBuzz" if i%x==0 and i%y==0 else "Fizz" if i%x==0 else "Buzz" if i%y==0 else i)
+X, Y, N = map(int, input().split())
+for i in range(1, N+1):
+    if i%X == 0 and i%Y == 0:
+        print("FizzBuzz")
+    elif i%X == 0 :
+        print("Fizz")
+    elif i%Y == 0 :
+        print("Buzz")
+    else:
+        print(i)
