ebbf6fff14a98670dcdaeb6c43a79a84f8430e7a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-21 17:44:45 +0900

    [Bronze III] Title: 서버, Time: 76 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10409. 서버/서버.py
+++ 백준/Bronze/10409. 서버/서버.py
@@ -4,7 +4,8 @@ n,t=map(int,input().split())
 arr=[*map(int,input().split())]
 rlt,cnt=0,0
 for i in range(n):
-    if rlt+arr[i]>t: break
+    if rlt+arr[i]>t:
+        break
     cnt+=1
     rlt+=arr[i]
 print(cnt)
