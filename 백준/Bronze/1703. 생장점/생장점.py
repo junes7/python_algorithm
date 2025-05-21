5f4900d4289d11219f203e725b32d53b06f20e56
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-21 10:27:52 +0900

    [Bronze III] Title: 생장점, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1703. 생장점/생장점.py
+++ 백준/Bronze/1703. 생장점/생장점.py
@@ -1,10 +1,9 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-while 1:
-    line = list(map(int, input().split()))
-    if line[0] == 0: break
-    leaf = 1
-    for i in range(1, len(line), 2):
-        leaf *= line[i]
-        leaf -= line[i + 1]
-    print(leaf)
+while True:
+    line=[*map(int,input().split())]
+    if line[0]==0: break
+    leaves=1
+    for i in range(1,len(line),2):
+        leaves=leaves*line[i]-line[i+1]
+    print(leaves)
