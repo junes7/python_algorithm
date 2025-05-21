47ff45095f2667ca6cbdb649bece623b27baa55f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-21 16:18:57 +0900

    [Bronze III] Title: 계산기 프로그램, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5613. 계산기 프로그램/계산기 프로그램.py
+++ 백준/Bronze/5613. 계산기 프로그램/계산기 프로그램.py
@@ -1,22 +1,16 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-num,op=[],[]
-while 1:
-    s=input()
-    if s=='': break
-    if s!='=':
-        if s.isdigit():
-            num+=[int(s)]
-        else:
-            op+=[s]
-rlt=num[0]
-for i in range(len(op)):
-    if op[i]=='+':
-        rlt+=num[i+1]
-    elif op[i]=='-':
-        rlt-=num[i+1]
-    elif op[i]=='*':
-        rlt*=num[i+1]
-    elif op[i]=='/':
-        rlt//=num[i+1]
-print(rlt)
+res=int(input())
+while True:
+    op=input()
+    if op=='=': break
+    n=int(input())
+    if op=='+':
+        res+=n
+    elif op=='-':
+        res-=n
+    elif op=='*':
+        res*=n
+    else:
+        res//=n
+print(res)
