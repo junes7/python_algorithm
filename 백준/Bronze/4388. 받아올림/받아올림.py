6312ed10ea0105c54970ec1f0d0066a8be641b04
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-22 08:46:19 +0900

    [Bronze III] Title: 받아올림, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/4388. 받아올림/받아올림.py
@@ -0,0 +1,13 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+while True:
+    a,b=input().split()
+    if a==b=='0': break
+    if len(a)<len(b): a,b=b,a;
+    while len(a)!=len(b): b='0'+b
+    carry,cnt=0,0
+    for i in range(len(a)-1,-1,-1):
+        if carry>0: cnt+=1
+        carry=(carry+int(a[i])+int(b[i]))//10
+    if carry>0: cnt+=1
+    print(cnt)
