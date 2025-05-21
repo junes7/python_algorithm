f13ea94cb52f42affa024f93d6a1e47be9f53c10
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-21 17:04:32 +0900

    [Bronze III] Title: 미국 스타일, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2712. 미국 스타일/미국 스타일.py
+++ 백준/Bronze/2712. 미국 스타일/미국 스타일.py
@@ -4,10 +4,10 @@ for _ in range(int(input())):
     n,unit=input().split()
     n=float(n)
     if unit=='kg':
-        print("%.4f lb" % (n*2.2046/1))
+        print("%.4f lb" % (n*2.2046))
     elif unit=='lb':
-        print("%.4f kg" % (n*0.4536/1))
+        print("%.4f kg" % (n*0.4536))
     elif unit=='l':
-        print("%.4f g" % (n*0.2642/1))
+        print("%.4f g" % (n*0.2642))
     elif unit=='g':
-        print("%.4f l" % (n*3.7854/1))
+        print("%.4f l" % (n*3.7854))
