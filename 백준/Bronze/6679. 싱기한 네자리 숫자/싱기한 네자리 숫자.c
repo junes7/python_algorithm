f5bff524054af2310ecfd04811cc5511151f7447
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-21 15:07:38 +0900

    [Bronze II] Title: 싱기한 네자리 숫자, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/6679. 싱기한 네자리 숫자/싱기한 네자리 숫자.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int num_len(int n, int t) {
+    int rem = 0;
+    while (n > 0) {
+        rem += n % t;
+        n /= t;
+    }
+    return rem;
+}
+int main(void) {
+    for (int i = 1000; i < 10000; i++) {
+        if (num_len(i, 10) == num_len(i, 12) && num_len(i, 12) == num_len(i, 16)) {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
