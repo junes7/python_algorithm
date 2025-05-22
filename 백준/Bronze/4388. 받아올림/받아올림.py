9ae17b76b99961c6ea21165832cb252aa0583d92
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-22 11:32:35 +0900

    [Bronze III] Title: 받아올림, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/4388. 받아올림/받아올림.py
+++ 백준/Bronze/4388. 받아올림/받아올림.py
@@ -7,7 +7,6 @@ while True:
     while len(a)!=len(b): b='0'+b
     carry,cnt=0,0
     for i in range(len(a)-1,-1,-1):
-        if carry>0: cnt+=1
         carry=(carry+int(a[i])+int(b[i]))//10
-    if carry>0: cnt+=1
+        if carry>0: cnt+=1
     print(cnt)
