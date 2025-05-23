81776af0f7237f440d1186daf9b7fee3212f4f9c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-23 14:39:54 +0900

    [Silver V] Title: A+B, Time: 24 ms, Memory: 2024 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/27674. A＋B/A＋B.cc
+++ 백준/Silver/27674. A＋B/A＋B.cc
@@ -3,16 +3,17 @@
 #include <vector>
 using namespace std;
 int main(void) {
-    int t, len, m, total;
-    string st, junk;
+    int t, len;
+    long long m, total;
+    string st;
     cin >> t;
     for (int i = 0; i < t; i++) {
         cin >> st;
         len = st.size(), m = 1, total = 0;
-        vector<int> arr(len);
+        vector<long long> arr(len);
         for (int i = 0; i < len; i++)
             arr[i] = st[i] - 48;
-        sort(arr.begin(), arr.end(), greater<int>());
+        sort(arr.begin(), arr.end(), greater<long long>());
         for (int i = len - 2; i >= 0; i--) {
             total += arr[i] * m;
             m *= 10;
