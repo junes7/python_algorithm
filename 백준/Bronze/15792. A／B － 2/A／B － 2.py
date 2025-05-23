b107c2bf48fd1a23837b0111701e1930c79e764f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-23 23:44:38 +0900

    [Bronze I] Title: A/B - 2, Time: 124 ms, Memory: 108384 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,4 +1,11 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 a,b=map(int,input().split())
-print(round(a/b,1000))
+t=str(a//b)+'.'
+if a%b:
+    i=0
+    while a%b and i<1000:
+        a=a%b*10
+        i+=1
+        t+=str(a//b)
+print(t)
