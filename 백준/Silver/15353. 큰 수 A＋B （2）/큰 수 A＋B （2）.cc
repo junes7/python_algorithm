14d9e3e79099fd8965e88d0053cfa0ee145abb86
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-23 12:15:01 +0900

    [Silver III] Title: 큰 수 A+B (2), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
+++ 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
@@ -2,28 +2,16 @@
 #include <iostream>
 using namespace std;
 int main(void) {
-    int carry = 0, idx, t;
+    int carry = 0;
     string a, b, rlt = "";
     cin >> a >> b;
     if (a.size() < b.size()) swap(a, b);
     while (a.size() != b.size()) b = '0' + b;
     for (int i = a.size() - 1; i >= 0; i--) {
-        t = a[i] - 48 + b[i] - 48 + carry;
-        rlt = to_string(t % 10) + rlt;
-        carry = t / 10;
+        rlt = to_string((carry + a[i] - 48 + b[i] - 48) % 10) + rlt;
+        carry = (carry + a[i] - 48 + b[i] - 48) / 10;
     }
-    if (carry > 0)
-        rlt = to_string(carry) + rlt;
-    for (int i = 0; i < rlt.size(); i++) {
-        if (rlt[i] != '0') {
-            idx = i;
-            break;
-        }
-    }
-    if (idx >= rlt.size())
-        rlt = "0";
-    else
-        rlt.erase(0, idx);
+    if (carry) rlt = to_string(carry) + rlt;
     cout << rlt;
     return 0;
 }
