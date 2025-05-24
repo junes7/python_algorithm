430c9750e6daa1dd0099f4e465ae158688c23b51
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-24 11:16:35 +0900

    [Bronze V] Title: A+B - 8, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -2,4 +2,4 @@ import sys
 input=lambda:sys.stdin.readline().rstrip()
 for i in range(1,int(input())+1):
     a,b=map(int,input().split())
-    print("Case #%d: %d + %d = %d" % (i,a,b,a+b))
+    print(f"Case #{i}: {a} + {b} = {a+b}")
