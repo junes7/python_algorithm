f5c969fb3558d7b08b4fb5baff0f08f1b578ce75
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-26 00:09:40 +0900

    [Bronze III] Title: ZOAC 4, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -1,7 +1,4 @@
 import sys,math
 input=lambda:sys.stdin.readline().rstrip()
-H,W,N,M = list(map(int,input().split(' ')))
-a = math.ceil(H/(N+1)) # 세로에 몇 명이 앉는지를 계산합니다
-b = math.ceil(W/(M+1)) # 가로에 몇 명이 앉는지를 계산합니다
-answer = a*b #가로와 세로의 값을 곱합니다
-print(answer)
+h,w,n,m=map(int,input().split())
+print(math.ceil(h/(n+1))*math.ceil(w/(m+1)))
