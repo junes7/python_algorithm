ef7ea6483a373c68cd13b2221f2ec2f1ac0e6db6
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-05-30 23:51:10 +0900

    [Bronze III] Title: 단원평가, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/31798. 단원평가/단원평가.py
@@ -0,0 +1,9 @@
+import sys
+input=lambda:sys.stdin.readline().strip()
+a, b, c = map(int, input().split())
+if a == 0:
+    print(int(c**2 - b))
+elif b == 0:
+    print(int(c**2 - a))
+elif c == 0:
+    print(int((a + b) ** (1 / 2)))
