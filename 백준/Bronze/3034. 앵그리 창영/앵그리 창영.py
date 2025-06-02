3efa5926e8d1c59693ab548d2c15d3345e4e2ed1
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-02 20:36:16 +0900

    [Bronze III] Title: 앵그리 창영, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3034. 앵그리 창영/앵그리 창영.py
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.py
@@ -3,5 +3,4 @@ input=lambda:sys.stdin.readline().rstrip()
 n,w,h=map(int,input().split())
 d=(w**2+h**2)**(0.5)
 for i in range(n):
-    t=int(input())
-    print("DA" if t<=d else "NE")
+    print("DA" if int(input())<=d else "NE")
