398105dd45e0369099c754660b86a785474dd4b2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-02 21:14:11 +0900

    [Bronze III] Title: 369, Time: 136 ms, Memory: 109544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,7 +1,9 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 cnt=0
-for i in range(3,int(input())+1):
-    t=str(i)
-    cnt+=t.count('3')+t.count('6')+t.count('9')
+for i in range(1,int(input())+1):
+    while i>0:
+        if i%10==3 or i%10==6 or i%10==9:
+            cnt+=1 
+        i//=10
 print(cnt)
