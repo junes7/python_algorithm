07deeca5ace0406a637a305cd12d2265a1f2d6cb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-03 10:44:29 +0900

    [Bronze IV] Title: 특별한 작은 분수, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/27890. 특별한 작은 분수/특별한 작은 분수.py
+++ 백준/Bronze/27890. 특별한 작은 분수/특별한 작은 분수.py
@@ -2,8 +2,5 @@ import sys
 input=lambda:sys.stdin.readline().rstrip()
 x,n=map(int,input().split())
 for _ in range(n):
-    if x%2==0:
-        x=(x//2)^6
-    else:
-        x=(2*x)^6
+    x=(x//2 if x%2==0 else 2*x)^6
 print(x)
