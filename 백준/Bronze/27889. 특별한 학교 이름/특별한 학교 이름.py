f6401cf131aafb871218cc03dff8e84d6e93a7fd
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-03 15:17:46 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -1,5 +1,4 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-abbrev={"NLCS":"North London Collegiate School","BHA":"Branksome Hall Asia",
-"KIS":"Korea International School","SJA":"St. Johnsbury Academy"}
-print(abbrev[input()])
+dic={"NLCS":"North London Collegiate School","BHA":"Branksome Hall Asia", "KIS":"Korea International School", "SJA":"St. Johnsbury Academy"}
+print(dic[input()])
