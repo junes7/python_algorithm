29da602aa17b4f4bf35fdef2a82b032a67a73a0f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-08 11:59:07 +0900

    [Bronze III] Title: 스케이트보드, Time: 172 ms, Memory: 111492 KB -BaekjoonHub

--- 백준/Bronze/28417. 스케이트보드/스케이트보드.py
+++ 백준/Bronze/28417. 스케이트보드/스케이트보드.py
@@ -1,11 +1,9 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-rlt = 0
-for _ in range(int(input())):
-    Skateboard = list(map(int, input().split()))
-    Run = Skateboard[:2]
-    Trick = Skateboard[2:]
-    Run.sort(reverse=True)
-    Trick.sort(reverse=True)
-    rlt=max(rlt, Run[0] + sum(Trick[:2]))
-print(rlt)
+n=int(input())
+total=[0]*n
+for i in range(n):
+    scr=[*map(int,input().split())]
+    run,trick=sorted(scr[:2],reverse=True),sorted(scr[2:],reverse=True)
+    total[i]=run[0]+trick[0]+trick[1]
+print(max(total))
