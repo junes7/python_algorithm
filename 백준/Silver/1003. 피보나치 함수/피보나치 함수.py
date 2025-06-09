087037386786f44263e4ae8cad1d8ed38ddd0330
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-09 19:10:38 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,7 +1,9 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 for _ in range(int(input())):
-    a,b=1,0
-    for _ in range(int(input())):
-        a,b=b,a+b
-    print(a,b)
+    n=int(input())
+    dpz,dpo=[1],[0]
+    for i in range(n):
+        dpz+=[dpo[i]]
+        dpo+=[dpz[i]+dpo[i]]
+    print(dpz[n],dpo[n])
