0d8a1eb2ec646df11d56403d937fb2f17e5e8f86
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-10 23:57:08 +0900

    [Bronze II] Title: 연속구간, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2495. 연속구간/연속구간.py
@@ -0,0 +1,14 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+for _ in range(3):
+    s = str(input())
+    mymax = 1
+    cnt = 1
+    for i in range(1,len(s)):
+        if s[i]==s[i-1]:
+            cnt+=1
+        else:
+            mymax=max(cnt,mymax)
+            cnt=1
+    mymax = max(cnt, mymax)
+    print(mymax)
