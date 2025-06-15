0458571ad332fa2a6e2a71d4dbbb2c69084227cc
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-15 17:40:02 +0900

    [Bronze II] Title: 대표값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2592. 대표값/대표값.cc
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void) {
+    int tot = 0, n;
+    vector<int> cnt(1001, 0);
+    for (int i = 0; i < 10; i++) {
+        cin >> n;
+        tot += n;
+        cnt[n] += 1;
+    }
+    cout << tot / 10 << "\n";
+    cout << find(cnt.begin(), cnt.end(), *max_element(cnt.begin(), cnt.end())) - cnt.begin();
+    return 0;
+}
