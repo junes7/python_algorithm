1c907a4d5f1df9f11e28f8671b152b09f5ccfd3c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-15 23:51:38 +0900

    [Bronze II] Title: 주차의 신, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5054. 주차의 신/주차의 신.py
@@ -0,0 +1,7 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    store = sorted(map(int, input().split()))
+    print((store[-1] - store[0]) * 2)
