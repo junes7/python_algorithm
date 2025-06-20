255da309423acc40606bc0676443d6dc55fa68f7
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-20 23:34:20 +0900

    [Silver II] Title: 마지막 팩토리얼 수, Time: 100 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Silver/2553. 마지막 팩토리얼 수/마지막 팩토리얼 수.py
+++ 백준/Silver/2553. 마지막 팩토리얼 수/마지막 팩토리얼 수.py
@@ -4,8 +4,7 @@ n=int(input())
 f=1
 for i in range(1,n+1):
     f*=i
-rlt=str(f)
-for i in range(len(rlt)-1,-1,-1):
-    if rlt[i]!='0':
-        print(rlt[i])
-        break
+    while f%10==0:
+        f//=10
+    f%=10**20
+print(f%10)
