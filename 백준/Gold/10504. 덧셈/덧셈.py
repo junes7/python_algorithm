be33f5624246b51dfc62615f06e14903da7c2b96
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-20 15:44:40 +0900

    [Gold III] Title: 덧셈, Time: 1644 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/10504. 덧셈/덧셈.py
+++ 백준/Gold/10504. 덧셈/덧셈.py
@@ -1,23 +1,18 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 for _ in range(int(input())):
-    n=int(input())
-    arr,x=[],2
+    n,x=int(input()),2
     while 1:
         t=n-x*(x+1)//2
         if t<0:
+            st=""
             break
         if t%x==0:
+            st=str(n)+" = ";
             rem=t//x
-            for i in range(1,x+1):
-                arr+=[i+rem]
+            for i in range(1,x):
+                st+=str(i+rem)+" + "
+            st+=str(x+rem)
             break
         x+=1
-    if len(arr):
-        st=str(n)+" = ";
-        for i in range(len(arr)-1):
-            st+=str(arr[i])+" + "
-        st+=str(arr[len(arr)-1])
-        print(st)
-    else:
-        print("IMPOSSIBLE")
+    print("IMPOSSIBLE" if len(st)==0 else st)
