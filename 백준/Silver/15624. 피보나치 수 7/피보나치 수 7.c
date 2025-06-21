5d78d7870573cff8c9ab46a28d8d0b5706a6ce77
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-21 23:51:17 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 4 ms, Memory: 5020 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+    int n, *f;
+    scanf("%d", &n);
+    f = (int*)malloc(sizeof(int) * (n + 1));
+    f[0] = 0, f[1] = 1;
+    for (int i = 2; i < n + 1; i++)
+        f[i] = (f[i - 1] + f[i - 2]) % 1000000007;
+    printf("%d", f[n]);
+    return 0;
+}
