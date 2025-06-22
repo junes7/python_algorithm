ebe00691ff0e081f8d304bc95cf772c5a029c26c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-22 15:47:33 +0900

    [Silver III] Title: 피보나치 더하기, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/30824. 피보나치 더하기/피보나치 더하기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool solve(vector<long long> &f, int k, long long x) {
+    if (k == 1) {
+        for (int i = 1; i < 79; i++)
+            if (f[i] == x) return true;
+    } else if (k == 2) {
+        for (int i = 1; i < 79; i++) {
+            for (int j = 1; j < 79; j++)
+                if (f[i] + f[j] == x) return true;
+        }
+    } else {
+        for (int i = 1; i < 79; i++) {
+            for (int j = 1; j < 79; j++) {
+                for (int k = 1; k < 79; k++)
+                    if (f[i] + f[j] + f[k] == x) return true;
+            }
+        }
+    }
+    return false;
+}
+int main(void) {
+    int t, k;
+    long long x;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> t;
+    vector<long long> f(79);
+    f[0] = 0, f[1] = 1;
+    for (int i = 2; i < 79; i++)
+        f[i] = f[i - 1] + f[i - 2];
+    for (int i = 0; i < t; i++) {
+        cin >> k >> x;
+        cout << (solve(f, k, x) == 1 ? "YES" : "NO") << "\n";
+    }
+    return 0;
+}
