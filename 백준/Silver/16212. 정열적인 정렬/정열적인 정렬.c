1a99a56ea429383f7f2db27b15e1cf95df8a1c50
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-27 19:55:50 +0900

    [Silver V] Title: 정열적인 정렬, Time: 212 ms, Memory: 5024 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int cmp(const void *pa, const void *pb) {
+    const int a = *(int *)pa;
+    const int b = *(int *)pb;
+    return a > b;
+}
+int main(void) {
+    int n, *arr;
+    scanf("%d", &n);
+    arr = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    qsort(arr, n, sizeof(int), cmp);
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
