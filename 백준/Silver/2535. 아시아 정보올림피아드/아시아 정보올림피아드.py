a49ec405749adbd72a572d23ce590f49ba47fa84
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-06-29 23:58:21 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.py
@@ -0,0 +1,14 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n = int(input())
+lst = []
+for _ in range(n):
+    lst.append(list(map(int, sys.stdin.readline().split())))
+lst.sort(key = lambda x: -x[2])
+print(*lst[0][:2])
+print(*lst[1][:2])
+i = 2
+if lst[0][0] == lst[1][0]:
+    while lst[0][0] == lst[i][0]:
+        i += 1
+print(*lst[i][:2])
