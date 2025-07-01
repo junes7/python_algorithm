f75c45d672f7f6fe81f5d4f6ae7ac4883f521082
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-01 15:35:42 +0900

    [Bronze II] Title: 콘테스트, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5576. 콘테스트/콘테스트.py
@@ -0,0 +1,5 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+w=sorted([int(input()) for _ in range(10)],reverse=True)[:3]
+k=sorted([int(input()) for _ in range(10)],reverse=True)[:3]
+print(sum(w),sum(k))
