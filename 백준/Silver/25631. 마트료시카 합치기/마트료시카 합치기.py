8135b034297bdbce9704482afe3cebc2b284f79c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-08 17:34:22 +0900

    [Silver V] Title: 마트료시카 합치기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.py
@@ -0,0 +1,9 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n=int(input())
+arr=sorted([*map(int,input().split())])
+dic,rlt={},0
+for c in arr:
+    dic[c]=dic.get(c,0)+1
+    rlt=max(rlt,dic.get(c))
+print(rlt)
