b1e6f687a9f22f6769e61cce26abde57d5bc246f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-11 17:39:45 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 48 ms, Memory: 35480 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -2,12 +2,12 @@ import sys
 input=lambda:sys.stdin.readline().rstrip()
 n=int(input())
 arr=[*map(int,input().split())]
-maxn=0
-for i in range(n):
-    cnt=0
-    for j in range(i+1,n):
-        if arr[j]>arr[i]:
-            break
+tar,cnt,maxn=0,0,0
+for i in arr:
+    if tar<i:
+        tar=i
+        cnt=0
+    else:
         cnt+=1
     if maxn<cnt:
         maxn=cnt
