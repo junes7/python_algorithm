9806c4f0e75750fc967a82e1f2a9fa528dc60686
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-14 16:09:07 +0900

    [Silver IV] Title: 나는 위대한 슈퍼스타K, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2865. 나는 위대한 슈퍼스타K/나는 위대한 슈퍼스타K.py
+++ 백준/Silver/2865. 나는 위대한 슈퍼스타K/나는 위대한 슈퍼스타K.py
@@ -1,12 +1,11 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-n, m, k = map(int, input().split())
-sing = [0 for _ in range(n)]
+n,m,k=map(int,input().split())
+sing=[0.0]*n
 for _ in range(m):
-    scr = list(input().split())
-    for i in range(0, len(scr), 2):
-        if sing[int(scr[i])-1] < float(scr[i+1]):
-            sing[int(scr[i])-1] = float(scr[i+1])
-sing.sort(reverse=True)
-sing = sing[:k]
+    scr=[*map(float,input().split())]
+    for i in range(0,len(scr),2):
+        if sing[int(scr[i])-1]<scr[i+1]:
+            sing[int(scr[i])-1]=scr[i+1]
+sing=sorted(sing,reverse=True)[:k]
 print(round(sum(sing),1))
