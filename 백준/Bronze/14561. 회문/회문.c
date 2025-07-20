c126d1be665adc4518eb75186fc371d49c96c059
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-20 22:53:04 +0900

    [Bronze II] Title: 회문, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14561. 회문/회문.c
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+    int t, n, len, idx;
+    long long a, tmp;
+    bool flag;
+    scanf("%d", &t);
+    char *arr = (char *)malloc(sizeof(char) * 25);
+    for (int i = 0; i < t; i++) {
+        idx = 0;
+        scanf("%lld %d", &a, &n);
+        memset(arr, 0, sizeof(char) * 25);
+        while (a > 0) {
+            tmp = a % n;
+            arr[idx++] = tmp < 10 ? (char)(tmp + 48) : (char)(tmp + 55);
+            a /= n;
+        }
+        flag = true;
+        for (int j = 0; j < idx / 2; j++) {
+            if (arr[j] != arr[idx - 1 - j]) {
+                flag = false;
+                break;
+            }
+        }
+        printf("%d\n", flag ? 1 : 0);
+    }
+    return 0;
+}
