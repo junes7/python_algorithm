52c356dfd21caf7f1e3fb6ae79f63996e9830ee5
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-20 16:58:54 +0900

    [Bronze III] Title: Easy Encryption, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/9783. Easy Encryption/Easy Encryption.py
+++ 백준/Bronze/9783. Easy Encryption/Easy Encryption.py
@@ -3,11 +3,11 @@ input=lambda:sys.stdin.readline().rstrip()
 st,rlt=input(),""
 for i in range(len(st)):
     if st[i].isupper():
-        t=str(ord(st[i])-38)
-        rlt+="0"+t if len(t)==1 else t
+        t=ord(st[i])-38
+        rlt+="0"+str(t) if t<10 else str(t)
     elif st[i].islower():
-        t=str(ord(st[i])-96)
-        rlt+="0"+t if len(t)==1 else t
+        t=ord(st[i])-96
+        rlt+="0"+str(t) if t<10 else str(t)
     elif st[i].isdigit():
         rlt+="#"+str(st[i])
     else:
