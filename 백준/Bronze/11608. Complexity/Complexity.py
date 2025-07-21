048958a4a990114a336168d59f95b1ab8f6efc42
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-21 11:19:39 +0900

    [Bronze II] Title: Complexity, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/11608. Complexity/Complexity.py
@@ -0,0 +1,14 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+alpha,v=[0]*26,[]
+st,cnt,rlt=input(),0,0
+for i in range(len(st)):
+    alpha[ord(st[i])-97]+=1
+for i in range(26):
+    if alpha[i]:
+        v+=[alpha[i]]
+        cnt+=1
+v.sort()
+for i in range(cnt-2):
+    rlt+=v[i]
+print(rlt)
