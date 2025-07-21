773e466d7b3a46c16bbca4208a6254d6f1a93c94
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-22 08:25:20 +0900

    [Silver IV] Title: 로프, Time: 124 ms, Memory: 112044 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,8 +1,12 @@
 import sys
-n=int(sys.stdin.readline())
-ropes=sorted([int(sys.stdin.readline()) for i in range(n)])
-best=0
-for i in range(n):
-    if best<ropes[i]*(n-i):
-        best=ropes[i]*(n-i)
-print(best)
+input=lambda:sys.stdin.readline().rstrip()
+def max_weight(N, ropes):
+    ropes.sort(reverse=False)  # 로프 정렬
+    max_w = 0
+    for i in range(N):
+        current_w = ropes[i] * (N-i)  # 현재 로프부터 사용할 때의 최대 중량
+        max_w = max(max_w, current_w)  # 최대 중량 갱신
+    return max_w
+N = int(input())
+ropes = [int(input()) for _ in range(N)]
+print(max_weight(N, ropes))
