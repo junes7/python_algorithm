2d66f7926f69ea392984538e7149f55af3c679ad
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-24 22:34:34 +0900

    [Bronze II] Title: 사과와 바나나 나눠주기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14914. 사과와 바나나 나눠주기/사과와 바나나 나눠주기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int gcd(int x, int y) {
+    return x % y == 0 ? y : gcd(y, x % y);
+}
+int main(void) {
+    int a, b, gcdn;
+    cin >> a >> b;
+    gcdn = gcd(a, b);
+    for (int i = 1; i < gcdn + 1; i++) {
+        if (gcdn % i == 0) {
+            cout << i << ' ' << a / i << ' ' << b / i << "\n";
+        }
+    }
+    return 0;
+}
