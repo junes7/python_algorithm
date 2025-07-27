7610e646a5fdde4fd3003230c640894fc768e178
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-28 00:05:31 +0900

    [Bronze II] Title: 창영마을, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/3028. 창영마을/창영마을.py
@@ -0,0 +1,12 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+li = [1, 0, 0]
+s = input()
+for c in s:
+    if c == 'A':
+        li[0], li[1] = li[1], li[0]
+    elif c == 'B':
+        li[1], li[2] = li[2], li[1]
+    else:
+        li[0], li[2] = li[2], li[0]
+print(li.index(1) + 1)
