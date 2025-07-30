c8f3705b44513ca9c5cab90c0863a1e15df84077
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-30 23:58:42 +0900

    [Bronze II] Title: 비교 연산자, Time: 176 ms, Memory: 112040 KB -BaekjoonHub

---
+++ 백준/Bronze/5656. 비교 연산자/비교 연산자.py
@@ -0,0 +1,13 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+cnt=1
+while True:
+    num1,cmd,num2=input().strip().split()
+    if cmd=="E":break
+    if cmd=="!=":print("Case {}: ".format(cnt)+str(int(num1)!=int(num2)).lower())
+    elif cmd=="<":print("Case {}: ".format(cnt)+str(int(num1)<int(num2)).lower())
+    elif cmd=="<=":print("Case {}: ".format(cnt)+str(int(num1)<=int(num2)).lower())
+    elif cmd==">":print("Case {}: ".format(cnt)+str(int(num1)>int(num2)).lower())
+    elif cmd==">=":print("Case {}: ".format(cnt)+str(int(num1)>=int(num2)).lower())
+    elif cmd=="==":print("Case {}: ".format(cnt)+str(int(num1)==int(num2)).lower())
+    cnt+=1
