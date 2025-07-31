50f7264021fdcdc394538525be467c209b1b77ef
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-07-31 15:49:59 +0900

    [Bronze II] Title: 비교 연산자, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5656. 비교 연산자/비교 연산자.py
+++ 백준/Bronze/5656. 비교 연산자/비교 연산자.py
@@ -2,12 +2,7 @@ import sys
 input=lambda:sys.stdin.readline().rstrip()
 cnt=1
 while True:
-    num1,cmd,num2=input().strip().split()
+    num1,cmd,num2=input().split()
     if cmd=="E":break
-    if cmd=="!=":print("Case {}: ".format(cnt)+str(int(num1)!=int(num2)).lower())
-    elif cmd=="<":print("Case {}: ".format(cnt)+str(int(num1)<int(num2)).lower())
-    elif cmd=="<=":print("Case {}: ".format(cnt)+str(int(num1)<=int(num2)).lower())
-    elif cmd==">":print("Case {}: ".format(cnt)+str(int(num1)>int(num2)).lower())
-    elif cmd==">=":print("Case {}: ".format(cnt)+str(int(num1)>=int(num2)).lower())
-    elif cmd=="==":print("Case {}: ".format(cnt)+str(int(num1)==int(num2)).lower())
+    print(f"Case {cnt}: {str(eval(cmd.join([num1,num2]))).lower()}")
     cnt+=1
