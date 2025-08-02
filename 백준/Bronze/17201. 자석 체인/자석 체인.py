91aba64008bf0615e97d015140fdb58da2897566
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-08-02 23:58:34 +0900

    [Bronze III] Title: 자석 체인, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/17201. 자석 체인/자석 체인.py
@@ -0,0 +1,8 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n=int(input())
+s=input()
+cnt=0
+for i in range(len(s)-1):
+    if s[i]==s[i+1]:cnt=1;print("No");exit()
+if cnt==0: print("Yes")
