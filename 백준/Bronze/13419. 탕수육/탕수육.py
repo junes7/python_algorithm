38f6796a560580c0903ede9630444ada60b46cc9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-08-03 16:43:00 +0900

    [Bronze II] Title: 탕수육, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/13419. 탕수육/탕수육.py
+++ 백준/Bronze/13419. 탕수육/탕수육.py
@@ -1,12 +1,9 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-t,games=int(input()),[]
-for _ in range(t):
-    games.append(input())
-for game in games:
-    if len(game) %2 == 0: #문자열의 길이가 짝수일 때
-        print(game[0::2])  #인덱스가 0인것 부터 2씩 증가하면서 가져온다
-        print(game[1::2])  #인덱스가 1인것 부터 2씩 증가면서 가져온다
+for _ in range(int(input())):
+    wave=input()
+    if len(wave)%2!=0:
+        print(wave[0::2],wave[1::2],sep='')
+        print(wave[1::2],wave[0::2],sep='')
     else:
-        print(game[0::2],game[1::2], sep='')  #sep='' 안붙여주면 공백이 출력되기 때문
-        print(game[1::2],game[0::2],sep='')
+        print(wave[0::2],wave[1::2],sep="\n")
