7620866c3ccf4886d0a21aa9f7df1d8bdca077d9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-08-03 23:59:36 +0900

    [Bronze II] Title: 너의 이름은 몇 점이니?, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/15813. 너의 이름은 몇 점이니？/너의 이름은 몇 점이니？.py
@@ -0,0 +1,8 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+count = int(input())
+name = list(input())
+ascii_list = []
+for i in name:
+    ascii_list.append(ord(i)-64)
+print(sum(ascii_list))
