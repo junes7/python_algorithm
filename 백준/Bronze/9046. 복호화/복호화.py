d0226cfae669391f6508c724de4529de22003763
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-08-04 19:55:55 +0900

    [Bronze II] Title: 복호화, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9046. 복호화/복호화.py
+++ 백준/Bronze/9046. 복호화/복호화.py
@@ -1,12 +1,12 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-T = int(input())
-for _ in range(T):
-    s = input().replace(' ', '')
-    count_s = [0] * 26
-    for i in s:
-        count_s[ord(i) - 97] += 1
-    if count_s.count(max(count_s)) > 1:
-        print('?')
-    else:
-        print(chr(97 + count_s.index(max(count_s))))
+for _ in range(int(input())):
+    st,alpha=input(),{}
+    for c in st:
+        if c.isalpha():
+            if c in alpha:
+                alpha[c]+=1
+            else:
+                alpha[c]=1
+    alpha_sort=sorted(alpha.items(),key=lambda x:(-x[1]))
+    print("?" if len(alpha_sort)>1 and alpha_sort[1][1]==alpha_sort[0][1] else alpha_sort[0][0])
