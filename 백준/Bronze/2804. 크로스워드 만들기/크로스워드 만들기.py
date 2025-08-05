7ebd836d54db2b6591b2d1c98c878a3ce357ae41
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-08-05 16:53:35 +0900

    [Bronze I] Title: 크로스워드 만들기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2804. 크로스워드 만들기/크로스워드 만들기.py
+++ 백준/Bronze/2804. 크로스워드 만들기/크로스워드 만들기.py
@@ -1,17 +1,16 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-A, B = input().split()
-N = len(A)
-M = len(B)
-matrix = [['.'] * N for _ in range(M)]
-for i in range(N):
-    if A[i] in B:
-        row = i
-        col = B.index(A[i])
+word1,word2=input().split()
+l1,l2,idx=len(word1),len(word2),[0]*2
+rlt=[['.']*l1 for _ in range(l2)]
+for i in range(l1):
+    if word1[i] in word2:
+        idx[0]=i
+        idx[1]=word2.index(word1[i])
         break
-for i in range(M):
-    matrix[i][row] = B[i]
-for i in range(N):
-    matrix[col][i] = A[i]
-for i in matrix:
-    print(''.join(i))
+for i in range(l2):
+    rlt[i][idx[0]]=word2[i]
+for i in range(l1):
+    rlt[idx[1]][i]=word1[i]
+for c in rlt:
+    print(''.join(c))
