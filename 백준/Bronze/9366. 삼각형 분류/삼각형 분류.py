d35ea1347289eb9d20b89ff7f6a6a9518d2832bd
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-08-13 22:30:48 +0900

    [Bronze III] Title: 삼각형 분류, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/9366. 삼각형 분류/삼각형 분류.py
+++ 백준/Bronze/9366. 삼각형 분류/삼각형 분류.py
@@ -1,8 +1,8 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
-for i in range(1, int(input()) + 1) :
-    a, b, c = map(int, input().split())
-    if a == b == c : print(f"Case #{i}: equilateral")
-    elif max([a, b, c]) < sum([a, b, c]) - max([a, b, c]) and (a == b or b == c or c == a) : print(f"Case #{i}: isosceles")
-    elif max([a, b, c]) < sum([a, b, c]) - max([a, b, c]) : print(f"Case #{i}: scalene")
-    else : print(f"Case #{i}: invalid!")
+for i in range(1,int(input())+1):
+    arr=[*map(int,input().split())]
+    maxn=max(arr)
+    if arr[0]==arr[1]==arr[2]: print(f"Case #{i}: equilateral")
+    elif maxn<sum(arr)-maxn: print(f"Case #{i}: {'isosceles' if arr[0]==arr[1] or arr[1]==arr[2] or arr[0]==arr[2] else 'scalene'}")
+    else: print(f"Case #{i}: invalid!")
