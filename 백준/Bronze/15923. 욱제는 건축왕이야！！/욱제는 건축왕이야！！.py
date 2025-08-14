61538308ede0f6dbe804e88d77e6bf1fa3d644e8
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-08-14 23:41:36 +0900

    [Bronze III] Title: 욱제는 건축왕이야!!, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/15923. 욱제는 건축왕이야！！/욱제는 건축왕이야！！.py
@@ -0,0 +1,7 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+x, y = [], []
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    x.append(a); y.append(b)
+print((max(x)-min(x) + max(y)-min(y))*2)
