aeb116c30ec33880c7cc0e029b26ca70bebc7f7f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-08-21 00:02:48 +0900

    [Bronze I] Title: 성 지키기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/1236. 성 지키기/성 지키기.py
@@ -0,0 +1,14 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n, m = map(int,input().split())
+board = []
+for _ in range(n):
+    board.append(input())
+a, b = 0, 0
+for i in range(n):
+    if "X" not in board[i]:
+        a += 1
+for j in range(m):
+    if "X" not in [board[i][j] for i in range(n)]:
+        b += 1
+print(max(a ,b))
