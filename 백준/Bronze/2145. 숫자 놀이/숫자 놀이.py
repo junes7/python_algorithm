7085beca0570b6b0dc6402b6a504f8eb8e7a743d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-08-26 23:54:59 +0900

    [Bronze II] Title: 숫자 놀이, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2145. 숫자 놀이/숫자 놀이.py
@@ -0,0 +1,9 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+while(True):
+    n = int(input())
+    if n == 0:
+        break
+    while(n>9):
+        n = sum(map(int,list(str(n))))
+    print(n)
