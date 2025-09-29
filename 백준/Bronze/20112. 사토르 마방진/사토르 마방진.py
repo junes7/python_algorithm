87bf2f6853a0636b56f4e04cf0fdf6cb5beb196f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-09-30 08:58:13 +0900

    [Bronze I] Title: 사토르 마방진, Time: 104 ms, Memory: 111412 KB -BaekjoonHub

---
+++ 백준/Bronze/20112. 사토르 마방진/사토르 마방진.py
@@ -0,0 +1,14 @@
+import sys
+N = int(sys.stdin.readline())
+# 해당 크기 만큼의 배열 초기화
+row_word = [['' for _ in range(N)] for _ in range(N)]
+col_word = [['' for _ in range(N)] for _ in range(N)]
+result = "YES"
+for i in range(N):
+    word = input()
+    for j, val in enumerate(word, start=0):
+        row_word[i][j] = val
+        col_word[j][i] = row_word[i][j]
+if row_word != col_word:
+    result = "NO"
+print(result)
