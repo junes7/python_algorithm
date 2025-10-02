4ac07d2f589ddbdff9dbc0d83d9cb57b0909199d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-03 07:50:45 +0900

    [level 1] Title: 소수 만들기, Time: 23.88 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int[] nums) {
-        int len = nums.length, d, idx, sums, rlt = 0;
-        for(int i=0;i<len-2;i++) {
-            for(int j=i+1;j<len-1;j++) {
-                for(int k=j+1;k<len;k++) {
-                    d=2; idx=0;
+        int rlt = 0, sums, idx, d;
+        for(int i=0;i<nums.length-2;i++) {
+            for(int j=i+1;j<nums.length-1;j++) {
+                for(int k=j+1;k<nums.length;k++) {
                     sums=nums[i]+nums[j]+nums[k];
+                    d=2;idx=0;
                     while(sums>1) {
                         if(sums%d==0) {
                             idx++;
@@ -13,12 +13,14 @@ class Solution {
                         } else {
                             d++;
                         }
-                        
                     }
                     rlt+=idx==1?1:0;
                 }
             }
         }
+        
+        
+
         return rlt;
     }
 }
