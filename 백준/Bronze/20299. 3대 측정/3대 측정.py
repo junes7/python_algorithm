0aa7aa6dc6df616ad9d52567784649bc4af9a705
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-04 01:24:32 +0900

    [Bronze II] Title: 3대 측정, Time: 468 ms, Memory: 189064 KB -BaekjoonHub

---
+++ 백준/Bronze/20299. 3대 측정/3대 측정.py
@@ -0,0 +1,18 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+N, K, L = map(int, input().split())
+cnt = 0 # 클럽 가입이 가능한 팀원 수
+res = []
+for _ in range(N):
+    team = list(map(int, input().split()))
+    check = True	# 조건 판별 변수
+    for i in team:
+        if i < L:
+            check = False
+            break
+    if check:
+        if sum(team) >= K:
+            res.extend(team)
+            cnt += 1
+print(cnt)
+print(*res)	# *list 를 통해 한 줄 출력
