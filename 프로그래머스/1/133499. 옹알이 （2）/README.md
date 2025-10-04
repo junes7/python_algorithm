6a7700ba9fe4f32ec16b72620dfeb17c57833189
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-04 11:05:35 +0900

    [level 1] Title: 옹알이 (2), Time: 1.43 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/README.md
+++ 프로그래머스/1/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499?language=java) 
 
 ### 성능 요약
 
-메모리: 4.11 MB, 시간: 0.08 ms
+메모리: 78.9 MB, 시간: 1.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 12일 21:06:51
+2025년 10월 04일 11:05:33
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int rlt = 0;
+        for(int i=0;i<babbling.length;i++) {
+            if(babbling[i].contains("ayaaya")||
+               babbling[i].contains("yeye")||
+               babbling[i].contains("woowoo")||
+               babbling[i].contains("mama"))
+                continue;
+            babbling[i]=babbling[i].replace("aya"," ");
+            babbling[i]=babbling[i].replace("ye"," ");
+            babbling[i]=babbling[i].replace("woo"," ");
+            babbling[i]=babbling[i].replace("ma"," ");
+            babbling[i]=babbling[i].replace(" ","");
+            if(babbling[i].length()==0) rlt+=1;
+        }
+        return rlt;
+    }
+}
