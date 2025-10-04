2a7a308b91b347c1a69f5ccad0cd4638cf8cae49
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-04 12:21:21 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 2.30 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,6 +1,6 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 04일 12:21:03
+2025년 10월 04일 12:21:19
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,14 +1,22 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.Map;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int zeros=0, matches=0, rlt[] = new int[2];
-        for(int item:lottos) {
-            if(item==0) zeros++;
-            else if(IntStream.of(win_nums).anyMatch(elem -> elem==item))matches++;
+        Map<Integer,Integer> rank=Map.of(6,1,5,2,4,3,3,4,2,5);
+        int len=lottos.length, zeros=0, matches=0, rlt[] = new int[2];
+        for(int i=0;i<len;i++) {
+            if(lottos[i]==0)
+                zeros++;
+            for(int j=0;j<win_nums.length;j++) {
+                if(lottos[i]==win_nums[j]) {
+                    matches++;
+                    break;
+                }
+            }
         }
-        rlt[1]=7-matches>=6?6:7-matches;
-        rlt[0]=rlt[1]-zeros<1?1:rlt[1]-zeros;
+        System.out.println(zeros);
+        System.out.println(matches);
+        rlt[0]=Optional.ofNullable(rank).rank(Integer::trim).orElse(0);
+        rlt[1]=rank.get(matches);
         return rlt;
     }
 }
