6702b48450cb08181f7967fd38ed491977f3c0be
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-07 06:34:43 +0900

    [Silver V] Title: 카드1, Time: 92 ms, Memory: 109412 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,13 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+nums = int(input())
+card = []
+ans = []
+for i in range(1, nums+1):
+    card.append(i)
+while (len(card) != 0):
+    ans.append(card.pop(0))
+    if(len(card) != 0):
+        card.append(card.pop(0))
+for j in ans:
+    print(j, end =" ")
