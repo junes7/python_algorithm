2e76144e46fc6b20265a55e18d47c710e72c3ef9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-09 05:35:14 +0900

    [Silver III] Title: 이전 순열, Time: 100 ms, Memory: 111424 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,14 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+N = int(input())
+a = list(map(int, input().split()))
+for i in range(N-1, 0, -1):
+    if a[i] < a[i-1]:
+        x, y = i-1, i
+        for j in range(N-1, 0, -1):
+            if a[j] < a[x]:
+                a[j], a[x] = a[x], a[j]
+                a = a[:i] + sorted(a[i:], reverse=True)
+                print(*a)
+                exit(0)
+print(-1)
