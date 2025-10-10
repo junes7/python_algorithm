ea5092c24f23c451f009c371248aaec0edc09f18
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-10 13:22:21 +0900

    [Bronze III] Title: 3n+1 수열, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/14920. 3n＋1 수열/3n＋1 수열.py
@@ -0,0 +1,11 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n = int(input())
+i = 1
+while n != 1:
+    if n % 2:
+        n = 3*n + 1
+    else:
+        n = n//2
+    i += 1
+print(i)
