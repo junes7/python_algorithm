5a98fd42753fd3368728155b320a23ff3df96003
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-13 07:02:24 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 140 ms, Memory: 113804 KB -BaekjoonHub

---
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -0,0 +1,18 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n = int(input())
+ban=[]
+same=[0]*n
+for i in range(n):
+    ban.append([*map(int,input().split())])
+    same[i]=[0]*n
+for i in range(5):
+    for j in range(n):
+        for k in range(j+1,n):
+            if ban[j][i]==ban[k][i]:
+                same[k][j]=1
+                same[j][k]=1
+cnt=[]
+for s in same:
+    cnt.append(s.count(1))
+print(cnt.index(max(cnt))+1)
