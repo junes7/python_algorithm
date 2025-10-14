3f23dd35403c83a765ff455620a0829f3df46d15
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-15 07:40:33 +0900

    [Bronze I] Title: 행복한지 슬픈지, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
@@ -0,0 +1,11 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+message = input()
+if message.count(":-)") == 0 and message.count(":-(") == 0:
+    print("none")
+elif message.count(":-)") == message.count(":-("):
+    print("unsure")
+elif message.count(":-)") > message.count(":-("):
+    print("happy")
+else:
+    print("sad")
