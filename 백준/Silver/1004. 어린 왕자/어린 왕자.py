25315c88f0fb259c1fc5c4d79cfd0cfda2cf2a13
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-16 23:33:42 +0900

    [Silver III] Title: 어린 왕자, Time: 104 ms, Memory: 110760 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -0,0 +1,22 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+T = int(input())
+for _ in range(T):
+    # 출발점 도착점
+    x1, y1, x2, y2 = list(map(int, input().split()))
+    # 행성계의 개수
+    n = int(input())
+    count = 0
+    for _ in range(n):
+        cx, cy, cr = map(int, input().split())
+        dis1 = (x1 - cx)**2 + (y1 - cy)**2
+        dis2 = (x2 - cx)**2 + (y2 - cy)**2
+        pow_cr = cr**2
+        
+        if pow_cr > dis1 and pow_cr > dis2:
+            pass
+        elif pow_cr > dis1:
+            count += 1
+        elif pow_cr > dis2:
+            count += 1
+    print(count)
